00efaa2828089dbb96e15ec20e43601156f190e8
Roshan Pius <rpius@google.com>
2016-09-29 09:03:59 -0700

    wifi: Restructure wifi legacy HAL implementation
    Changes in the CL:
    1. Move the legacy HAL implementation from
    "frameworks/opt/net/wifi/wifi_hal_legacy" to
    "hardware/interfaces/wifi/1.0/default".
    2. Renamed the .rc file and the module name in makefile to
    conform to the HIDL style guide.
    3. Restructured the existing code to create a new class called
    |WifiLegacy|. This class will be used to invoke all the legacy
    HAL functions and handle the "C" style callbacks.
    
    No changes have been made to the existing failure_reason.cpp,
    service.cpp files.
    
    Bug: 31821133
    Test: mmma -j32 hardware/interfaces/wifi/1.0/default
    Change-Id: I63e8543f49886f8446101320a97d1e96e30d1035

---
+++ wifi/1.0/default/wifi_chip.cpp
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "wifi_chip.h"
+
+#include <android-base/logging.h>
+
+#include "failure_reason_util.h"
+
+namespace android {
+namespace hardware {
+namespace wifi {
+namespace V1_0 {
+namespace implementation {
+
+WifiChip::WifiChip(std::shared_ptr<WifiLegacyHal> legacy_hal)
+    : legacy_hal_(legacy_hal) {}
+
+void WifiChip::invalidate() {
+  legacy_hal_.reset();
+  callbacks_.clear();
+}
+
+Return<void> WifiChip::registerEventCallback(
+    const sp<IWifiChipEventCallback>& callback) {
+  if (!legacy_hal_.get())
+    return Void();
+  // TODO(b/31632518): remove the callback when the client is destroyed
+  callbacks_.insert(callback);
+  return Void();
+}
+
+Return<void> WifiChip::getAvailableModes(getAvailableModes_cb cb) {
+  if (!legacy_hal_.get()) {
+    cb(hidl_vec<ChipMode>());
+    return Void();
+  } else {
+    // TODO add implementation
+    return Void();
+  }
+}
+
+Return<void> WifiChip::configureChip(uint32_t /*mode_id*/) {
+  // TODO add implementation
+  return Void();
+}
+
+Return<uint32_t> WifiChip::getMode() {
+  if (!legacy_hal_.get())
+    return 0;
+  // TODO add implementation
+  return 0;
+}
+
+Return<void> WifiChip::requestChipDebugInfo() {
+  // TODO add implementation
+  return Void();
+}
+
+Return<void> WifiChip::requestDriverDebugDump() {
+  // TODO implement
+  return Void();
+}
+
+Return<void> WifiChip::requestFirmwareDebugDump() {
+  // TODO implement
+  return Void();
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace wifi
+}  // namespace hardware
+}  // namespace android
