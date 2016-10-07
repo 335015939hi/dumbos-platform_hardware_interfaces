e5fe737198b18bbf2dc708b3f6ddf713266de914
Roshan Pius <rpius@google.com>
2016-10-07 09:21:42 -0700

    wifi: Invoke failure callbacks
    Invoke the failure callbacks for |requestChipDebugInfo|,
    |requestDriverDebugDump| and |requestFirmwareDebugDump| methods.
    
    Bug: 32014425
    Test: Compiles
    Change-Id: I49b8ae93fe846df6d9663b10d2a70eb89bd00931

--- wifi/1.0/default/wifi_chip.cpp
+++ wifi/1.0/default/wifi_chip.cpp
@@ -87,6 +87,11 @@ Return<void> WifiChip::requestChipDebugInfo() {
   if (ret.first != WIFI_SUCCESS) {
     LOG(ERROR) << "Failed to get driver version: "
                << LegacyErrorToString(ret.first);
+    FailureReason reason = CreateFailureReasonLegacyError(
+        ret.first, " failed to get driver version");
+    for (const auto& callback : callbacks_) {
+      callback->onChipDebugInfoFailure(reason);
+    }
     return Void();
   }
   result.driverDescription = ret.second.c_str();
@@ -95,6 +100,11 @@ Return<void> WifiChip::requestChipDebugInfo() {
   if (ret.first != WIFI_SUCCESS) {
     LOG(ERROR) << "Failed to get firmware version: "
                << LegacyErrorToString(ret.first);
+    FailureReason reason = CreateFailureReasonLegacyError(
+        ret.first, " failed to get firmware version");
+    for (const auto& callback : callbacks_) {
+      callback->onChipDebugInfoFailure(reason);
+    }
     return Void();
   }
   result.firmwareDescription = ret.second.c_str();
@@ -114,6 +124,10 @@ Return<void> WifiChip::requestDriverDebugDump() {
   if (ret.first != WIFI_SUCCESS) {
     LOG(ERROR) << "Failed to get driver debug dump: "
                << LegacyErrorToString(ret.first);
+    FailureReason reason = CreateFailureReasonLegacyError(ret.first, "");
+    for (const auto& callback : callbacks_) {
+      callback->onDriverDebugDumpFailure(reason);
+    }
     return Void();
   }
 
@@ -136,6 +150,10 @@ Return<void> WifiChip::requestFirmwareDebugDump() {
   if (ret.first != WIFI_SUCCESS) {
     LOG(ERROR) << "Failed to get firmware debug dump: "
                << LegacyErrorToString(ret.first);
+    FailureReason reason = CreateFailureReasonLegacyError(ret.first, "");
+    for (const auto& callback : callbacks_) {
+      callback->onFirmwareDebugDumpFailure(reason);
+    }
     return Void();
   }
 
