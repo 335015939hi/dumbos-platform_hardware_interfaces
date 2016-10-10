53fb5647cb11ef3fb6b48f02b14debdb418f53c7
Connor O'Brien <connoro@google.com>
2016-10-10 12:31:37 -0700

    Add boot_control HIDL default implementation
    Create basic implementation that passes commands
    through to the old HAL implementation.
    
    Bug: 31864052
    Test: Ran and compared output to old implementation
    Change-Id: I01f4450dc3a1893e13b8fb325ea40cf9c98297be
    Signed-off-by: Connor O'Brien <connoro@google.com>

---
+++ boot_control/1.0/default/BootControl.h
@@ -0,0 +1,45 @@
+#ifndef HIDL_GENERATED_android_hardware_boot_control_V1_0_BootControl_H_
+#define HIDL_GENERATED_android_hardware_boot_control_V1_0_BootControl_H_
+
+#include <android/hardware/boot_control/1.0/IBootControl.h>
+#include <hidl/Status.h>
+
+#include <hidl/MQDescriptor.h>
+namespace android {
+namespace hardware {
+namespace boot_control {
+namespace V1_0 {
+namespace implementation {
+
+using ::android::hardware::boot_control::V1_0::CommandResult;
+using ::android::hardware::boot_control::V1_0::IBootControl;
+using ::android::hardware::Return;
+using ::android::hardware::Void;
+using ::android::hardware::hidl_vec;
+using ::android::hardware::hidl_string;
+using ::android::sp;
+
+struct BootControl : public IBootControl {
+    BootControl(boot_control_module_t* module);
+    // Methods from ::android::hardware::boot_control::V1_0::IBootControl follow.
+    Return<uint32_t> getNumberSlots()  override;
+    Return<uint32_t> getCurrentSlot()  override;
+    Return<void> markBootSuccessful(markBootSuccessful_cb _hidl_cb)  override;
+    Return<void> setActiveBootSlot(uint32_t slot, setActiveBootSlot_cb _hidl_cb)  override;
+    Return<void> setSlotAsUnbootable(uint32_t slot, setSlotAsUnbootable_cb _hidl_cb)  override;
+    Return<int32_t> isSlotBootable(uint32_t slot)  override;
+    Return<int32_t> isSlotMarkedSuccessful(uint32_t slot)  override;
+    Return<void> getSuffix(uint32_t slot, getSuffix_cb _hidl_cb)  override;
+private:
+    boot_control_module_t* mModule;
+};
+
+extern "C" IBootControl* HIDL_FETCH_IBootControl(const char* name);
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace boot_control
+}  // namespace hardware
+}  // namespace android
+
+#endif  // HIDL_GENERATED_android_hardware_boot_control_V1_0_BootControl_H_
