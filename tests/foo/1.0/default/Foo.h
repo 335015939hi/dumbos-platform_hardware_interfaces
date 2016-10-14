d391023eb5dd37ae6be77172546e0f184e7c5886
Yifan Hong <elsk@google.com>
2016-10-14 10:41:41 -0700

    [ DO NOT SUBMIT ] WIP: hidl_test: add test for vec<handle> and handle in struct.
    Test: hidl_test
    
    Bug: 32160974
    
    Change-Id: I386f04b06c6c2e9b718a4ae7516a5fd21de145ac

--- tests/foo/1.0/default/Foo.h
+++ tests/foo/1.0/default/Foo.h
@@ -5,6 +5,7 @@
 #include <hidl/Status.h>
 
 #include <hidl/MQDescriptor.h>
+#include <vector>
 namespace android {
 namespace hardware {
 namespace tests {
@@ -44,7 +45,12 @@ struct Foo : public IFoo {
     virtual Return<void> transpose2(const hidl_array<hidl_string, 5 /* 5 */, 3 /* 3 */>& in, transpose2_cb _hidl_cb)  override;
     virtual Return<void> sendVec(const hidl_vec<uint8_t>& data, sendVec_cb _hidl_cb)  override;
     virtual Return<void> sendVecVec(sendVecVec_cb _hidl_cb)  override;
+    virtual Return<void> createMyHandle(createMyHandle_cb _hidl_cb)  override;
+    virtual Return<void> createHandles(uint32_t size, createHandles_cb _hidl_cb)  override;
+    virtual Return<void> closeHandles()  override;
 
+private:
+    std::vector<::native_handle_t *> mHandles;
 };
 
 extern "C" IFoo* HIDL_FETCH_IFoo(const char* name);
