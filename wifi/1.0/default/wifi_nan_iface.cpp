f384fd53821f69b6ebb7d863d739b2b37ecc7519
Roshan Pius <rpius@google.com>
2016-10-17 10:04:04 -0700

    wifi: Stub implementation for rtt & nan
    Bug: 31991232
    Bug: 31991076
    Test: Compiles
    Change-Id: I965c337178ca95b0ca2738b791478b7d823e1d95

--- wifi/1.0/default/wifi_nan_iface.cpp
+++ wifi/1.0/default/wifi_nan_iface.cpp
@@ -57,6 +57,200 @@ Return<void> WifiNanIface::getType(getType_cb hidl_status_cb) {
   return Void();
 }
 
+Return<void> WifiNanIface::registerEventCallback(
+    const sp<IWifiNanIfaceEventCallback>& event_callback,
+    registerEventCallback_cb hidl_status_cb) {
+  if (!is_valid_) {
+    hidl_status_cb(createWifiStatus(WifiStatusCode::ERROR_WIFI_IFACE_INVALID));
+    return Void();
+  }
+  // TODO(b/31632518): remove the callback when the client is destroyed
+  event_callbacks_.emplace_back(event_callback);
+  hidl_status_cb(createWifiStatus(WifiStatusCode::SUCCESS));
+  return Void();
+}
+
+Return<void> WifiNanIface::enableRequest(uint32_t /* cmdId */,
+                                         const NanEnableRequest& /* msg */,
+                                         enableRequest_cb hidl_status_cb) {
+  if (!is_valid_) {
+    hidl_status_cb(createWifiStatus(WifiStatusCode::ERROR_WIFI_IFACE_INVALID));
+    return Void();
+  }
+  hidl_status_cb(createWifiStatus(WifiStatusCode::SUCCESS));
+  return Void();
+}
+
+Return<void> WifiNanIface::disableRequest(uint32_t /* cmdId */,
+                                          disableRequest_cb hidl_status_cb) {
+  if (!is_valid_) {
+    hidl_status_cb(createWifiStatus(WifiStatusCode::ERROR_WIFI_IFACE_INVALID));
+    return Void();
+  }
+  hidl_status_cb(createWifiStatus(WifiStatusCode::SUCCESS));
+  return Void();
+}
+
+Return<void> WifiNanIface::publishRequest(uint32_t /* cmdId */,
+                                          const NanPublishRequest& /* msg */,
+                                          publishRequest_cb hidl_status_cb) {
+  if (!is_valid_) {
+    hidl_status_cb(createWifiStatus(WifiStatusCode::ERROR_WIFI_IFACE_INVALID));
+    return Void();
+  }
+  hidl_status_cb(createWifiStatus(WifiStatusCode::SUCCESS));
+  return Void();
+}
+
+Return<void> WifiNanIface::publishCancelRequest(
+    uint32_t /* cmdId */,
+    const NanPublishCancelRequest& /* msg */,
+    publishCancelRequest_cb hidl_status_cb) {
+  if (!is_valid_) {
+    hidl_status_cb(createWifiStatus(WifiStatusCode::ERROR_WIFI_IFACE_INVALID));
+    return Void();
+  }
+  hidl_status_cb(createWifiStatus(WifiStatusCode::SUCCESS));
+  return Void();
+}
+
+Return<void> WifiNanIface::subscribeRequest(
+    uint32_t /* cmdId */,
+    const NanSubscribeRequest& /* msg */,
+    subscribeRequest_cb hidl_status_cb) {
+  if (!is_valid_) {
+    hidl_status_cb(createWifiStatus(WifiStatusCode::ERROR_WIFI_IFACE_INVALID));
+    return Void();
+  }
+  hidl_status_cb(createWifiStatus(WifiStatusCode::SUCCESS));
+  return Void();
+}
+
+Return<void> WifiNanIface::subscribeCancelRequest(
+    uint32_t /* cmdId */,
+    const NanSubscribeCancelRequest& /* msg */,
+    subscribeCancelRequest_cb hidl_status_cb) {
+  if (!is_valid_) {
+    hidl_status_cb(createWifiStatus(WifiStatusCode::ERROR_WIFI_IFACE_INVALID));
+    return Void();
+  }
+  hidl_status_cb(createWifiStatus(WifiStatusCode::SUCCESS));
+  return Void();
+}
+
+Return<void> WifiNanIface::transmitFollowupRequest(
+    uint32_t /* cmdId */,
+    const NanTransmitFollowupRequest& /* msg */,
+    transmitFollowupRequest_cb hidl_status_cb) {
+  if (!is_valid_) {
+    hidl_status_cb(createWifiStatus(WifiStatusCode::ERROR_WIFI_IFACE_INVALID));
+    return Void();
+  }
+  hidl_status_cb(createWifiStatus(WifiStatusCode::SUCCESS));
+  return Void();
+}
+
+Return<void> WifiNanIface::configRequest(uint32_t /* cmdId */,
+                                         const NanConfigRequest& /* msg */,
+                                         configRequest_cb hidl_status_cb) {
+  if (!is_valid_) {
+    hidl_status_cb(createWifiStatus(WifiStatusCode::ERROR_WIFI_IFACE_INVALID));
+    return Void();
+  }
+  hidl_status_cb(createWifiStatus(WifiStatusCode::SUCCESS));
+  return Void();
+}
+
+Return<void> WifiNanIface::beaconSdfPayloadRequest(
+    uint32_t /* cmdId */,
+    const NanBeaconSdfPayloadRequest& /* msg */,
+    beaconSdfPayloadRequest_cb hidl_status_cb) {
+  if (!is_valid_) {
+    hidl_status_cb(createWifiStatus(WifiStatusCode::ERROR_WIFI_IFACE_INVALID));
+    return Void();
+  }
+  hidl_status_cb(createWifiStatus(WifiStatusCode::SUCCESS));
+  return Void();
+}
+
+Return<void> WifiNanIface::getVersion(getVersion_cb hidl_status_cb) {
+  if (!is_valid_) {
+    hidl_status_cb(createWifiStatus(WifiStatusCode::ERROR_WIFI_IFACE_INVALID),
+                   0);
+    return Void();
+  }
+  hidl_status_cb(createWifiStatus(WifiStatusCode::SUCCESS), 0);
+  return Void();
+}
+
+Return<void> WifiNanIface::getCapabilities(uint32_t /* cmdId */,
+                                           getCapabilities_cb hidl_status_cb) {
+  if (!is_valid_) {
+    hidl_status_cb(createWifiStatus(WifiStatusCode::ERROR_WIFI_IFACE_INVALID));
+    return Void();
+  }
+  hidl_status_cb(createWifiStatus(WifiStatusCode::SUCCESS));
+  return Void();
+}
+
+Return<void> WifiNanIface::dataInterfaceCreate(
+    uint32_t /* cmdId */,
+    const hidl_string& /* ifaceName */,
+    dataInterfaceCreate_cb hidl_status_cb) {
+  if (!is_valid_) {
+    hidl_status_cb(createWifiStatus(WifiStatusCode::ERROR_WIFI_IFACE_INVALID));
+    return Void();
+  }
+  hidl_status_cb(createWifiStatus(WifiStatusCode::SUCCESS));
+  return Void();
+}
+
+Return<void> WifiNanIface::dataInterfaceDelete(
+    uint32_t /* cmdId */,
+    const hidl_string& /* ifaceName */,
+    dataInterfaceDelete_cb hidl_status_cb) {
+  if (!is_valid_) {
+    hidl_status_cb(createWifiStatus(WifiStatusCode::ERROR_WIFI_IFACE_INVALID));
+    return Void();
+  }
+  hidl_status_cb(createWifiStatus(WifiStatusCode::SUCCESS));
+  return Void();
+}
+Return<void> WifiNanIface::dataRequestInitiator(
+    uint32_t /* cmdId */,
+    const NanDataPathInitiatorRequest& /* msg */,
+    dataRequestInitiator_cb hidl_status_cb) {
+  if (!is_valid_) {
+    hidl_status_cb(createWifiStatus(WifiStatusCode::ERROR_WIFI_IFACE_INVALID));
+    return Void();
+  }
+  hidl_status_cb(createWifiStatus(WifiStatusCode::SUCCESS));
+  return Void();
+}
+
+Return<void> WifiNanIface::dataIndicationResponse(
+    uint32_t /* cmdId */,
+    const NanDataPathIndicationResponse& /* msg */,
+    dataIndicationResponse_cb hidl_status_cb) {
+  if (!is_valid_) {
+    hidl_status_cb(createWifiStatus(WifiStatusCode::ERROR_WIFI_IFACE_INVALID));
+    return Void();
+  }
+  hidl_status_cb(createWifiStatus(WifiStatusCode::SUCCESS));
+  return Void();
+}
+
+Return<void> WifiNanIface::dataEnd(uint32_t /* cmdId */,
+                                   const NanDataPathEndRequest& /* msg */,
+                                   dataEnd_cb hidl_status_cb) {
+  if (!is_valid_) {
+    hidl_status_cb(createWifiStatus(WifiStatusCode::ERROR_WIFI_IFACE_INVALID));
+    return Void();
+  }
+  hidl_status_cb(createWifiStatus(WifiStatusCode::SUCCESS));
+  return Void();
+}
+
 }  // namespace implementation
 }  // namespace V1_0
 }  // namespace wifi
