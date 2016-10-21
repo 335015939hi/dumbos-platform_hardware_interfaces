766116b55109dde9b1b96f5185d5eeeec957360b
Zhuoyao Zhang <zhuoyao@google.com>
2016-10-21 10:19:59 -0700

    Add implementation for hwbinder benchmark service.
    Test: make android.hardware.tests.libhwbinder@1.0-impl
    Change-Id: I75056afa32b8189020608779ec495659ea35ed48

---
+++ tests/libhwbinder/1.0/default/Benchmark.cpp
@@ -0,0 +1,31 @@
+#define LOG_TAG "libhwbinder_benchmark"
+
+#include "Benchmark.h"
+#include <android-base/logging.h>
+#include <inttypes.h>
+#include <utils/Timers.h>
+
+namespace android {
+namespace hardware {
+namespace tests {
+namespace libhwbinder {
+namespace V1_0 {
+namespace implementation {
+
+Return<void> Benchmark::sendVec(
+        const ::android::hardware::hidl_vec<uint8_t>& data,
+        sendVec_cb _hidl_cb) {
+    _hidl_cb(data);
+    return Void();
+}
+
+IBenchmark* HIDL_FETCH_IBenchmark(const char* /* name */) {
+    return new Benchmark();
+}
+
+} // namespace implementation
+}  // namespace V1_0
+}  // namespace libhwbinder
+}  // namespace tests
+}  // namespace hardware
+}  // namespace android
