08117f2d64674d3367cb30fc05f9396e2f2ceb9b
Connor O'Brien <connoro@google.com>
2016-12-02 16:22:51 -0800

    Add consumerir HIDL default implementation
    Create a basic HAL implementation that passes commands
    through to the old HAL.
    
    Test: make android.hardware.consumerir@1.0-impl
    Change-Id: Idc4b0a67cf936645b699c24504e9fab219b1ffbe
    Signed-off-by: Connor O'Brien <connoro@google.com>

---
+++ ir/1.0/default/ConsumerIr.cpp
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "ConsumerIrService"
+#include <android/log.h>
+
+#include <hardware/hardware.h>
+#include <hardware/consumerir.h>
+#include "ConsumerIr.h"
+
+namespace android {
+namespace hardware {
+namespace ir {
+namespace V1_0 {
+namespace implementation {
+
+ConsumerIr::ConsumerIr(consumerir_device_t *device) {
+    mDevice = device;
+}
+
+// Methods from ::android::hardware::consumerir::V1_0::IConsumerIr follow.
+Return<int32_t> ConsumerIr::transmit(int32_t carrierFreq, const hidl_vec<int32_t>& pattern, int32_t patternLen) {
+    return mDevice->transmit(mDevice, carrierFreq, pattern.data(), patternLen);
+}
+
+Return<int32_t> ConsumerIr::getNumCarrierFreqs() {
+    return mDevice->get_num_carrier_freqs(mDevice);
+}
+
+Return<void> ConsumerIr::getCarrierFreqs(uint64_t len, getCarrierFreqs_cb _hidl_cb) {
+    consumerir_freq_range_t *rangeAr = new consumerir_freq_range_t[len];
+    int32_t ret = mDevice->get_carrier_freqs(mDevice, len, rangeAr);
+    hidl_vec<ConsumerIrFreqRange> rangeVec;
+    rangeVec.setToExternal((ConsumerIrFreqRange*)rangeAr, len);
+    _hidl_cb(ret, rangeVec);
+    return Void();
+}
+
+
+IConsumerIr* HIDL_FETCH_IConsumerIr(const char *name) {
+    consumerir_device_t *dev;
+    const hw_module_t *hw_module = NULL;
+
+    int ret = hw_get_module(name, &hw_module);
+    if (ret != 0) {
+        ALOGE("hw_get_module %s failed: %d", name, ret);
+        return nullptr;
+    }
+    ret = hw_module->methods->open(hw_module, CONSUMERIR_TRANSMITTER, (hw_device_t **) &dev);
+    if (ret < 0) {
+        ALOGE("Can't open consumer IR transmitter, error: %d", ret);
+        return nullptr;
+    }
+    return new ConsumerIr(dev);
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace ir
+}  // namespace hardware
+}  // namespace android
