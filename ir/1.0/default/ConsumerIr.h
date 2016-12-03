2181559707a5ec6e8deaf87c57c6a9e7cc7008a2
Connor O'Brien <connoro@google.com>
2016-12-02 16:22:51 -0800

    Add consumerir HIDL default implementation
    Create a basic HAL implementation that passes commands
    through to the old HAL.
    
    Test: make android.hardware.consumerir@1.0-impl
    Change-Id: Idc4b0a67cf936645b699c24504e9fab219b1ffbe
    Signed-off-by: Connor O'Brien <connoro@google.com>

---
+++ ir/1.0/default/ConsumerIr.h
@@ -0,0 +1,42 @@
+#ifndef ANDROID_HARDWARE_IR_V1_0_IR_H
+#define ANDROID_HARDWARE_IR_V1_0_IR_H
+
+#include <android/hardware/ir/1.0/IConsumerIr.h>
+#include <hardware/consumerir.h>
+#include <hidl/MQDescriptor.h>
+#include <hidl/Status.h>
+
+namespace android {
+namespace hardware {
+namespace ir {
+namespace V1_0 {
+namespace implementation {
+
+using ::android::hardware::ir::V1_0::ConsumerIrFreqRange;
+using ::android::hardware::ir::V1_0::IConsumerIr;
+using ::android::hardware::hidl_array;
+using ::android::hardware::hidl_string;
+using ::android::hardware::hidl_vec;
+using ::android::hardware::Return;
+using ::android::hardware::Void;
+using ::android::sp;
+
+struct ConsumerIr : public IConsumerIr {
+    ConsumerIr(consumerir_device_t *device);
+    // Methods from ::android::hardware::ir::V1_0::IConsumerIr follow.
+    Return<int32_t> transmit(int32_t carrierFreq, const hidl_vec<int32_t>& pattern, int32_t patternLen) override;
+    Return<int32_t> getNumCarrierFreqs() override;
+    Return<void> getCarrierFreqs(uint64_t len, getCarrierFreqs_cb _hidl_cb) override;
+private:
+    consumerir_device_t *mDevice;
+};
+
+extern "C" IConsumerIr* HIDL_FETCH_IConsumerIr(const char* name);
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace ir
+}  // namespace hardware
+}  // namespace android
+
+#endif  // ANDROID_HARDWARE_IR_V1_0_IR_H
