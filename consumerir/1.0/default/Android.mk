2de7fa764d136e49ad76f9f76676cc06b7f42832
Connor O'Brien <connoro@google.com>
2016-12-02 16:34:24 -0800

    Add android.hardware.consumerir@1.0 service.
    Test: make android.hardware.consumerir@1.0-service
    Change-Id: Ife45f98836d4d5a3b40d26f21d43e1903fdb8fb3
    Signed-off-by: Connor O'Brien <connoro@google.com>

--- consumerir/1.0/default/Android.mk
+++ consumerir/1.0/default/Android.mk
@@ -16,3 +16,21 @@ LOCAL_SHARED_LIBRARIES := \
     android.hardware.consumerir@1.0 \
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE := android.hardware.consumerir@1.0-service
+LOCAL_INIT_RC := android.hardware.consumerir@1.0-service.rc
+LOCAL_SRC_FILES := \
+    service.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libhwbinder \
+    libhardware \
+    libhidlbase \
+    libhidltransport \
+    libutils \
+    android.hardware.consumerir@1.0 \
+
+include $(BUILD_EXECUTABLE)
