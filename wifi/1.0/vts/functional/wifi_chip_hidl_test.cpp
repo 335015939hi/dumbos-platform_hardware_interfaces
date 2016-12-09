bb42bb02af39bd3544468fbeb7246b6d881dfd77
Roshan Pius <rpius@google.com>
2016-12-09 14:28:42 -0800

    wifi(vts): Some IWifiChip tests
    These tests basically just ensure that we can call the corresponding
    API's successfully.
    
    Bug: 33459306
    Test: adb push
    out/target/product/angler/data/nativetest/wifi_hidl_test/wifi_hidl_test
    /data/ &&  adb shell /data/wifi_hidl_test
    
    Change-Id: I12ad26653993ce7167d007a887e7aff07fd0a5ce

--- wifi/1.0/vts/functional/wifi_chip_hidl_test.cpp
+++ wifi/1.0/vts/functional/wifi_chip_hidl_test.cpp
@@ -22,16 +22,41 @@
 
 #include "wifi_hidl_test_utils.h"
 
-using ::android::hardware::wifi::V1_0::IWifiChip;
 using ::android::sp;
+using ::android::hardware::hidl_string;
+using ::android::hardware::hidl_vec;
+using ::android::hardware::wifi::V1_0::ChipId;
+using ::android::hardware::wifi::V1_0::WifiStatus;
+using ::android::hardware::wifi::V1_0::WifiStatusCode;
+using ::android::hardware::wifi::V1_0::IWifiChip;
 
 class WifiChipHidlTest : public ::testing::Test {
  public:
-  virtual void SetUp() override {}
+  virtual void SetUp() override {
+    wifi_chip_ = getWifiChip();
+    ASSERT_NE(wifi_chip_, nullptr);
+  }
 
   virtual void TearDown() override { stopWifi(); }
 
+  // Helper function to configure the Chip in one of the supported modes.
+  // Most of the non mode configuration related methods require chip
+  // to be first configured.
+  void configureChip() {
+    std::vector<IWifiChip::ChipMode> chip_modes;
+    wifi_chip_->getAvailableModes([&](
+        const WifiStatus& status, const hidl_vec<IWifiChip::ChipMode>& modes) {
+      EXPECT_EQ(status.code, WifiStatusCode::SUCCESS);
+      EXPECT_GT(modes.size(), 0u);
+      chip_modes = modes;
+    });
+    wifi_chip_->configureChip(chip_modes[0].id, [&](const WifiStatus& status) {
+      EXPECT_EQ(status.code, WifiStatusCode::SUCCESS);
+    });
+  }
+
  protected:
+  sp<IWifiChip> wifi_chip_;
 };
 
 /*
@@ -42,4 +67,61 @@ class WifiChipHidlTest : public ::testing::Test {
 TEST(WifiChipHidlTestNoFixture, Create) {
   EXPECT_NE(getWifiChip(), nullptr);
   stopWifi();
+}
+
+/*
+ * GetId:
+ * Invokes the |IWifiChip.getId| method and ensures that it
+ * succeeds.
+ */
+TEST_F(WifiChipHidlTest, GetId) {
+  wifi_chip_->getId([&](const WifiStatus& status, ChipId id) {
+    EXPECT_EQ(status.code, WifiStatusCode::SUCCESS);
+  });
+}
+
+/*
+ * GetAvailableMode:
+ * Invokes the |IWifiChip.getAvailableModes| method and ensures that it
+ * succeeds.
+ */
+TEST_F(WifiChipHidlTest, GetAvailableModes) {
+  wifi_chip_->getAvailableModes([&](
+      const WifiStatus& status, const hidl_vec<IWifiChip::ChipMode>& modes) {
+    EXPECT_EQ(status.code, WifiStatusCode::SUCCESS);
+    EXPECT_GT(modes.size(), 0u);
+  });
+}
+
+/*
+ * ConfigureChip:
+ * Invokes the |IWifiChip.configureChip| method and ensures that it
+ * succeeds.
+ */
+TEST_F(WifiChipHidlTest, ConfigureChip) {
+  std::vector<IWifiChip::ChipMode> chip_modes;
+  wifi_chip_->getAvailableModes([&](
+      const WifiStatus& status, const hidl_vec<IWifiChip::ChipMode>& modes) {
+    EXPECT_EQ(status.code, WifiStatusCode::SUCCESS);
+    EXPECT_GT(modes.size(), 0u);
+    chip_modes = modes;
+  });
+  for (const auto& mode : chip_modes) {
+    wifi_chip_->configureChip(mode.id, [&](const WifiStatus& status) {
+      EXPECT_EQ(status.code, WifiStatusCode::SUCCESS);
+    });
+  }
+}
+
+/*
+ * GetCapabilities:
+ * Invokes the |IWifiChip.getCapabilities| method and ensures that it
+ * succeeds.
+ */
+TEST_F(WifiChipHidlTest, GetCapabilities) {
+  configureChip();
+  wifi_chip_->getCapabilities([&](const WifiStatus& status, uint32_t caps) {
+    EXPECT_EQ(status.code, WifiStatusCode::SUCCESS);
+    EXPECT_NE(caps, 0u);
+  });
 }
