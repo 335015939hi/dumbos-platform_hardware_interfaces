2b433d04ef07cd724a1389ec59ef8ed71a6f9ea4
Roshan Pius <rpius@google.com>
2016-12-14 15:25:27 -0800

    supplicant(vts): Add gtest framework
    Bug: 33457575
    Test: Compiles
    Change-Id: I8b560483166822a33b8d81433178fdbc6077ec11

---
+++ wifi/supplicant/1.0/vts/functional/supplicant_hidl_test_utils.cpp
@@ -0,0 +1,170 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+
+#include <wifi_hal/driver_tool.h>
+#include <wifi_system/interface_tool.h>
+#include <wifi_system/supplicant_manager.h>
+
+#include "supplicant_hidl_test_utils.h"
+
+using ::android::hardware::wifi::supplicant::V1_0::ISupplicant;
+using ::android::hardware::wifi::supplicant::V1_0::ISupplicantIface;
+using ::android::hardware::wifi::supplicant::V1_0::ISupplicantNetwork;
+using ::android::hardware::wifi::supplicant::V1_0::ISupplicantStaIface;
+using ::android::hardware::wifi::supplicant::V1_0::ISupplicantStaNetwork;
+using ::android::hardware::wifi::supplicant::V1_0::ISupplicantP2pIface;
+using ::android::hardware::wifi::supplicant::V1_0::IfaceType;
+using ::android::hardware::wifi::supplicant::V1_0::SupplicantStatus;
+using ::android::hardware::wifi::supplicant::V1_0::SupplicantStatusCode;
+using ::android::wifi_hal::DriverTool;
+using ::android::wifi_system::InterfaceTool;
+using ::android::wifi_system::SupplicantManager;
+using ::android::sp;
+using ::android::hardware::hidl_string;
+using ::android::hardware::hidl_vec;
+
+const char kSupplicantServiceName[] = "wpa_supplicant";
+
+void stopFramework() {
+  ASSERT_EQ(std::system("svc wifi disable"), 0);
+  sleep(5);
+}
+
+void startFramework() {
+  ASSERT_EQ(std::system("svc wifi enable"), 0);
+}
+
+void stopSupplicant() {
+  DriverTool driver_tool;
+  SupplicantManager supplicant_manager;
+
+  ASSERT_TRUE(supplicant_manager.StopSupplicant());
+  ASSERT_TRUE(driver_tool.UnloadDriver());
+  ASSERT_FALSE(supplicant_manager.IsSupplicantRunning());
+}
+
+void startSupplicant() {
+  DriverTool driver_tool;
+  InterfaceTool iface_tool;
+  SupplicantManager supplicant_manager;
+
+  EXPECT_TRUE(driver_tool.LoadDriver());
+  EXPECT_TRUE(driver_tool.ChangeFirmwareMode(DriverTool::kFirmwareModeSta));
+  EXPECT_TRUE(iface_tool.SetWifiUpState(true));
+  ASSERT_TRUE(supplicant_manager.StartSupplicant());
+  sleep(5);
+  ASSERT_TRUE(supplicant_manager.IsSupplicantRunning());
+}
+
+sp<ISupplicant> getSupplicant() {
+  return ISupplicant::getService(kSupplicantServiceName);
+}
+
+bool findIfaceToSupportIfaceType(sp<ISupplicant> supplicant,
+                                 IfaceType type,
+                                 ISupplicant::IfaceInfo* out_info) {
+  bool operation_failed = false;
+  std::vector<ISupplicant::IfaceInfo> iface_infos;
+  supplicant->listInterfaces([&](const SupplicantStatus& status,
+                                 hidl_vec<ISupplicant::IfaceInfo> infos) {
+    if (status.code != SupplicantStatusCode::SUCCESS) {
+      operation_failed = true;
+    }
+    iface_infos = infos;
+  });
+  if (operation_failed) {
+    return false;
+  }
+  for (const auto& info : iface_infos) {
+    if (info.type == type) {
+      *out_info = info;
+      return true;
+    }
+  }
+  return false;
+}
+
+sp<ISupplicantStaIface> getSupplicantStaIface() {
+  sp<ISupplicant> supplicant = getSupplicant();
+  if (!supplicant.get()) {
+    return nullptr;
+  }
+  ISupplicant::IfaceInfo info;
+  if (!findIfaceToSupportIfaceType(supplicant, IfaceType::STA, &info)) {
+    return nullptr;
+  }
+  bool operation_failed = false;
+  sp<ISupplicantStaIface> sta_iface;
+  supplicant->getInterface(
+      info,
+      [&](const SupplicantStatus& status, const sp<ISupplicantIface>& iface) {
+        if (status.code != SupplicantStatusCode::SUCCESS) {
+          operation_failed = true;
+        }
+        sta_iface = ISupplicantStaIface::castFrom(iface);
+      });
+  if (operation_failed) {
+    return nullptr;
+  }
+  return sta_iface;
+}
+
+sp<ISupplicantStaNetwork> getSupplicantStaNetwork() {
+  sp<ISupplicantStaIface> sta_iface = getSupplicantStaIface();
+  if (!sta_iface.get()) {
+    return nullptr;
+  }
+  bool operation_failed = false;
+  sp<ISupplicantStaNetwork> sta_network;
+  sta_iface->addNetwork([&](const SupplicantStatus& status,
+                            const sp<ISupplicantNetwork>& network) {
+    if (status.code != SupplicantStatusCode::SUCCESS) {
+      operation_failed = true;
+    }
+    sta_network = ISupplicantStaNetwork::castFrom(network);
+  });
+  if (operation_failed) {
+    return nullptr;
+  }
+  return sta_network;
+}
+
+sp<ISupplicantP2pIface> getSupplicantP2pIface() {
+  sp<ISupplicant> supplicant = getSupplicant();
+  if (!supplicant.get()) {
+    return nullptr;
+  }
+  ISupplicant::IfaceInfo info;
+  if (!findIfaceToSupportIfaceType(supplicant, IfaceType::P2P, &info)) {
+    return nullptr;
+  }
+  bool operation_failed = false;
+  sp<ISupplicantP2pIface> p2p_iface;
+  supplicant->getInterface(
+      info,
+      [&](const SupplicantStatus& status, const sp<ISupplicantIface>& iface) {
+        if (status.code != SupplicantStatusCode::SUCCESS) {
+          operation_failed = true;
+        }
+        p2p_iface = ISupplicantP2pIface::castFrom(iface);
+      });
+  if (operation_failed) {
+    return nullptr;
+  }
+  return p2p_iface;
+}
