6b355d45a451a3bf6fe307f6766ac3762dfe4e82
Roshan Pius <rpius@google.com>
2016-12-14 15:25:27 -0800

    supplicant(vts): Add gtest framework
    Bug: 33457575
    Test: adb push
    out/target/product/angler/data/nativetest/supplicant_hidl_test/supplicant_hidl_test
    /data/ &&  adb shell /data/supplicant_hidl_test
    Change-Id: I8b560483166822a33b8d81433178fdbc6077ec11

---
+++ wifi/supplicant/1.0/vts/functional/supplicant_sta_network_hidl_test.cpp
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <android-base/logging.h>
+
+#include <gtest/gtest.h>
+
+#include <android/hardware/wifi/supplicant/1.0/ISupplicantStaNetwork.h>
+
+#include "supplicant_hidl_test_utils.h"
+
+using ::android::hardware::wifi::supplicant::V1_0::ISupplicantStaNetwork;
+using ::android::sp;
+
+class SupplicantStaNetworkHidlTest : public ::testing::Test {
+ public:
+  virtual void SetUp() override {
+    startSupplicantAndWaitForHidlService();
+    EXPECT_TRUE(turnOnExcessiveLogging());
+    sta_network_ = getSupplicantStaNetwork();
+    EXPECT_NE(sta_network_.get(), nullptr);
+  }
+
+  virtual void TearDown() override { stopSupplicant(); }
+
+ protected:
+  sp<ISupplicantStaNetwork> sta_network_;
+};
+
+/*
+ * Create:
+ * Ensures that an instance of the ISupplicantStaNetwork proxy object is
+ * successfully created.
+ */
+TEST(SupplicantStaNetworkHidlTestNoFixture, Create) {
+  startSupplicantAndWaitForHidlService();
+  EXPECT_NE(getSupplicantStaNetwork().get(), nullptr);
+  stopSupplicant();
+}
