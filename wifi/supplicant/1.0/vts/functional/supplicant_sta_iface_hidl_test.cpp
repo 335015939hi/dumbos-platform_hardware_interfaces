32149cc3cc610fe8dce826c950cae89e6f7e737f
Roshan Pius <rpius@google.com>
2016-12-14 15:25:27 -0800

    supplicant(vts): Add gtest framework
    Bug: 33457575
    Test: Compiles
    Change-Id: I8b560483166822a33b8d81433178fdbc6077ec11

---
+++ wifi/supplicant/1.0/vts/functional/supplicant_sta_iface_hidl_test.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <android-base/logging.h>
+
+#include <gtest/gtest.h>
+
+#include <android/hardware/wifi/supplicant/1.0/ISupplicantStaIface.h>
+
+#include "supplicant_hidl_test_utils.h"
+
+using ::android::hardware::wifi::supplicant::V1_0::ISupplicantStaIface;
+using ::android::sp;
+
+class SupplicantStaIfaceHidlTest : public ::testing::Test {
+ public:
+  virtual void SetUp() override {
+    startSupplicant();
+    sta_iface_ = getSupplicantStaIface();
+    EXPECT_NE(sta_iface_.get(), nullptr);
+  }
+
+  virtual void TearDown() override { stopSupplicant(); }
+
+ protected:
+  sp<ISupplicantStaIface> sta_iface_;
+};
+
+/*
+ * Create:
+ * Ensures that an instance of the ISupplicantStaIface proxy object is
+ * successfully created.
+ */
+TEST(SupplicantStaIfaceHidlTestNoFixture, Create) {
+  startSupplicant();
+  EXPECT_NE(getSupplicantStaIface().get(), nullptr);
+  stopSupplicant();
+}
