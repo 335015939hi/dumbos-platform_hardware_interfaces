2371f9a691f5a68015406316a7ece4336f3e616f
Roshan Pius <rpius@google.com>
2017-01-27 14:43:20 -0800

    wifi(implementation): Add logs for RPC method errors
    Add logs for the error status returned for every RPC method.
    
    Sample error log:
    01-28 00:11:49.344   452   452 E android.hardware.wifi@1.0-service:
    Returned error: NOT_AVAILABLE() from std::pair<WifiStatus, uint32_t>
    android::hardware::wifi::V1_0::implementation::WifiChip::getModeInternal()
    
    Bug: 34775790
    Test: Manual tests
    Test: VTS
    
    Change-Id: Id7c2518b39108de443fd137c551bba50979fd9cb

--- wifi/1.0/default/wifi_status_util.h
+++ wifi/1.0/default/wifi_status_util.h
@@ -21,6 +21,25 @@
 
 #include "wifi_legacy_hal.h"
 
+#define GET_MACRO(_1, _2, NAME, ...) NAME
+
+#define CREATE_WIFI_STATUS2(code, description) \
+  createWifiStatus(code, description, __PRETTY_FUNCTION__)
+#define CREATE_WIFI_STATUS1(code) \
+  createWifiStatus(code, "", __PRETTY_FUNCTION__)
+#define CREATE_WIFI_STATUS(...) \
+  GET_MACRO(__VA_ARGS__, CREATE_WIFI_STATUS2, CREATE_WIFI_STATUS1)(__VA_ARGS__)
+
+#define CREATE_WIFI_STATUS_FROM_LEGACY_ERROR2(legacy_code, description) \
+  createWifiStatusFromLegacyError(legacy_code, description, __PRETTY_FUNCTION__)
+#define CREATE_WIFI_STATUS_FROM_LEGACY_ERROR1(legacy_code) \
+  createWifiStatusFromLegacyError(legacy_code, "", __PRETTY_FUNCTION__)
+#define CREATE_WIFI_STATUS_FROM_LEGACY_ERROR(...)  \
+  GET_MACRO(__VA_ARGS__,                           \
+            CREATE_WIFI_STATUS_FROM_LEGACY_ERROR2, \
+            CREATE_WIFI_STATUS_FROM_LEGACY_ERROR1) \
+  (__VA_ARGS__)
+
 namespace android {
 namespace hardware {
 namespace wifi {
@@ -29,11 +48,11 @@ namespace implementation {
 
 std::string legacyErrorToString(legacy_hal::wifi_error error);
 WifiStatus createWifiStatus(WifiStatusCode code,
-                            const std::string& description);
-WifiStatus createWifiStatus(WifiStatusCode code);
+                            const std::string& description,
+                            const std::string& function_name);
 WifiStatus createWifiStatusFromLegacyError(legacy_hal::wifi_error error,
-                                           const std::string& description);
-WifiStatus createWifiStatusFromLegacyError(legacy_hal::wifi_error error);
+                                           const std::string& description,
+                                           const std::string& function_name);
 
 }  // namespace implementation
 }  // namespace V1_0
