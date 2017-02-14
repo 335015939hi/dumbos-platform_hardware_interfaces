33f3f4edf4ea342b043a5400f9f0cc8dc548eb54
Etan Cohen <etancohen@google.com>
2017-02-14 09:27:39 -0800

    [AWARE] VTS tests for enableRequest API
    Add parameterized tests for enableRequest API expected to succeed.
    
    Bug: 35276551
    Test: existing VTS test behavior unmodified (fail expected)
    Change-Id: I28370ec784f7af66c53d23d5b6d4f6486ef8bfea

--- wifi/1.0/vts/functional/wifi_nan_iface_hidl_test.cpp
+++ wifi/1.0/vts/functional/wifi_nan_iface_hidl_test.cpp
@@ -31,6 +31,8 @@ using ::android::sp;
 using ::nan::CallbackType;
 using ::nan::WifiNanIfaceEventCallback;
 
+static uint16_t commandId = 0;
+
 /**
  * Fixture to use for all NAN Iface HIDL interface tests.
  */
@@ -83,7 +85,7 @@ TEST(WifiNanIfaceHidlTestNoFixture, FailOnIfaceInvalid) {
  * getCapabilitiesRequest: validate that returns capabilities.
  */
 TEST_F(WifiNanIfaceHidlTest, getCapabilitiesRequest) {
-  uint16_t inputCmdId = 10;
+  uint16_t inputCmdId = commandId++;
   ASSERT_EQ(WifiStatusCode::SUCCESS,
         HIDL_INVOKE(iwifiNanIface, getCapabilitiesRequest, inputCmdId).code);
   // wait for a callback
@@ -109,3 +111,138 @@ TEST_F(WifiNanIfaceHidlTest, getCapabilitiesRequest) {
   EXPECT_GT(cbd->capabilities.maxSubscribeInterfaceAddresses, (unsigned int) 0);
   EXPECT_NE(cbd->capabilities.supportedCipherSuites, (unsigned int) 0);
 }
+
+/*
+ * EnableRequest fixture and parameterized tests
+ */
+
+/*
+ * Contains the EnableRequest parameters which we'll be varying in the framework - i.e. high
+ * priority for testing.
+ */
+class EnableRequestParameters {
+  public:
+    EnableRequestParameters(uint8_t masterPref, bool disableDiscoveryAddressChangeIndication,
+        bool disableStartedClusterIndication, bool disableJoinedClusterIndication,
+        uint16_t clusterIdTopRangeVal, uint16_t clusterIdBottomRangeVal) :
+        masterPref(masterPref),
+        disableDiscoveryAddressChangeIndication(disableDiscoveryAddressChangeIndication),
+        disableStartedClusterIndication(disableStartedClusterIndication),
+        disableJoinedClusterIndication(disableJoinedClusterIndication),
+        clusterIdTopRangeVal(clusterIdTopRangeVal),
+        clusterIdBottomRangeVal(clusterIdBottomRangeVal) {}
+
+    uint8_t masterPref;
+
+    bool disableDiscoveryAddressChangeIndication;
+    bool disableStartedClusterIndication;
+    bool disableJoinedClusterIndication;
+
+    uint16_t clusterIdTopRangeVal;
+    uint16_t clusterIdBottomRangeVal;
+};
+
+/* EnableRequest parameterized fixture */
+class EnableRequestFixture: public WifiNanIfaceHidlTest,
+                            public ::testing::WithParamInterface<EnableRequestParameters> {
+  public:
+    /*
+     * Returns a NanEnableRequest initialized to the baseline/default values
+     * used by the framework.
+     */
+    std::shared_ptr<NanEnableRequest> getInitializedNanEnableRequest() {
+      NanEnableRequest* msg = new NanEnableRequest;
+
+      // fill-in NanEnableRequest with typical values used by the framework
+      msg->operateInBand[(size_t) NanBandIndex::NAN_BAND_24GHZ] = true;
+      msg->operateInBand[(size_t) NanBandIndex::NAN_BAND_5GHZ] = true;
+      msg->hopCountMax = 2;
+      msg->configParams.masterPref = 10;
+      msg->configParams.disableDiscoveryAddressChangeIndication = false;
+      msg->configParams.disableStartedClusterIndication = false;
+      msg->configParams.disableJoinedClusterIndication = false;
+      msg->configParams.includePublishServiceIdsInBeacon = true;
+      msg->configParams.numberOfPublishServiceIdsInBeacon = 0;
+      msg->configParams.includeSubscribeServiceIdsInBeacon = true;
+      msg->configParams.numberOfSubscribeServiceIdsInBeacon = 0;
+      msg->configParams.rssiWindowSize = 8;
+      msg->configParams.macAddressRandomizationIntervalSec = 1800;
+
+      NanBandSpecificConfig& config24 = msg->configParams.bandSpecificConfig[
+            (size_t) NanBandIndex::NAN_BAND_24GHZ];
+      config24.rssiClose = 60;
+      config24.rssiMiddle = 70;
+      config24.rssiCloseProximity = 60;
+      config24.dwellTimeMs = 200;
+      config24.scanPeriodSec = 20;
+      config24.validDiscoveryWindowIntervalVal = false;
+
+      NanBandSpecificConfig& config5 = msg->configParams.bandSpecificConfig[
+            (size_t) NanBandIndex::NAN_BAND_5GHZ];
+      config5.rssiClose = 60;
+      config5.rssiMiddle = 75;
+      config5.rssiCloseProximity = 60;
+      config5.dwellTimeMs = 200;
+      config5.scanPeriodSec = 20;
+      config5.validDiscoveryWindowIntervalVal = false;
+
+      msg->debugConfigs.validClusterIdVals = true;
+      msg->debugConfigs.clusterIdTopRangeVal = 0xFFFF;
+      msg->debugConfigs.clusterIdBottomRangeVal = 0x0000;
+      msg->debugConfigs.validIntfAddrVal = false;
+      msg->debugConfigs.validOuiVal = false;
+      msg->debugConfigs.ouiVal = 0;
+      msg->debugConfigs.validRandomFactorForceVal = false;
+      msg->debugConfigs.randomFactorForceVal = 0;
+      msg->debugConfigs.validHopCountForceVal = false;
+      msg->debugConfigs.hopCountForceVal = 0;
+      msg->debugConfigs.validDiscoveryChannelVal = false;
+      msg->debugConfigs.discoveryChannelMhzVal[(size_t) NanBandIndex::NAN_BAND_24GHZ] = 0;
+      msg->debugConfigs.discoveryChannelMhzVal[(size_t) NanBandIndex::NAN_BAND_5GHZ] = 0;
+      msg->debugConfigs.validUseBeaconsInBandVal = false;
+      msg->debugConfigs.useBeaconsInBandVal[(size_t) NanBandIndex::NAN_BAND_24GHZ] = true;
+      msg->debugConfigs.useBeaconsInBandVal[(size_t) NanBandIndex::NAN_BAND_5GHZ] = true;
+      msg->debugConfigs.validUseSdfInBandVal = false;
+      msg->debugConfigs.useSdfInBandVal[(size_t) NanBandIndex::NAN_BAND_24GHZ] = true;
+      msg->debugConfigs.useSdfInBandVal[(size_t) NanBandIndex::NAN_BAND_5GHZ] = true;
+
+      return std::shared_ptr<NanEnableRequest>(msg);
+    }
+};
+
+/*
+ * Validate that enableRequest calls succeeds with various set of parameters.
+ */
+TEST_P(EnableRequestFixture, enableRequestSuccess) {
+  uint16_t inputCmdId = commandId++;
+  std::shared_ptr<NanEnableRequest> msg = getInitializedNanEnableRequest();
+
+  const EnableRequestParameters& overrides = GetParam();
+  msg->configParams.masterPref = overrides.masterPref;
+  msg->configParams.disableDiscoveryAddressChangeIndication =
+        overrides.disableDiscoveryAddressChangeIndication;
+  msg->configParams.disableStartedClusterIndication = overrides.disableStartedClusterIndication;
+  msg->configParams.disableJoinedClusterIndication = overrides.disableJoinedClusterIndication;
+  msg->debugConfigs.clusterIdTopRangeVal = overrides.clusterIdTopRangeVal;
+  msg->debugConfigs.clusterIdBottomRangeVal = overrides.clusterIdBottomRangeVal;
+
+  ASSERT_EQ(WifiStatusCode::SUCCESS,
+        HIDL_INVOKE(iwifiNanIface, enableRequest, inputCmdId, *msg).code);
+  // wait for a callback
+  auto cbd = callback->wait(CallbackType::NOTIFY_ENABLE_RESPONSE);
+  ASSERT_NE(cbd->callbackType, CallbackType::TIMEOUT);
+  ASSERT_EQ(cbd->callbackType, CallbackType::NOTIFY_ENABLE_RESPONSE);
+  ASSERT_EQ(cbd->id, inputCmdId);
+  ASSERT_EQ(cbd->status.status, NanStatusType::SUCCESS);
+}
+
+INSTANTIATE_TEST_CASE_P(enableRequestSuccessTestCases, EnableRequestFixture,
+    ::testing::Values(
+    EnableRequestParameters(0, false, false, false, 0xFFFF, 0x0000),
+    EnableRequestParameters(1, false, false, true, 0xFFFF, 0x1000),
+    EnableRequestParameters(10, false, true, true, 0x1000, 0x0000),
+    EnableRequestParameters(10, true, false, false, 0xFFFF, 0x0000),
+    EnableRequestParameters(10, true, false, true, 0xFFFF, 0x0000),
+    EnableRequestParameters(10, true, true, false, 0xFFFF, 0x0000),
+    EnableRequestParameters(10, true, true, true, 0xFFFF, 0x0000)
+));
