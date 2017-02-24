0523f885d35f7b15ca3f95e6b1d2bcc6938a9403
Yuexi Ma <yuexima@google.com>
2017-02-24 13:17:02 -0800

    change all vts hidl tests to use VtsHalHidlTargetBaseTest
    Bug: 33385836
    Test: mma
    Change-Id: I660d6e0fce6422d87d4b227273a77c95d20ed3f2

--- wifi/1.0/vts/functional/wifi_hidl_test.cpp
+++ wifi/1.0/vts/functional/wifi_hidl_test.cpp
@@ -18,7 +18,7 @@
 
 #include <android/hardware/wifi/1.0/IWifi.h>
 
-#include <gtest/gtest.h>
+#include <VtsHalHidlTargetBaseTest>
 
 #include "wifi_hidl_test_utils.h"
 
@@ -28,7 +28,7 @@ using ::android::sp;
 /**
  * Fixture to use for all root Wifi HIDL interface tests.
  */
-class WifiHidlTest : public ::testing::Test {
+class WifiHidlTest : public ::VtsHalHidlTargetBaseTest {
    public:
     virtual void SetUp() override {}
 
