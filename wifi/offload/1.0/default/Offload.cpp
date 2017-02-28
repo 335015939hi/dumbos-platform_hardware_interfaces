95e3bfed83064dfbea44152d61a450d533a1e63d
Sohani Rao <sohanirao@google.com>
2017-02-28 11:55:17 -0800

    Chre offload HAL default implementation
    Implementation of the Offload HAL service.
    
    Bug: 32842314
    Test: Unit tests and Mannual test to ensure service is running
    Change-Id: Iac694bf8553904ab77bbc3241c3e3f3833b03418

---
+++ wifi/offload/1.0/default/Offload.cpp
@@ -0,0 +1,63 @@
+#include "Offload.h"
+#include <android-base/logging.h>
+
+namespace android {
+namespace hardware {
+namespace wifi {
+namespace offload {
+namespace V1_0 {
+namespace implementation {
+
+Offload::Offload() :
+        mScanEventSubscription(false),
+        mSubscriptionTimeMs(0),
+        mSubscriptionDelayMs(0) {
+    // Load Nano app
+    LOG(android::base::INFO) << "Wifi Offload HAL impl";
+}
+
+// Methods from ::android::hardware::wifi::offload::V1_0::IOffload follow.
+Return<void> Offload::configureScans(const ScanParam& param, const ScanFilter& filter) {
+    Offload::mScanParam = param;
+    Offload::mScanFilter = filter;
+    // TODO: implement Wifi Nano app scan configuration
+    return Void();
+}
+
+Return<void> Offload::getScanStats(getScanStats_cb offloadScanStatsCallback) {
+    ScanStats *scanStats = nullptr;
+    // TODO: implement getting scan stats from Wifi Nano app
+    if (scanStats != nullptr) {
+        offloadScanStatsCallback(*scanStats);
+    }
+    return Void();
+}
+
+Return<void> Offload::subscribeScanResults(uint32_t delayMs) {
+    mScanEventSubscription = false;
+    // TODO: get current time from Wifi Nano App
+    mSubscriptionTimeMs = 0;
+    mSubscriptionDelayMs = delayMs;
+    // TODO implement informing Wifi Nano App
+    return Void();
+}
+
+Return<void> Offload::unsubscribeScanResults() {
+    mScanEventSubscription = false;
+    // TODO: implement updating Wifi Nano app
+    return Void();
+}
+
+Return<void> Offload::setEventCallback(const sp<IOffloadCallback>& cb) {
+    Offload::mScanEventCallback = cb;
+    return Void();
+}
+
+// Methods from ::android::hidl::base::V1_0::IBase follow.
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace offload
+}  // namespace wifi
+}  // namespace hardware
+}  // namespace android
