e6ec08def09e8642ed6bce6dc82c00f484707295
Wileen Chiu <wileenc@codeaurora.org>
2017-03-03 11:57:48 -0800

    Add p2 option in OpenLogicalChannel
    - Adding support for P2 parameter
    - Needed to support OMAPI 3.0 defined
      by SIM Alliance
    
    Bug: 27360148
    Change-Id: I88134eded001e14c12e1f2a18efd0a2b51bc22fe

--- radio/1.0/vts/functional/radio_hidl_hal_icc.cpp
+++ radio/1.0/vts/functional/radio_hidl_hal_icc.cpp
@@ -233,13 +233,11 @@ TEST_F(RadioHidlTest, iccTransmitApduBasicChannel) {
  */
 TEST_F(RadioHidlTest, iccOpenLogicalChannel) {
     int serial = 1;
-
-    for (int i = 0; i < (int) cardStatus.applications.size(); i++) {
-        radio->iccOpenLogicalChannel(++serial, cardStatus.applications[i].aidPtr);
-        EXPECT_EQ(std::cv_status::no_timeout, wait());
-        EXPECT_EQ(serial, radioRsp->rspInfo.serial);
-        EXPECT_EQ(RadioResponseType::SOLICITED, radioRsp->rspInfo.type);
-    }
+    // Specified in ISO 7816-4 clause 7.1.1 0x04 means that FCP template is requested.
+    radio->iccOpenLogicalChannel(++serial, hidl_string("A000000063504B43532D3135"), 0x04);
+    EXPECT_EQ(std::cv_status::no_timeout, wait());
+    EXPECT_EQ(serial, radioRsp->rspInfo.serial);
+    EXPECT_EQ(RadioResponseType::SOLICITED, radioRsp->rspInfo.type);
 }
 
 /*
