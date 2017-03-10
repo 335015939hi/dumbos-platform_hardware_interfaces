869c666a734d62b4f69bdd5e49d8bc3bacb05cc0
Yuexi Ma <yuexima@google.com>
2017-03-10 00:38:17 -0800

    Change vts target base test class name
    Test: make vts succeed, vts kernel tests and nfc hal tests passed
    Change-Id: I38ae6cc940d0f17bb3352f360efdf9c70697c76b

--- wifi/1.0/vts/functional/wifi_sta_iface_hidl_test.cpp
+++ wifi/1.0/vts/functional/wifi_sta_iface_hidl_test.cpp
@@ -18,7 +18,7 @@
 
 #include <android/hardware/wifi/1.0/IWifiStaIface.h>
 
-#include <VtsHalHidlTargetBaseTest.h>
+#include <VtsHalHidlTargetTestBase.h>
 
 #include "wifi_hidl_test_utils.h"
 
@@ -28,7 +28,7 @@ using ::android::sp;
 /**
  * Fixture to use for all STA Iface HIDL interface tests.
  */
-class WifiStaIfaceHidlTest : public ::testing::VtsHalHidlTargetBaseTest {
+class WifiStaIfaceHidlTest : public ::testing::VtsHalHidlTargetTestBase {
    public:
     virtual void SetUp() override {}
 
