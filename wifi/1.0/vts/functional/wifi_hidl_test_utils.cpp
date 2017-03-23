bcb0427ea3005334713862489fb236f628e9d8e6
Etan Cohen <etancohen@google.com>
2017-03-23 08:11:47 -0700

    [WIFI] Restore initialization code
    Restore the usage of WIFI-specific main function. The main
    function was inadvertantly disabled by moving it into a static
    library - which caused the default main to be used.
    
    Need a custom main since need to stop WIFI before tests start
    to enforce a clean starting point.
    
    Bug: 35276551
    Test: confirms custom main routine is called
    Change-Id: I978827bc0f0f16d9ca4f159fbf42d0bc53f5df99

--- wifi/1.0/vts/functional/wifi_hidl_test_utils.cpp
+++ wifi/1.0/vts/functional/wifi_hidl_test_utils.cpp
@@ -83,12 +83,11 @@ bool configureChipToSupportIfaceTypeInternal(const sp<IWifiChip>& wifi_chip,
 }  // namespace
 
 void stopFramework() {
-    ASSERT_EQ(std::system("stop"), 0);
     stopWifi();
     sleep(5);
 }
 
-void startFramework() { ASSERT_EQ(std::system("start"), 0); }
+void startFramework() {}
 
 sp<IWifi> getWifi() {
     sp<IWifi> wifi = ::testing::VtsHalHidlTargetTestBase::getService<IWifi>();
