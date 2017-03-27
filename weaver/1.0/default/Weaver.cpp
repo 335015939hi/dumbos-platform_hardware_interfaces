c95efbe35f0e88eed555f00761adb765ded23d86
Andrew Scull <ascull@google.com>
2017-03-27 14:24:18 +0100

    Weaver HAL
    Weaver is used to harden the user authentication flow by securely
    storing secrets which can only be read by presenting the associated key
    secret. The key secret is derived from the user's credential.
    
    Bug: 35628284
    Test: Implement HAL, boot and call from system_server
    Change-Id: Ia72fd537670c08d27320c944a0fe91243137bf6c

---
+++ weaver/1.0/default/Weaver.cpp
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "Weaver.h"
+
+namespace android {
+namespace hardware {
+namespace weaver {
+namespace V1_0 {
+namespace implementation {
+
+// Methods from ::android::hardware::weaver::V1_0::IWeaver follow.
+Return<void> Weaver::getConfig(getConfig_cb _hidl_cb) {
+    _hidl_cb(WeaverStatus::FAILED, WeaverConfig{});
+    return Void();
+}
+
+Return<WeaverStatus> Weaver::write(uint32_t /* slotId */, const hidl_vec<uint8_t>& /* key */,
+                                   const hidl_vec<uint8_t>& /* value */) {
+    return WeaverStatus::FAILED;
+}
+
+Return<void> Weaver::read(uint32_t /* slotId */, const hidl_vec<uint8_t>& /* key */,
+                          read_cb _hidl_cb) {
+    _hidl_cb(WeaverReadStatus::FAILED, WeaverReadResponse{});
+    return Void();
+}
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace weaver
+}  // namespace hardware
+}  // namespace android
