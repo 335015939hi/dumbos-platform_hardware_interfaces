a7414a4b5d81d82691d4ea8eebf81a8c91af2a88
Steven Moreland <smoreland@google.com>
2017-10-03 09:32:54 -0700

    Add canary in hardware/interfaces update-makefiles
    Write notice that update-makefiles is not working
    in the first makefile to get updated.
    
    Test: manual w/ update-makefiles
    Change-Id: I47f98736779f7000977470e53336725dab56d7af

--- update-makefiles.sh
+++ update-makefiles.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# If hidl-gen is broken during development, this provides early warning
+# it is updating makefiles in another tree.
+echo 'update-makefiles is not working' > $ANDROID_BUILD_TOP/hardware/interfaces/audio/2.0/Android.bp
+
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
