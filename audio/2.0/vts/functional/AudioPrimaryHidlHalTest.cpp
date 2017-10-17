be72f23306e42456e0622161bf7def9116ac6edb
Tri Vo <trong@google.com>
2017-10-17 11:07:29 -0700

    Set pipe size to >1MB for debugDump test cases.
    Test: VtsHalAudioV2_0Target
    Change-Id: I2dd43851a43609ad2953030303edaeb684b63096

--- audio/2.0/vts/functional/AudioPrimaryHidlHalTest.cpp
+++ audio/2.0/vts/functional/AudioPrimaryHidlHalTest.cpp
@@ -535,6 +535,9 @@ static void testDebugDump(DebugDump debugDump) {
     int fds[2];
     ASSERT_EQ(0, pipe2(fds, O_NONBLOCK)) << errno;
 
+    // Make sure that the pipe is at least 1 MB in size.
+    fcntl(fds[0], F_SETPIPE_SZ, 1 << 20);
+
     // Wrap the temporary file file descriptor in a native handle
     auto* nativeHandle = native_handle_create(1, 0);
     ASSERT_NE(nullptr, nativeHandle);
