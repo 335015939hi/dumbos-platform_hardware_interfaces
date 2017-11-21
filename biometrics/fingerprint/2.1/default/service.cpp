ddebf83f7089d718a6ad68e6731fa9cdbbf8e3c0
Chih-Hung Hsieh <chh@google.com>
2017-11-20 16:02:42 -0800

    Use -Werror in hardware/interfaces/biometrics/fingerprint
    * Suppress warning of unused function call result.
    
    Bug: 66996870
    Test: build with WITH_TIDY=1
    Change-Id: I91da207b80d0715d015195357d645f8cf324fa1e

--- biometrics/fingerprint/2.1/default/service.cpp
+++ biometrics/fingerprint/2.1/default/service.cpp
@@ -35,7 +35,7 @@ int main() {
     configureRpcThreadpool(1, true /*callerWillJoin*/);
 
     if (bio != nullptr) {
-        bio->registerAsService();
+        (void)bio->registerAsService();
     } else {
         ALOGE("Can't create instance of BiometricsFingerprint, nullptr");
     }
