d5e93372f50603f2279dc30bc54da6808c5e42ef
Andrii Romanchuk <andrii.romanchuk@globallogic.com>
2018-01-29 18:48:45 +0200

    MediaOMXHal: fix software codecs compatibility issue
    Add OMX data structures alignment to enable 32/64 bit processes
    compatibility.
    This patch eliminates a large number of vts failures of
    mediaOMXHal.
    
    Test:
        run vts -m VtsHalMediaOmxV1_0Host
    
    Bug: 111864530
    
    Change-Id: I7d58dffbb2b37f6dd7ca578eb3953b39b3f860dc
    Signed-off-by: Andrii Romanchuk <andrii.romanchuk@globallogic.com>

--- media/omx/1.0/vts/functional/video/media_video_hidl_test_common.cpp
+++ media/omx/1.0/vts/functional/video/media_video_hidl_test_common.cpp
@@ -16,7 +16,9 @@
 
 #define LOG_TAG "media_omx_hidl_video_test_common"
 #ifdef __LP64__
+#ifndef USE_64BITMEDIA
 #define OMX_ANDROID_COMPILE_AS_32BIT_ON_64BIT_PLATFORMS
+#endif
 #endif
 
 #include <android-base/logging.h>
