fde2e1e39c0481b0f7e22aecc42cc20c088ad7b1
Yifan Hong <elsk@google.com>
2018-08-08 17:50:48 -0700

    health.filesystem: Add reference implementation.
    Test: lshal shows reference implementation when installed
    Bug: 111655771
    Change-Id: Ibb09836b99310b6bc21e87be9c0177175efa29df

---
+++ health/filesystem/1.0/default/FileSystem.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ANDROID_HARDWARE_HEALTH_FILESYSTEM_V1_0_FILESYSTEM_H
+#define ANDROID_HARDWARE_HEALTH_FILESYSTEM_V1_0_FILESYSTEM_H
+
+#include <android/hardware/health/filesystem/1.0/IFileSystem.h>
+#include <hidl/Status.h>
+
+namespace android {
+namespace hardware {
+namespace health {
+namespace filesystem {
+namespace V1_0 {
+namespace implementation {
+
+using ::android::sp;
+using ::android::hardware::Return;
+
+struct FileSystem : public IFileSystem {
+    // Methods from ::android::hardware::health::filesystem::V1_0::IFileSystem follow.
+    Return<Result> garbageCollect() override;
+};
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace filesystem
+}  // namespace health
+}  // namespace hardware
+}  // namespace android
+
+#endif  // ANDROID_HARDWARE_HEALTH_FILESYSTEM_V1_0_FILESYSTEM_H
