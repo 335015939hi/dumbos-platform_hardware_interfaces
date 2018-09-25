9760b619073d926f1a7fe9d2ed11f76de688171e
Michael Butler <butlermichael@google.com>
2018-09-24 17:32:34 -0700

    [WIP] [DO NOT SUBMIT] NNAPI Burst prototype -- HAL
    This is a prototyping effort.
    
    FastMessageQueue is a Treble-compliant data structure that enables fast
    communication between two processes. The FMQ object itself is an atomic
    circular buffer that is optionally synchronized with a futex. However,
    FMQ has no notion of ownership or lifetime across processes, so it must
    be paired with higher-level constructs to manage the lifetime and
    ownership.
    
    The NNAPI is introducing the notion of an "Execution Burst" object (or
    more simply a "Burst" object), which is similar to an
    ANeuralNetworksExecution, but is intended to be reused across multiple
    executions and has lower IPC overheads. It achieves this low IPC
    overhead by replacing HIDL HwBinder calls with "ExecutionBurstChannels",
    where each channel is an FMQ object paired with a HIDL interface object
    for ownership and ref-counting purposes. Each channel is a
    unidirectional flow of information with exactly one producer and exactly
    one consumer. In this prototype, the object is always owned by the
    sender and referenced by the receiver.
    
    This CL includes three different changes:
    1) Defines the ExecutionBurstChannel structure in IFmqContextToken.hal
       and adds the setup method IPreparedModel::configureExecutionBurst in
       IPreparedModel.hal.
    2) Tests the Burst in both the execution path and validation path in the
       Vendor Test Suite (VTS) in neuralnetworks/1.*/vts/functional/.
    3) Creates a simple native Google Benchmark test to check the
       performance of three HAL models (add_float, mobilenet_quantized, and
       mobilenet_float) when running through three execution paths (original
       execution, blocking FMQ with futex, and spinning FMQ).
    
    The VTS binary--VtsHalNeuralnetworksV1_2TargetTest--can be built and run
    as any previous version could.
    
    The two new native benchmarks can be built using:
      m BurstBenchmark -j40
      m BurstFunctional -j40
    Pushed to the device using:
      adb push $OUT/data/benchmarktest64/BurstBenchmark/BurstBenchmark /data/local/tmp/BurstBenchmark
      adb push $OUT/data/nativetest64/BurstFunctional/BurstFunctional /data/local/tmp/BurstFunctional
    And run using:
      adb shell /data/local/tmp/BurstBenchmark [--benchmark_repetitions=N]
      adb shell /data/local/tmp/BurstFunctional [--gtest_filter=*]
    
    The only local modification to this CL is to change the name of the
    service being tested in BurstBenchmark and BurstFunctional. Currently
    it's pre-set to "sample-all" in BurstBenchmark line 76 and
    BurstFunctional line 97.
    
    Bug: 119570067
    Test: mma
    Test: BurstBenchmark
    Test: BurstFunctional
    Test: VtsHalNeuralnetworksV1_2TargetTest
    Change-Id: Ia7136151289e5f9e04486718702502016ee72f67
    (cherry picked from commit 8c2e706567ab60077b6829c84de09e5d0f12cf17)

---
+++ neuralnetworks/1.2/vts/benchmark/BurstBenchmark.cpp
@@ -0,0 +1,271 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <android/hardware/neuralnetworks/1.0/types.h>
+#include <android/hardware/neuralnetworks/1.1/types.h>
+#include <android/hardware/neuralnetworks/1.2/IDevice.h>
+#include <android/hardware/neuralnetworks/1.2/IPreparedModel.h>
+#include <android/hardware/neuralnetworks/1.2/types.h>
+#include <benchmark/benchmark.h>
+#include <utils/StrongPointer.h>
+#include <utils/Trace.h>
+#include <memory>
+#include <tuple>
+#include "Callbacks.h"
+#include "ExecutionBurstController.h"
+#include "Utils.h"
+#include "add_float.h"
+#include "mobilenet_float.h"
+#include "mobilenet_quantized.h"
+
+// shorthand
+
+namespace V1_0 = ::android::hardware::neuralnetworks::V1_0;
+namespace V1_1 = ::android::hardware::neuralnetworks::V1_1;
+namespace V1_2 = ::android::hardware::neuralnetworks::V1_2;
+
+using V1_0::ErrorStatus;
+using V1_0::Request;
+using V1_0::toString;
+using V1_1::ExecutionPreference;
+using V1_2::IDevice;
+using V1_2::IPreparedModel;
+using V1_2::MeasureTiming;
+using V1_2::Model;
+
+using V1_2::implementation::ExecutionCallback;
+using V1_2::implementation::PreparedModelCallback;
+
+using ::android::sp;
+using ::android::hardware::Return;
+using ::android::nn::convertToV1_2;
+using ::android::nn::createExecutionBurstController;
+using ::android::nn::ExecutionBurstController;
+
+// Test Fixtures
+
+const std::vector<std::pair<Model, Request>> kTestValues = {
+        std::make_pair(convertToV1_2(add_float::createTestModel()), add_float::createTestRequest()),
+        std::make_pair(convertToV1_2(mobilenet_quantized::createTestModel()),
+                       mobilenet_quantized::createTestRequest()),
+        std::make_pair(convertToV1_2(mobilenet_float::createTestModel()),
+                       mobilenet_float::createTestRequest())};
+
+class ModelExecution : public ::benchmark::Fixture {
+   public:
+    void SetUp(::benchmark::State& state) override {
+        ::benchmark::Fixture::SetUp(state);
+
+        // parameters
+        const int argIndex = state.range(0);
+        std::tie(model, request) = kTestValues[argIndex];
+
+        // create memory keys for burst
+        burstMemoryKeys.resize(request.pools.size());
+        for (size_t i = 0; i < burstMemoryKeys.size(); ++i) {
+            burstMemoryKeys[i] = reinterpret_cast<intptr_t>(&request.pools[i]);
+        }
+
+        // get device
+        device = IDevice::getService("sample-all");
+        if (device == nullptr) {
+            state.SkipWithError("Failed to retrieve service.");
+        }
+
+        // get capabilities to disable vlog
+        device->getCapabilities_1_1([](auto, const auto&) {}).isOk();
+
+        // build model
+        const sp<PreparedModelCallback> callback = new PreparedModelCallback();
+        if (callback == nullptr) {
+            state.SkipWithError("Failed to allocate callback object.");
+        }
+        const Return<ErrorStatus> ret =
+                device->prepareModel_1_2(model, ExecutionPreference::SUSTAINED_SPEED, callback);
+        if (!ret.isOk()) {
+            state.SkipWithError("Hidl transport error for IDevice::prepare.");
+        }
+        if (ret != ErrorStatus::NONE) {
+            state.SkipWithError("IDevice::prepare returned launch error.");
+        }
+
+        // get preparedModel
+        callback->wait();
+        const ErrorStatus status = callback->getStatus();
+        if (status != ErrorStatus::NONE) {
+            state.SkipWithError("IDevice::prepare returned async error.");
+        }
+        preparedModel = IPreparedModel::castFrom(callback->getPreparedModel()).withDefault(nullptr);
+        if (preparedModel == nullptr) {
+            state.SkipWithError("Failed to retrieve preparedModel.");
+        }
+    }
+
+    void TearDown(const ::benchmark::State& state) override {
+        ::benchmark::Fixture::TearDown(state);
+    }
+
+   protected:
+    Model model;
+    Request request;
+    std::vector<intptr_t> burstMemoryKeys;
+    sp<IDevice> device;
+    sp<IPreparedModel> preparedModel;
+};
+
+class ExecutionBurstBlocking : public ModelExecution {
+   public:
+    void SetUp(::benchmark::State& state) override {
+        ModelExecution::SetUp(state);
+
+        // variables
+        burst = createExecutionBurstController(preparedModel, true);
+        if (burst == nullptr) {
+            state.SkipWithError("createExecutionBurstController error.");
+        }
+    }
+
+    void TearDown(const ::benchmark::State& state) override { ModelExecution::TearDown(state); }
+
+   protected:
+    std::unique_ptr<ExecutionBurstController> burst;
+};
+
+class ExecutionBurstSpinning : public ModelExecution {
+   public:
+    void SetUp(::benchmark::State& state) override {
+        ModelExecution::SetUp(state);
+
+        // variables
+        burst = createExecutionBurstController(preparedModel, false);
+        if (burst == nullptr) {
+            state.SkipWithError("createExecutionBurstController error.");
+        }
+    }
+
+    void TearDown(const ::benchmark::State& state) override { ModelExecution::TearDown(state); }
+
+   protected:
+    std::unique_ptr<ExecutionBurstController> burst;
+};
+
+// Benchmarks
+
+BENCHMARK_DEFINE_F(ModelExecution, BM_Async_Time)(benchmark::State& state) {
+    for (auto _ : state) {
+        sp<ExecutionCallback> callback = new ExecutionCallback();
+        ErrorStatus status = ErrorStatus::GENERAL_FAILURE;
+        Return<ErrorStatus> ret = ErrorStatus::GENERAL_FAILURE;
+
+        {
+            // systrace to see IPC overhead
+            ::android::ScopedTrace trace(ATRACE_TAG_NNAPI, "ModelExecution::BM_Time");
+
+            // start execution
+            ret = preparedModel->execute_1_2(request, MeasureTiming::NO, callback);
+
+            // wait for response
+            callback->wait();
+            status = callback->getStatus();
+        }
+
+        // error handling
+        if (!ret.isOk()) {
+            state.SkipWithError("Hidl transport error for ModelExecution.");
+        }
+        if (ret.isOk() && status != ErrorStatus::NONE) {
+            state.SkipWithError(("ModelExecution returned an error: " + toString(status)).c_str());
+        }
+    }
+}
+
+BENCHMARK_DEFINE_F(ModelExecution, BM_Sync_Time)(benchmark::State& state) {
+    for (auto _ : state) {
+        ErrorStatus status = ErrorStatus::GENERAL_FAILURE;
+        Return<void> ret;
+
+        {
+            // systrace to see IPC overhead
+            ::android::ScopedTrace trace(ATRACE_TAG_NNAPI, "ModelExecution::BM_Time");
+
+            // start execution
+            ret = preparedModel->executeSynchronously(
+                    request, MeasureTiming::NO,
+                    [&status](ErrorStatus errorStatus, const auto&, const auto&) {
+                        status = errorStatus;
+                    });
+        }
+
+        // error handling
+        if (!ret.isOk()) {
+            state.SkipWithError("Hidl transport error for ModelExecution.");
+        }
+        if (ret.isOk() && status != ErrorStatus::NONE) {
+            state.SkipWithError(("ModelExecution returned an error: " + toString(status)).c_str());
+        }
+    }
+}
+
+BENCHMARK_DEFINE_F(ExecutionBurstBlocking, BM_Time)(benchmark::State& state) {
+    for (auto _ : state) {
+        ErrorStatus status;
+        std::vector<OutputShape> outputShapes;
+
+        {
+            // systrace to see IPC overhead
+            ::android::ScopedTrace trace(ATRACE_TAG_NNAPI, "ExecutionBurst::BM_Time");
+
+            // perform execution
+            std::tie(status, outputShapes, std::ignore) =
+                    burst->compute(request, MeasureTiming::NO, burstMemoryKeys);
+        }
+
+        // error checking
+        if (status != ErrorStatus::NONE) {
+            state.SkipWithError(("ExecutionBurst returned an error: " + toString(status)).c_str());
+        }
+    }
+}
+
+BENCHMARK_DEFINE_F(ExecutionBurstSpinning, BM_Time)(benchmark::State& state) {
+    for (auto _ : state) {
+        ErrorStatus status;
+        std::vector<OutputShape> outputShapes;
+
+        {
+            // systrace to see IPC overhead
+            ::android::ScopedTrace trace(ATRACE_TAG_NNAPI, "ExecutionBurst::BM_Time");
+
+            // perform execution
+            std::tie(status, outputShapes, std::ignore) =
+                    burst->compute(request, MeasureTiming::NO, burstMemoryKeys);
+        }
+
+        // error checking
+        if (status != ErrorStatus::NONE) {
+            state.SkipWithError(("ExecutionBurst returned an error: " + toString(status)).c_str());
+        }
+    }
+}
+
+BENCHMARK_REGISTER_F(ModelExecution, BM_Async_Time)->Range(0, kTestValues.size() - 1);
+BENCHMARK_REGISTER_F(ModelExecution, BM_Sync_Time)->Range(0, kTestValues.size() - 1);
+BENCHMARK_REGISTER_F(ExecutionBurstBlocking, BM_Time)->Range(0, kTestValues.size() - 1);
+BENCHMARK_REGISTER_F(ExecutionBurstSpinning, BM_Time)->Range(0, kTestValues.size() - 1);
+
+// Main
+
+BENCHMARK_MAIN();
