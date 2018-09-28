72c1fcd1138ddc1a0710e9e880b6e260a4a735d9
Max Bires <jbires@google.com>
2018-09-28 13:14:09 -0700

    Expanding VTS to cover unknown tag behavior
    This change updates the NewKeyGenerationTest suite in
    keymaster_hidl_hal_test. When key generation encounters an unknown tag,
    it should add it to software enforcements on the key characteristics and
    return that in getKeyCharacteristics.
    
    Bug: 113369560
    Bug: 112048115
    Test: atest VtsHalKeymasterV4_0TargetTest
    Change-Id: I1232482e4be2727ff7173e9da890bab65e9c0f0b

--- keymaster/4.0/vts/functional/keymaster_hidl_hal_test.cpp
+++ keymaster/4.0/vts/functional/keymaster_hidl_hal_test.cpp
@@ -434,6 +434,29 @@ TEST_F(NewKeyGenerationTest, RsaNoDefaultSize) {
 }
 
 /*
+ * NewKeyGenerationTest.ReturnUnknownTag
+ *
+ * Verifies that an unknown tag and its value is returned in the software enforced list in
+ * getKeyCharacteristics().
+ */
+TEST_F(NewKeyGenerationTest, ReturnUnknownTag) {
+    HidlBuf key_blob;
+    KeyCharacteristics key_characteristics;
+    typedef typename Tag2TypedTag<static_cast<Tag>((3 << 28) | 0xffff)>::type TAG_UNKNOWN_t;
+    TAG_UNKNOWN_t TAG_UNKNOWN;
+    ASSERT_EQ(ErrorCode::OK, GenerateKey(AuthorizationSetBuilder()
+                                             .RsaSigningKey(2048, 3)
+                                             .Digest(Digest::NONE)
+                                             .Padding(PaddingMode::NONE)
+                                             .Authorization(TAG_UNKNOWN, 3),
+                                         &key_blob, &key_characteristics));
+    CheckCharacteristics(key_blob, key_characteristics);
+    AuthorizationSet unknown_param = key_characteristics.hardwareEnforced;
+    EXPECT_TRUE(unknown_param.Contains(TAG_UNKNOWN));
+    EXPECT_TRUE(unknown_param.Contains(TAG_UNKNOWN, 3));
+}
+
+/*
  * NewKeyGenerationTest.Ecdsa
  *
  * Verifies that keymaster can generate all required EC key sizes, and that the resulting keys have
