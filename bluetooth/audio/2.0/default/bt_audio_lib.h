b9a191fecf07d91e1099a7bbaad1c6c0e43f446b
Cheney Ni <cheneyni@google.com>
2018-11-12 02:51:59 +0800

    WIP: Implementation of Bluetooth Audio HAL interface V2
    This is the default implementation of BluetoothAudio V2 and 2 providers
    are defined.
    
    * BluetoothAudioProvider is the default provider, which is a stub when
      no SessionType matched.
    * A2dpLegacyAudioProvider is to replace the old A2DP legacy module and
      IPC via HIDL between BT Stack and Audio HAL.
    
    Bug: 111519504
    Test: manual
    
    Change-Id: I54d86291482b7fb041a3ac3e171b68a4ed7bfa07

---
+++ bluetooth/audio/2.0/default/bt_audio_lib.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef BLUETOOTH_AUDIO_HIDL_LIB_H_
+#define BLUETOOTH_AUDIO_HIDL_LIB_H_
+
+#include <fmq/MessageQueue.h>
+#include <hidl/MQDescriptor.h>
+
+#include <android/hardware/bluetooth/audio/2.0/types.h>
+#include <android/hardware/bluetooth/audio/2.0/IBluetoothAudioPort.h>
+
+using android::hardware::kSynchronizedReadWrite;
+using android::hardware::MessageQueue;
+using android::hardware::hidl_vec;
+using android::sp;
+
+using ::android::hardware::bluetooth::audio::V2_0::SessionType;
+using ::android::hardware::bluetooth::audio::V2_0::IBluetoothAudioPort;
+
+typedef MessageQueue<uint8_t, kSynchronizedReadWrite> DataMQ;
+typedef ::android::hardware::bluetooth::audio::V2_0::Status BluetoothAudioStatus;
+
+const sp<IBluetoothAudioPort> bt_audio_get_port_ctrl_path(SessionType sessionType);
+const DataMQ::Descriptor* bt_audio_get_data_fmq(SessionType sessionType);
+
+int bt_audio_set_port_ctrl_result_cb(const SessionType& sessionType,
+                                     std::function<void(const SessionType, const BluetoothAudioStatus&)>& res_cb,
+                                     std::function<void(const SessionType)>& end_cb);
+void bt_audio_reset_port_ctrl_result_cb(const SessionType& sessionType, const int& key);
+
+#endif // BLUETOOTH_AUDIO_HIDL_LIB_H_
