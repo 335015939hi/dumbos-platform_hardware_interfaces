218b0abc4df8764f47236244dd8a77efad965377
Janis Danisevskis <jdanis@google.com>
2018-11-13 10:59:42 -0800

    Revert "authorization_set.cpp: relax serialization of unknown tags"
    This reverts commit 28a6b79f4b15d60d81984b1ea99a3240bb029530.

--- keymaster/4.0/support/authorization_set.cpp
+++ keymaster/4.0/support/authorization_set.cpp
@@ -261,8 +261,9 @@ struct choose_serializer<MetaList<Tags...>> {
 template <>
 struct choose_serializer<> {
     static OutStreams& serialize(OutStreams& out, const KeyParameter& param) {
-        LOG(ERROR) << "Trying to serialize unknown tag " << unsigned(param.tag)
+        LOG(FATAL) << "Trying to serialize unknown tag " << unsigned(param.tag)
                    << ". Did you forget to add it to all_tags_t?";
+        abort();
         return out;
     }
 };
