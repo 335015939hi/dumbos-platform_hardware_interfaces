a5b7159f854c46cec48ff8e680f4ac7abd0634ed
Cheney Ni <cheneyni@google.com>
2018-11-17 01:42:19 +0800

    Implementation of Bluetooth Audio HAL interface v2
    This is the default implementation of Bluetooth Audio v2 and 3 providers
    are defined.
    
    * BluetoothAudioProvider is the default provider, which is a stub when
      no SessionType matched.
    * A2dpLegacyAudioProvider is to replace the old A2DP legacy module and
      IPC via HIDL between BT Stack and Audio HAL.
    * HearingAidAudioProvider is to replace the Hearing Aids module which
      uses UNIX socket as IPC and now there is HIDL based interface.
    
    Bug: 111519504
    Test: manual
    TODO: 1. HearingAids
    
    Change-Id: I54d86291482b7fb041a3ac3e171b68a4ed7bfa07

---
+++ bluetooth/audio/2.0/default/A2dpLegacyAudioProvider.cpp
@@ -0,0 +1,142 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "BTAudioProviderA2dpLegacy"
+
+#include <log/log.h>
+
+#include "A2dpLegacyAudioProvider.h"
+
+namespace android {
+namespace hardware {
+namespace bluetooth {
+namespace audio {
+namespace V2_0 {
+namespace implementation {
+
+using android::hardware::hidl_death_recipient;
+using android::hardware::Void;
+
+using ::android::hardware::bluetooth::audio::V2_0::SampleRate;
+using ::android::hardware::bluetooth::audio::V2_0::BitsPerSample;
+using ::android::hardware::bluetooth::audio::V2_0::ChannelMode;
+using ::android::hardware::bluetooth::audio::V2_0::CodecType;
+
+typedef ::android::hardware::bluetooth::audio::V2_0::CodecConfiguration::EncodedDataConfiguration EncodedDataConfiguration;
+
+bool A2dpLegacyAudioProvider::build(const SessionType& sessionType) {
+  // A2dpLegacyAudioProvider can only support types of A2DP
+  session_type_ = SessionType::A2DP_SOFTWARE_ENCODING_DATAPATH;
+  return (sessionType == SessionType::A2DP_SOFTWARE_ENCODING_DATAPATH);
+}
+
+Return<void> A2dpLegacyAudioProvider::startSession(
+    const sp<IBluetoothAudioPort>& hostIf,
+    const CodecConfiguration& codecConfig, startSession_cb _hidl_cb) {
+
+  /**
+   * Initialize the audio platform if codecConfiguration is supported.
+   * Save the the IBluetoothAudioPort interface, so that it can be used
+   * later to send stream control commands to the HAL client, based on
+   * interaction with Audio framework.
+   */
+  const PcmDataConfiguration pcmConfig = codecConfig.pcmDataConfiguration;
+  /**
+   * SBC: mSampleRate:(44100), mBitsPerSample:(16), mChannelMode:(MONO|STEREO)
+   * AAC: mSampleRate:(44100), mBitsPerSample:(16), mChannelMode:(STEREO)
+   * aptX: mSampleRate:(44100|48000), mBitsPerSample:(16), mChannelMode:(STEREO)
+   * aptX HD: mSampleRate:(44100|48000), mBitsPerSample:(24), mChannelMode:(STEREO)
+   * LDAC: mSampleRate:(44100|48000|88200|96000), mBitsPerSample:(16|24|32), mChannelMode:(STEREO)
+   */
+  switch (pcmConfig.sampleRate) {
+    case SampleRate::RATE_44100:
+    case SampleRate::RATE_48000:
+      // assume those rates are supported by all codecs
+      break;
+    case SampleRate::RATE_88200:
+    case SampleRate::RATE_96000:
+      // this rates are supported by LDAC only
+      if (codecConfig.encodedDataConfiguration.codecType == CodecType::LDAC)
+        break;
+#ifdef FALLTHROUGH_INTENDED
+      else
+        FALLTHROUGH_INTENDED;
+#endif /* FALLTHROUGH_INTENDED */
+    default:
+      ALOGW("%s - Unsupported CodecConfiguration=%s", __func__,
+            toString(codecConfig).c_str());
+      _hidl_cb(BluetoothAudioStatus::UNSUPPORTED_CODEC_CONFIGURATION, DataMQ::Descriptor());
+      return Void();
+  }
+  switch (pcmConfig.bitsPerSample) {
+    case BitsPerSample::BITS_16:
+      // assume those formats are supported by all codecs
+      break;
+    case BitsPerSample::BITS_24:
+      // those formats are supported by aptX HD and LDAC only
+      if (codecConfig.encodedDataConfiguration.codecType == CodecType::APTX_HD
+          || codecConfig.encodedDataConfiguration.codecType == CodecType::LDAC)
+        break;
+#ifdef FALLTHROUGH_INTENDED
+      else
+        FALLTHROUGH_INTENDED;
+#endif /* FALLTHROUGH_INTENDED */
+    case BitsPerSample::BITS_32:
+      // this format is supported by LDAC only
+      if (codecConfig.encodedDataConfiguration.codecType == CodecType::LDAC)
+        break;
+#ifdef FALLTHROUGH_INTENDED
+      else
+        FALLTHROUGH_INTENDED;
+#endif /* FALLTHROUGH_INTENDED */
+    default:
+      ALOGW("%s - Unsupported CodecConfiguration=%s", __func__,
+            toString(codecConfig).c_str());
+      _hidl_cb(BluetoothAudioStatus::UNSUPPORTED_CODEC_CONFIGURATION, DataMQ::Descriptor());
+      return Void();
+  }
+  switch (pcmConfig.channelMode) {
+    case ChannelMode::MONO:
+    case ChannelMode::STEREO:
+      // assume those channels are supported by all codecs
+      break;
+    default:
+      ALOGW("%s - Unsupported CodecConfiguration=%s", __func__,
+            toString(codecConfig).c_str());
+      _hidl_cb(BluetoothAudioStatus::UNSUPPORTED_CODEC_CONFIGURATION, DataMQ::Descriptor());
+      return Void();
+  }
+
+  return BluetoothAudioProvider::startSession(hostIf, codecConfig, _hidl_cb);
+}
+
+void A2dpLegacyAudioProvider::updateStreamTracksMetadata(const struct source_metadata *source_metadata) {
+  if (has_session_ && stack_iface_) {
+    ALOGI("%s - Metadata is no needed for SessionType=%s(0x%02hhx)", __func__,
+          toString(session_type_).c_str(), session_type_);
+  } else
+    ALOGW("%s - provider has NO session", __func__);
+
+  // [SEG_BT] FIXME: for test only and remember to remove after HearingAids added
+  BluetoothAudioProvider::updateStreamTracksMetadata(source_metadata);
+}
+
+}  // namespace implementation
+}  // namespace V2_0
+}  // namespace audio
+}  // namespace bluetooth
+}  // namespace hardware
+}  // namespace android
