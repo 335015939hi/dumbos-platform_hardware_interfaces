d0a07cd32b84eb6c7d74f76ebb3b5465cf96d2d0
Cheney Ni <cheneyni@google.com>
2018-11-17 01:42:19 +0800

    Implementation of Bluetooth Audio HAL interface v2
    This is the default implementation of Bluetooth Audio Hal v2, and 3
    providers are defined.
    
    * BluetoothAudioProvider is the default provider, which is a stub when
      no SessionType matched.
    * A2dpSoftwareAudioProvider uses to replace the old A2DP legacy module
      and uses IPC via HIDL interface between Bluetooth stack and Audio HAL.
    * HearingAidAudioProvider uses to replace the Hearing Aids module which
      uses UNIX socket as IPC and now there is HIDL based interface.
    
    There is also a shared library named "libbluetooth_audio_session.so" to
    provide the linkage directly between this HIDL and its HAL module,
    bluetooth_audio. With this library, we can break the library dependency
    and guarantee those symbols of libraries while compiling.
    
    Bug: 111519504
    Bug: 122503379
    Test: boot and manual test
    
    Change-Id: I54d86291482b7fb041a3ac3e171b68a4ed7bfa07

---
+++ bluetooth/audio/2.0/default/BluetoothAudioProvidersFactory.cpp
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "BTAudioProvidersFactory"
+
+#include <log/log.h>
+
+#include "BluetoothAudioProvidersFactory.h"
+#include "OffloadSupportedCodecsDB.h"
+
+namespace android {
+namespace hardware {
+namespace bluetooth {
+namespace audio {
+namespace V2_0 {
+namespace implementation {
+
+using ::android::hardware::hidl_vec;
+using ::android::hardware::Void;
+
+using BluetoothAudioStatus =
+    ::android::hardware::bluetooth::audio::V2_0::Status;
+
+A2dpSoftwareAudioProvider
+    BluetoothAudioProvidersFactory::a2dp_software_provider_instance_;
+A2dpOffloadAudioProvider
+    BluetoothAudioProvidersFactory::a2dp_offload_provider_instance_;
+HearingAidAudioProvider
+    BluetoothAudioProvidersFactory::hearing_aid_provider_instance_;
+
+Return<void> BluetoothAudioProvidersFactory::openProvider(
+    const SessionType sessionType, openProvider_cb _hidl_cb) {
+  LOG(INFO) << __func__ << " - SessionType=" << toString(sessionType);
+  BluetoothAudioStatus status = BluetoothAudioStatus::SUCCESS;
+  BluetoothAudioProvider* provider = nullptr;
+  switch (sessionType) {
+    case SessionType::A2DP_SOFTWARE_ENCODING_DATAPATH:
+      provider = &a2dp_software_provider_instance_;
+      break;
+    case SessionType::A2DP_HARDWARE_OFFLOAD_DATAPATH:
+      provider = &a2dp_offload_provider_instance_;
+      break;
+    case SessionType::HEARING_AID_SOFTWARE_ENCODING_DATAPATH:
+      provider = &hearing_aid_provider_instance_;
+      break;
+    default:
+      status = BluetoothAudioStatus::FAILURE;
+  }
+  if (provider && !provider->isValid(sessionType)) {
+    provider = nullptr;
+    status = BluetoothAudioStatus::FAILURE;
+    LOG(ERROR) << __func__ << " - SessionType=" << toString(sessionType)
+               << ", status=" << toString(status);
+  }
+  _hidl_cb(status, provider);
+  return Void();
+}
+
+Return<void> BluetoothAudioProvidersFactory::getOffloadCodecCapabilities(
+    const SessionType sessionType, getOffloadCodecCapabilities_cb _hidl_cb) {
+  LOG(INFO) << __func__ << " - SessionType=" << toString(sessionType);
+  hidl_vec<CodecCapability> codec_caps = hidl_vec<CodecCapability>(0);
+  if (sessionType != SessionType::A2DP_HARDWARE_OFFLOAD_DATAPATH) {
+    _hidl_cb(BluetoothAudioStatus::FAILURE, codec_caps);
+    return Void();
+  }
+  std::vector<CodecCapability> db_codec_caps =
+      android::bluetooth::audio::GetOffloadCodecCapabilities(
+          SessionType::A2DP_HARDWARE_OFFLOAD_DATAPATH);
+  if (!db_codec_caps.size()) {
+    _hidl_cb(BluetoothAudioStatus::FAILURE, codec_caps);
+    return Void();
+  }
+  codec_caps.resize(db_codec_caps.size());
+  int i = 0;
+  for (auto codec_cap : db_codec_caps) {
+    codec_caps[i++] = codec_cap;
+  }
+  _hidl_cb(BluetoothAudioStatus::SUCCESS, codec_caps);
+  return Void();
+}
+
+IBluetoothAudioProvidersFactory* HIDL_FETCH_IBluetoothAudioProvidersFactory(
+    const char* /* name */) {
+  return new BluetoothAudioProvidersFactory();
+}
+
+}  // namespace implementation
+}  // namespace V2_0
+}  // namespace audio
+}  // namespace bluetooth
+}  // namespace hardware
+}  // namespace android
