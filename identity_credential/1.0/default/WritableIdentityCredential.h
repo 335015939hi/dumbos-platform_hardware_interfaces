e63509c3824df6c31a3ef3ca2b449a328cac6b19
David Zeuthen <zeuthen@google.com>
2019-01-02 17:50:08 -0500

    Add default implementation of IdentityCredential HAL and VTS tests.
    For now it's an empty shell, implementation to be filled in.
    
    Non-trivial VTS tests courtesy of Michael.
    
    Test: VtsHalIdentityCredentialV1_0TargetTest
    Change-Id: I1f3450f27bb3c3cd7ab2824aa9b12e0baea36eb9

---
+++ identity_credential/1.0/default/WritableIdentityCredential.h
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2019, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ANDROID_HARDWARE_IDENTITY_CREDENTIAL_V1_0_WRITABLEIDENTITYCREDENTIAL_H
+#define ANDROID_HARDWARE_IDENTITY_CREDENTIAL_V1_0_WRITABLEIDENTITYCREDENTIAL_H
+
+#include <android/hardware/identity_credential/1.0/IWritableIdentityCredential.h>
+
+#include <android/hardware/identity_credential/support/IdentityCredentialUtils.h>
+
+namespace android {
+namespace hardware {
+namespace identity_credential {
+namespace V1_0 {
+namespace implementation {
+
+using ::android::hardware::hidl_string;
+using ::android::hardware::hidl_vec;
+using ::android::hardware::Return;
+using ::android::hardware::Void;
+using ::android::hardware::identity_credential::V1_0::EntryValue;
+using ::android::hardware::identity_credential::V1_0::ResultCode;
+using ::android::hardware::identity_credential::V1_0::SecureAccessControlProfile;
+using ::android::hardware::identity_credential::V1_0::StartRetrievalArguments;
+using ::android::hardware::keymaster::capability::V1_0::CapabilityType;
+
+struct WritableIdentityCredential : public IWritableIdentityCredential {
+    WritableIdentityCredential(const hidl_string& docType, bool testCredential)
+        : docType_(docType), testCredential_(testCredential) {}
+
+    // Methods from ::android::hardware::identity_credential::V1_0::IWritableIdentityCredential
+    // follow.
+    Return<void> getAttestationCertificate(const hidl_vec<uint8_t>& attestationApplicationId,
+                                           const hidl_vec<uint8_t>& attestationChallenge,
+                                           getAttestationCertificate_cb _hidl_cb) override;
+
+    Return<ResultCode> startPersonalization(uint8_t accessControlProfileCount,
+                                            const hidl_vec<uint16_t>& entryCounts) override;
+
+    Return<void> addAccessControlProfile(uint8_t id, const hidl_vec<uint8_t>& readerAuthPubKey,
+                                         uint64_t capabilityId, CapabilityType capabilityType,
+                                         uint32_t timeout,
+                                         addAccessControlProfile_cb _hidl_cb) override;
+
+    Return<ResultCode> beginAddEntry(const hidl_vec<uint8_t>& accessControlProfileIds,
+                                     const hidl_string& nameSpace, const hidl_string& name,
+                                     bool directlyAvailable, uint32_t entrySize) override;
+
+    Return<void> addEntryValue(const EntryValue& value, addEntryValue_cb _hidl_cb) override;
+
+    Return<void> finishAddingEntries(finishAddingEntries_cb _hidl_cb) override;
+
+   private:
+    std::string docType_;
+    bool testCredential_;
+
+    // These are set in getAttestationCertificate().
+    std::vector<uint8_t> storageKey_;
+    std::vector<uint8_t> credentialPrivKey_;
+    std::vector<uint8_t> credentialData_;
+
+    // These fields are initialized during startPersonalization()
+    size_t numAccessControlProfileRemaining_;
+    std::vector<uint16_t> remainingEntryCounts_;
+    support::SignedDataBuilder signedDataBuilder_;
+
+    // This is built in addAccessControlProfile
+    std::vector<SecureAccessControlProfile> accessControlProfiles_;
+
+    // These fields are initialized during beginAddEntry()
+    size_t entryRemainingBytes_;
+    std::vector<uint8_t> entryAdditionalData_;
+    std::string entryNameSpace_;
+    std::string entryName_;
+    std::vector<uint8_t> entryAccessControlProfileIds_;
+    bool entryDirectlyAvailable_;
+    std::vector<uint8_t> entryBStrValue_;
+    std::string entryStrValue_;
+};
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace identity_credential
+}  // namespace hardware
+}  // namespace android
+
+#endif  // ANDROID_HARDWARE_IDENTITY_CREDENTIAL_V1_0_WRITABLEIDENTITYCREDENTIAL_H
