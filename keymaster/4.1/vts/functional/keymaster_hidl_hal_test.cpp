20b967be0d76655a6b128bc13014fe3b66edebd8
Shawn Willden <swillden@google.com>
2019-09-09 02:13:58 -0600

    Add Keymaster 4.1
    Note that CL is missing complete tests (what's included is just a
    stub, really) and support library code.  All of that will come in
    near-future CLs.  This CL omits them because they'll take time and
    there's a need to unblock Keymaster 4.1 implementers now.
    
    Bug: 140193672
    Bug: 140192237
    Bug: 140824829
    Test: Will be in a future CL
    Change-Id: I0e6e3a38356f0517158a10604b549415641ad1b9

---
+++ keymaster/4.1/vts/functional/keymaster_hidl_hal_test.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "keymaster_hidl_hal_test"
+#include <cutils/log.h>
+
+#include "KeymasterHidlTest.h"
+
+using android::hardware::keymaster::V4_1::test::KeymasterHidlEnvironment;
+
+int main(int argc, char** argv) {
+    ::testing::AddGlobalTestEnvironment(KeymasterHidlEnvironment::Instance());
+    ::testing::InitGoogleTest(&argc, argv);
+    KeymasterHidlEnvironment::Instance()->init(&argc, argv);
+    int status = RUN_ALL_TESTS();
+    ALOGI("Test result = %d", status);
+    return status;
+}
