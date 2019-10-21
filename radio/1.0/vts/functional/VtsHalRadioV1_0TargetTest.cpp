764ffd9670967486d74a07bf61f4af15ee321b6b
Dan Shi <dshi@google.com>
2019-10-21 11:32:21 -0700

    Convert VtsHalRadioV1_*TargetTest to be parameterized test
    Bug: 142397658
    Bug: 142972256
    Test: atest VtsHalRadioV1_0TargetTest \
      VtsHalSapV1_0TargetTest \
      VtsHalRadioV1_1TargetTest \
      VtsHalRadioV1_2TargetTest \
      VtsHalRadioV1_3TargetTest \
      VtsHalRadioV1_4TargetTest \
    
    Change-Id: I1890f7a6b1401260e87c94c2bc4e1dfa7060a0cb

--- radio/1.0/vts/functional/VtsHalRadioV1_0TargetTest.cpp
+++ radio/1.0/vts/functional/VtsHalRadioV1_0TargetTest.cpp
@@ -14,12 +14,17 @@
  * limitations under the License.
  */
 
+#include <gtest/gtest.h>
+#include <hidl/GtestPrinter.h>
+#include <hidl/ServiceManagement.h>
 #include <radio_hidl_hal_utils_v1_0.h>
 
+INSTANTIATE_TEST_SUITE_P(PerInstance, RadioHidlTest,
+                         testing::ValuesIn((std::vector<std::string>){RADIO_SERVICE_NAME}),
+                         android::hardware::PrintInstanceNameToString);
+
 int main(int argc, char** argv) {
-    ::testing::AddGlobalTestEnvironment(RadioHidlEnvironment::Instance());
     ::testing::InitGoogleTest(&argc, argv);
-    RadioHidlEnvironment::Instance()->init(&argc, argv);
 
     // setup seed for rand function
     int seedSrand = time(NULL);
--- radio/1.0/vts/functional/VtsHalSapV1_0TargetTest.cpp
+++ radio/1.0/vts/functional/VtsHalSapV1_0TargetTest.cpp
@@ -14,12 +14,17 @@
  * limitations under the License.
  */
 
+#include <gtest/gtest.h>
+#include <hidl/GtestPrinter.h>
+#include <hidl/ServiceManagement.h>
 #include <sap_hidl_hal_utils.h>
 
+INSTANTIATE_TEST_SUITE_P(PerInstance, SapHidlTest,
+                         testing::ValuesIn((std::vector<std::string>){SAP_SERVICE_NAME}),
+                         android::hardware::PrintInstanceNameToString);
+
 int main(int argc, char** argv) {
-    ::testing::AddGlobalTestEnvironment(SapHidlEnvironment::Instance());
     ::testing::InitGoogleTest(&argc, argv);
-    SapHidlEnvironment::Instance()->init(&argc, argv);
 
     // setup seed for rand function
     int seedSrand = time(NULL);
