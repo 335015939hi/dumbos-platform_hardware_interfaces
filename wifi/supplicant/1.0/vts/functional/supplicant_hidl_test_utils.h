51bc7a2faf616efe25fd0fdaf26d761d1b40fdb4
Dan Shi <dshi@google.com>
2019-10-29 14:30:18 -0700

    Convert VtsHalWifiSupplicantV1_0TargetTest to be parameterized test
    Bug: 142397658
    Test: atest VtsHalWifiSupplicantV1_0TargetTest
    atest VtsHalWifiSupplicantP2pV1_0TargetTest
    
    Change-Id: I0cba2c29d90018d8248c583dec2354f9d3feba47

--- wifi/supplicant/1.0/vts/functional/supplicant_hidl_test_utils.h
+++ wifi/supplicant/1.0/vts/functional/supplicant_hidl_test_utils.h
@@ -25,7 +25,7 @@
 
 #include <getopt.h>
 
-#include <VtsHalHidlTargetTestEnvBase.h>
+#include "wifi_hidl_test_utils.h"
 
 // Used to stop the android wifi framework before every test.
 void stopWifiFramework();
@@ -33,67 +33,28 @@ void startWifiFramework();
 void stopSupplicant();
 // Used to configure the chip, driver and start wpa_supplicant before every
 // test.
-void startSupplicantAndWaitForHidlService();
+void startSupplicantAndWaitForHidlService(std::string service_name);
 
 // Helper functions to obtain references to the various HIDL interface objects.
 // Note: We only have a single instance of each of these objects currently.
 // These helper functions should be modified to return vectors if we support
 // multiple instances.
 android::sp<android::hardware::wifi::supplicant::V1_0::ISupplicant>
-getSupplicant();
+getSupplicant(std::string service_name, bool isP2pOn);
 android::sp<android::hardware::wifi::supplicant::V1_0::ISupplicantStaIface>
-getSupplicantStaIface();
+getSupplicantStaIface(
+    android::sp<android::hardware::wifi::supplicant::V1_0::ISupplicant>
+        supplicant);
 android::sp<android::hardware::wifi::supplicant::V1_0::ISupplicantStaNetwork>
-createSupplicantStaNetwork();
+createSupplicantStaNetwork(
+    android::sp<android::hardware::wifi::supplicant::V1_0::ISupplicant>
+        supplicant);
 android::sp<android::hardware::wifi::supplicant::V1_0::ISupplicantP2pIface>
-getSupplicantP2pIface();
-
-bool turnOnExcessiveLogging();
-
-class WifiSupplicantHidlEnvironment
-    : public ::testing::VtsHalHidlTargetTestEnvBase {
-   protected:
-    virtual void HidlSetUp() override { stopSupplicant(); }
-    virtual void HidlTearDown() override {
-        startSupplicantAndWaitForHidlService();
-    }
-
-   public:
-    // Whether P2P feature is supported on the device.
-    bool isP2pOn = true;
-
-    void usage(char* me, char* arg) {
-        fprintf(stderr,
-                "unrecognized option: %s\n\n"
-                "usage: %s <gtest options> <test options>\n\n"
-                "test options are:\n\n"
-                "-P, --p2p_on: Whether P2P feature is supported\n",
-                arg, me);
-    }
-
-    int initFromOptions(int argc, char** argv) {
-        static struct option options[] = {{"p2p_off", no_argument, 0, 'P'},
-                                          {0, 0, 0, 0}};
-
-        int c;
-        while ((c = getopt_long(argc, argv, "P", options, NULL)) >= 0) {
-            switch (c) {
-                case 'P':
-                    isP2pOn = false;
-                    break;
-                default:
-                    usage(argv[0], argv[optind]);
-                    return 2;
-            }
-        }
-
-        if (optind < argc) {
-            usage(argv[0], argv[optind]);
-            return 2;
-        }
-
-        return 0;
-    }
-};
+getSupplicantP2pIface(
+    android::sp<android::hardware::wifi::supplicant::V1_0::ISupplicant>
+        supplicant);
+bool turnOnExcessiveLogging(
+    android::sp<android::hardware::wifi::supplicant::V1_0::ISupplicant>
+        supplicant);
 
 #endif /* SUPPLICANT_HIDL_TEST_UTILS_H */
