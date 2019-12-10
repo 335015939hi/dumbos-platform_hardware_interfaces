8cd8e808b4d4d90e8a14962d222139d809744ff9
Nazish Tabassum <quic_ntabassu@quicinc.com>
2019-12-10 14:35:09 +0530

    CDMA MO SMS follow on DC feature
    Add new API in @1.5 IRadio and IRadioResponse interfaces
    to send MO CDMA SMS with expectMore option
    
    Bug: 72613248
    Change-Id: I9aec3a58b531c17ec55db290d0e82bc7d34afa84

--- radio/1.5/vts/functional/radio_hidl_hal_utils_v1_5.h
+++ radio/1.5/vts/functional/radio_hidl_hal_utils_v1_5.h
@@ -290,6 +290,8 @@ class RadioResponse_v1_5 : public ::android::hardware::radio::V1_5::IRadioRespon
 
     Return<void> sendCdmaSmsResponse(const RadioResponseInfo& info, const SendSmsResult& sms);
 
+    Return<void> sendCdmaSMSExpectMoreResponse(const RadioResponseInfo& info, const SendSmsResult& sms);
+
     Return<void> acknowledgeLastIncomingCdmaSmsResponse(const RadioResponseInfo& info);
 
     Return<void> getGsmBroadcastConfigResponse(
