a12236b6dcd05fbddbd5ada02845bf976691903d
Nathan Harold <nharold@google.com>
2019-12-23 14:34:26 -0800

    Add an Indication for Barring Info
    Add an indication and request+response to get updated
    barring information for the current cell.
    
    Bug: 8675309
    Test: compilation
    Change-Id: I226cf66dd6b9b71a6c020cdd618fe5e2af0baa20

--- radio/1.5/vts/functional/radio_indication.cpp
+++ radio/1.5/vts/functional/radio_indication.cpp
@@ -342,3 +342,10 @@ Return<void> RadioIndication_v1_5::registrationFailed(
         int32_t /*causeCode*/, int32_t /*additionalCauseCode*/) {
     return Void();
 }
+
+Return<void> RadioIndication_v1_5::onBarringInfoChanged(
+        const ::android::hardware::radio::V1_5::CellIdentity& /*cellIdentity*/,
+        const ::android::hardware::hidl_vec<::android::hardware::radio::V1_5::BarringInfo>&
+        /*barringInfos*/) {
+    return Void();
+}
