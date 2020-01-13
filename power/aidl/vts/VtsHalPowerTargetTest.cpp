457178b8bdd33e3baef74b994213a7e9c19e0197
Dan Stoza <stoza@google.com>
2020-01-13 13:06:13 -0800

    power: Add DPF and update imminent hints to AIDL
    Adds hints for the dynamic performance framework as well as a
    "display update imminent hint" to the AIDL version of the Power HAL.
    
    Test: VtsHalPowerTargetTest
    Bug: 120610745
    Bug: 136285293
    Bug: 146453294
    Change-Id: I0cda99822eed015131f8f068512842a388f9c25c

--- power/aidl/vts/VtsHalPowerTargetTest.cpp
+++ power/aidl/vts/VtsHalPowerTargetTest.cpp
@@ -17,8 +17,10 @@
 #include <aidl/Vintf.h>
 
 #include <android/hardware/power/Boost.h>
+#include <android/hardware/power/Hint.h>
 #include <android/hardware/power/IPower.h>
 #include <android/hardware/power/Mode.h>
+#include <android/hardware/power/ThreadHint.h>
 #include <binder/IServiceManager.h>
 #include <binder/ProcessState.h>
 
@@ -29,14 +31,19 @@ using android::sp;
 using android::String16;
 using android::binder::Status;
 using android::hardware::power::Boost;
+using android::hardware::power::Hint;
 using android::hardware::power::IPower;
 using android::hardware::power::Mode;
+using android::hardware::power::ThreadHint;
 
 const std::vector<Boost> kBoosts{android::enum_range<Boost>().begin(),
                                  android::enum_range<Boost>().end()};
 
 const std::vector<Mode> kModes{android::enum_range<Mode>().begin(),
                                android::enum_range<Mode>().end()};
+
+const std::vector<Hint> kHints{android::enum_range<Hint>().begin(),
+                               android::enum_range<Hint>().end()};
 
 const std::vector<Boost> kInvalidBoosts = {
         static_cast<Boost>(static_cast<int32_t>(kBoosts.front()) - 1),
@@ -46,6 +53,11 @@ const std::vector<Boost> kInvalidBoosts = {
 const std::vector<Mode> kInvalidModes = {
         static_cast<Mode>(static_cast<int32_t>(kModes.front()) - 1),
         static_cast<Mode>(static_cast<int32_t>(kModes.back()) + 1),
+};
+
+const std::vector<Hint> kInvalidHints = {
+        static_cast<Hint>(static_cast<int32_t>(kHints.front()) - 1),
+        static_cast<Hint>(static_cast<int32_t>(kHints.back()) + 1),
 };
 
 class PowerAidl : public testing::TestWithParam<std::string> {
@@ -77,7 +89,7 @@ TEST_P(PowerAidl, isModeSupported) {
     for (const auto& mode : kInvalidModes) {
         bool supported;
         ASSERT_TRUE(power->isModeSupported(mode, &supported).isOk());
-        // Should return false for values outsides enum
+        // Should return false for values outside enum
         ASSERT_FALSE(supported);
     }
 }
@@ -103,9 +115,59 @@ TEST_P(PowerAidl, isBoostSupported) {
     for (const auto& boost : kInvalidBoosts) {
         bool supported;
         ASSERT_TRUE(power->isBoostSupported(boost, &supported).isOk());
-        // Should return false for values outsides enum
+        // Should return false for values outside enum
         ASSERT_FALSE(supported);
     }
+}
+
+TEST_P(PowerAidl, fixedPerformanceSupportsAllOrNone) {
+    bool supportsMaxSustainable;
+    const auto result = power->isModeSupported(Mode::FIXED_PERFORMANCE_MAXIMUM_SUSTAINABLE,
+                                               &supportsMaxSustainable);
+    ASSERT_TRUE(result.isOk());
+
+    // If one FIXED_PERFORMANCE mode is supported, both should be.
+    bool supported;
+    ASSERT_TRUE(power->isModeSupported(Mode::FIXED_PERFORMANCE_MINIMUM, &supported).isOk());
+    ASSERT_EQ(supported, supportsMaxSustainable);
+}
+
+TEST_P(PowerAidl, isHintSupported) {
+    for (const auto& hint : kHints) {
+        bool supported;
+        ASSERT_TRUE(power->isHintSupported(hint, &supported).isOk());
+    }
+    for (const auto& hint : kInvalidHints) {
+        bool supported;
+        ASSERT_TRUE(power->isHintSupported(hint, &supported).isOk());
+        // Should return false for values outside enum
+        ASSERT_FALSE(supported);
+    }
+}
+
+TEST_P(PowerAidl, setWorkloadPeriod) {
+    const pid_t tid = gettid();
+    ASSERT_TRUE(power->setWorkloadPeriod(tid, 10000).isOk());
+    ASSERT_TRUE(power->setWorkloadPeriod(tid, 33333).isOk());
+}
+
+TEST_P(PowerAidl, notifyLoadChanged) {
+    const pid_t tid = gettid();
+    ASSERT_TRUE(power->notifyLoadChanged(tid, 0.5f, 0.5f).isOk());
+    ASSERT_TRUE(power->notifyLoadChanged(tid, 0.5f, 2.0f).isOk());
+    ASSERT_TRUE(power->notifyLoadChanged(tid, 2.0f, 0.5f).isOk());
+    ASSERT_TRUE(power->notifyLoadChanged(tid, 2.0f, 2.0f).isOk());
+}
+
+TEST_P(PowerAidl, setThreadHint) {
+    const pid_t tid = gettid();
+    ASSERT_TRUE(power->setThreadHint(tid, ThreadHint::NONE).isOk());
+    ASSERT_TRUE(power->setThreadHint(tid, ThreadHint::HIGH_UTILIZATION).isOk());
+    ASSERT_TRUE(power->setThreadHint(tid, ThreadHint::LOW_LATENCY).isOk());
+    const int32_t bothHints = static_cast<int32_t>(ThreadHint::HIGH_UTILIZATION) |
+                              static_cast<int32_t>(ThreadHint::LOW_LATENCY);
+    ASSERT_TRUE(power->setThreadHint(tid, static_cast<ThreadHint>(bothHints)).isOk());
+    ASSERT_TRUE(power->setThreadHint(tid, ThreadHint::NONE).isOk());
 }
 
 INSTANTIATE_TEST_SUITE_P(Power, PowerAidl,
