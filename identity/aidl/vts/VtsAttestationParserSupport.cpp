328d0098991d82a5405effc3e04f37d9c60cd488
Selene Huang <seleneh@google.com>
2020-03-11 04:37:48 -0700

    Add vts level parsing and unit testing for identity credential attestation certificate.
    Test: VtsHalIdentityTargetTest
    Change-Id: Ibe770e6eaf0b0018d60876926d824204e4eaf732

---
+++ identity/aidl/vts/VtsAttestationParserSupport.cpp
@@ -0,0 +1,243 @@
+/*
+ * Copyright 2019, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "VtsAttestationParserSupport.h"
+
+#include <aidl/Gtest.h>
+#include <map>
+
+namespace android::hardware::identity::test_utils {
+
+using std::endl;
+using std::map;
+using std::optional;
+using std::string;
+using std::vector;
+
+using ::android::sp;
+using ::android::String16;
+using ::android::binder::Status;
+
+using ::keymaster::ASN1_OBJECT_Ptr;
+using ::keymaster::AuthorizationSet;
+using ::keymaster::EVP_PKEY_Ptr;
+using ::keymaster::kAttestionRecordOid;
+using ::keymaster::TAG_ATTESTATION_APPLICATION_ID;
+using ::keymaster::TAG_IDENTITY_CREDENTIAL_KEY;
+using ::keymaster::TAG_INCLUDE_UNIQUE_ID;
+using ::keymaster::X509_Ptr;
+
+using support::certificateChainSplit;
+
+optional<keymaster_cert_chain_t> certificateVectorToKeymasterChain(
+        const vector<vector<uint8_t>>& certificate) {
+    if (certificate.size() <= 0) {
+        return {};
+    }
+
+    keymaster_cert_chain_t cert;
+    cert.entry_count = certificate.size();
+    cert.entries = (keymaster_blob_t*)malloc(sizeof(keymaster_blob_t) * cert.entry_count);
+
+    int index = 0;
+    for (auto c : certificate) {
+        cert.entries[index].data_length = c.size();
+        uint8_t* data = (uint8_t*)malloc(c.size());
+
+        memcpy(data, c.data(), c.size());
+        cert.entries[index].data = (const uint8_t*)data;
+        index++;
+    }
+
+    return cert;
+}
+
+optional<keymaster_cert_chain_t> convertAttestationVectorToChain(
+        const vector<uint8_t>& certificateChain) {
+    optional<vector<vector<uint8_t>>> chain = certificateChainSplit(certificateChain);
+    if (!chain) {
+        return {};
+    }
+
+    return certificateVectorToKeymasterChain(chain.value());
+}
+
+optional<keymaster_cert_chain_t> AttestationCertificateParser::certificateChainToKeymasterChain(
+        const vector<Certificate>& certificates) {
+    if (certificates.size() <= 0) {
+        return {};
+    }
+
+    keymaster_cert_chain_t kCert;
+    kCert.entry_count = certificates.size();
+    kCert.entries = (keymaster_blob_t*)malloc(sizeof(keymaster_blob_t) * kCert.entry_count);
+
+    int index = 0;
+    for (auto c : certificates) {
+        kCert.entries[index].data_length = c.encodedCertificate.size();
+        uint8_t* data = (uint8_t*)malloc(c.encodedCertificate.size());
+
+        memcpy(data, c.encodedCertificate.data(), c.encodedCertificate.size());
+        kCert.entries[index].data = (const uint8_t*)data;
+        index++;
+    }
+
+    return kCert;
+}
+
+bool AttestationCertificateParser::ParseCertificate() {
+    optional<keymaster_cert_chain_t> cert_chain =
+            certificateChainToKeymasterChain(originalCertificate_);
+    if (!cert_chain) {
+        return false;
+    }
+
+    if ((cert_chain.value().entry_count != 3) && (cert_chain.value().entry_count != 4)) {
+        return false;
+    }
+
+    if (!verify_chain(cert_chain.value())) {
+        return false;
+    }
+
+    if (!verify_attestation_record(cert_chain.value().entries[0])) {
+        return false;
+    }
+
+    keymaster_free_cert_chain(&cert_chain.value());
+    return true;
+}
+
+ASN1_OCTET_STRING* AttestationCertificateParser::get_attestation_record(X509* certificate) {
+    ASN1_OBJECT_Ptr oid(OBJ_txt2obj(kAttestionRecordOid, 1));
+    if (!oid.get()) return nullptr;
+
+    int location = X509_get_ext_by_OBJ(certificate, oid.get(), -1);
+    if (location == -1) return nullptr;
+
+    X509_EXTENSION* attest_rec_ext = X509_get_ext(certificate, location);
+    if (!attest_rec_ext) return nullptr;
+
+    ASN1_OCTET_STRING* attest_rec = X509_EXTENSION_get_data(attest_rec_ext);
+    return attest_rec;
+}
+
+X509* AttestationCertificateParser::parse_cert_blob(const keymaster_blob_t& blob) {
+    const uint8_t* p = blob.data;
+    return d2i_X509(nullptr, &p, blob.data_length);
+}
+
+bool AttestationCertificateParser::verify_attestation_record(
+        const keymaster_blob_t& attestation_cert) {
+    X509_Ptr cert(parse_cert_blob(attestation_cert));
+    if (!cert.get()) {
+        return false;
+    }
+
+    ASN1_OCTET_STRING* attest_rec = get_attestation_record(cert.get());
+    if (!attest_rec) {
+        return false;
+    }
+
+    keymaster_blob_t att_unique_id = {};
+    keymaster_error_t ret = parse_attestation_record(
+            attest_rec->data, attest_rec->length, &att_attestation_version_,
+            &att_attestation_security_level_, &att_keymaster_version_,
+            &att_keymaster_security_level_, &att_challenge_, &att_sw_enforced_, &att_hw_enforced_,
+            &att_unique_id);
+    if (ret) {
+        return false;
+    }
+
+    return true;
+}
+
+uint32_t AttestationCertificateParser::GetIdentityCredentialVersion() {
+    return att_keymaster_version_;
+}
+
+uint32_t AttestationCertificateParser::GetAttestationVersion() {
+    return att_attestation_version_;
+}
+
+string AttestationCertificateParser::GetApplicationId() {
+    keymaster_blob_t id = {nullptr, 0};
+    if (!att_sw_enforced_.GetTagValue(TAG_ATTESTATION_APPLICATION_ID, &id)) {
+        return "";
+    }
+
+    string idString(id.data, id.data + id.data_length);
+    return idString;
+}
+
+bool AttestationCertificateParser::IncludeUniqueId() {
+    if (att_hw_enforced_.GetTagValue(TAG_INCLUDE_UNIQUE_ID)) {
+        return true;
+    }
+
+    return false;
+}
+
+bool AttestationCertificateParser::IncludeIdentityCredentialKey() {
+    if (att_hw_enforced_.GetTagValue(TAG_IDENTITY_CREDENTIAL_KEY)) {
+        return true;
+    }
+
+    return false;
+}
+
+keymaster_blob_t AttestationCertificateParser::GetChallenge() {
+    return att_challenge_;
+}
+
+keymaster_security_level_t AttestationCertificateParser::GetKeymasterSecurityLevel() {
+    return att_keymaster_security_level_;
+}
+
+keymaster_security_level_t AttestationCertificateParser::GetAttestationSecurityLevel() {
+    return att_attestation_security_level_;
+}
+
+bool AttestationCertificateParser::verify_chain(const keymaster_cert_chain_t& chain) {
+    for (size_t i = 0; i < chain.entry_count - 1; ++i) {
+        keymaster_blob_t& key_cert_blob = chain.entries[i];
+        keymaster_blob_t& signing_cert_blob = chain.entries[i + 1];
+
+        X509_Ptr key_cert(parse_cert_blob(key_cert_blob));
+        X509_Ptr signing_cert(parse_cert_blob(signing_cert_blob));
+        if (!key_cert.get() || !signing_cert.get()) {
+            return false;
+        }
+
+        EVP_PKEY_Ptr signing_pubkey(X509_get_pubkey(signing_cert.get()));
+        if (!signing_pubkey.get()) return false;
+
+        if (X509_verify(key_cert.get(), signing_pubkey.get()) != 1) {
+            return false;
+        }
+
+        if (i + 1 == chain.entry_count - 1) {
+            // Last entry is self-signed.
+            if (X509_verify(signing_cert.get(), signing_pubkey.get()) != 1) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+}  // namespace android::hardware::identity::test_utils
---
+++ identity/aidl/vts/VtsAttestationUnitTests.cpp
@@ -0,0 +1,207 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "VtsIWritableVtsAttestationUnitTests"
+
+#include <aidl/Gtest.h>
+#include <aidl/Vintf.h>
+#include <android-base/logging.h>
+#include <android/hardware/identity/IIdentityCredentialStore.h>
+#include <android/hardware/identity/support/IdentityCredentialSupport.h>
+#include <binder/IServiceManager.h>
+#include <binder/ProcessState.h>
+#include <cppbor.h>
+#include <cppbor_parse.h>
+#include <gtest/gtest.h>
+#include <future>
+#include <map>
+
+#include "VtsAttestationParserSupport.h"
+#include "VtsIdentityTestUtils.h"
+
+namespace android::hardware::identity {
+
+using std::endl;
+using std::map;
+using std::optional;
+using std::string;
+using std::vector;
+
+using ::android::sp;
+using ::android::String16;
+using ::android::binder::Status;
+
+using test_utils::AttestationCertificateParser;
+using test_utils::SetupWritableCredential;
+
+// This file verifies the Identity Credential VTS Attestation Certificate
+// generated.
+class VtsAttestationUnitTests : public testing::TestWithParam<std::string> {
+  public:
+    virtual void SetUp() override {
+        credentialStore_ = android::waitForDeclaredService<IIdentityCredentialStore>(
+                String16(GetParam().c_str()));
+        ASSERT_NE(credentialStore_, nullptr);
+    }
+
+    bool ValidateAttestationCertificate(const vector<Certificate>& inputCertificates,
+                                        const vector<uint8_t>& expected_challenge,
+                                        vector<uint8_t>& expected_app_id) {
+        AttestationCertificateParser certParser_(inputCertificates);
+        bool ret = certParser_.ParseCertificate();
+        EXPECT_TRUE(ret);
+        if (!ret) {
+            return false;
+        }
+
+        // As per the IC HAL, the version of the Identity
+        // Credential HAL is 1.0 - and this is encoded as major*10 + minor. This field is used by
+        // Keymaster which is known to report integers less than or equal to 4 (for KM up to 4.0)
+        // and integers greater or equal than 41 (for KM starting with 4.1).
+        //
+        // Since we won't get to version 4.0 of the IC HAL for a while, let's also check that a KM
+        // version isn't errornously returned.
+        EXPECT_LE(10, certParser_.GetIdentityCredentialVersion());
+        EXPECT_GT(40, certParser_.GetIdentityCredentialVersion());
+        EXPECT_LE(3, certParser_.GetAttestationVersion());
+
+        // Verify the app id matches to whatever we set it to be.
+        EXPECT_EQ(expected_app_id.size(), certParser_.GetApplicationId().size());
+        EXPECT_EQ(0, memcmp(expected_app_id.data(), certParser_.GetApplicationId().data(),
+                            expected_app_id.size()));
+
+        EXPECT_FALSE(certParser_.IncludeUniqueId());
+        EXPECT_TRUE(certParser_.IncludeIdentityCredentialKey());
+
+        // Verify the challenge always matches in size and data of what is passed
+        // in.
+        keymaster_blob_t att_challenge = certParser_.GetChallenge();
+        EXPECT_EQ(expected_challenge.size(), att_challenge.data_length);
+        EXPECT_EQ(0,
+                  memcmp(expected_challenge.data(), att_challenge.data, expected_challenge.size()));
+
+        // The level is a bit tricky.  It may be different for each hardware.  May
+        // need to delete this, unless there is a correlation with the version
+        // number.
+        EXPECT_LE(KM_SECURITY_LEVEL_SOFTWARE, certParser_.GetKeymasterSecurityLevel());
+        EXPECT_LE(KM_SECURITY_LEVEL_SOFTWARE, certParser_.GetAttestationSecurityLevel());
+        return true;
+    }
+
+    sp<IIdentityCredentialStore> credentialStore_;
+};
+
+TEST_P(VtsAttestationUnitTests, verifyAttestationWithEmptyChallengeEmptyId) {
+    Status result;
+    sp<IWritableIdentityCredential> writableCredential;
+    ASSERT_TRUE(test_utils::SetupWritableCredential(writableCredential, credentialStore_));
+
+    vector<uint8_t> attestationChallenge;
+    vector<Certificate> attestationCertificate;
+    vector<uint8_t> attestationApplicationId = {};
+    result = writableCredential->getAttestationCertificate(
+            attestationApplicationId, attestationChallenge, &attestationCertificate);
+
+    EXPECT_TRUE(result.isOk()) << result.exceptionCode() << "; " << result.exceptionMessage()
+                               << endl;
+    EXPECT_TRUE(ValidateAttestationCertificate(attestationCertificate, attestationChallenge,
+                                               attestationApplicationId));
+}
+
+TEST_P(VtsAttestationUnitTests, verifyAttestationWithEmptyChallengeNonemptyId) {
+    Status result;
+    sp<IWritableIdentityCredential> writableCredential;
+    ASSERT_TRUE(SetupWritableCredential(writableCredential, credentialStore_));
+
+    vector<uint8_t> attestationChallenge;
+    vector<Certificate> attestationCertificate;
+    string applicationId = "Attestation Verification";
+    vector<uint8_t> attestationApplicationId = {applicationId.begin(), applicationId.end()};
+
+    result = writableCredential->getAttestationCertificate(
+            attestationApplicationId, attestationChallenge, &attestationCertificate);
+
+    EXPECT_TRUE(result.isOk()) << result.exceptionCode() << "; " << result.exceptionMessage()
+                               << endl;
+    EXPECT_TRUE(ValidateAttestationCertificate(attestationCertificate, attestationChallenge,
+                                               attestationApplicationId));
+}
+
+TEST_P(VtsAttestationUnitTests, verifyAttestationWithNonemptyChallengeEmptyId) {
+    Status result;
+    sp<IWritableIdentityCredential> writableCredential;
+    ASSERT_TRUE(SetupWritableCredential(writableCredential, credentialStore_));
+
+    string challenge = "NotSoRandomChallenge";
+    vector<uint8_t> attestationChallenge(challenge.begin(), challenge.end());
+    vector<Certificate> attestationCertificate;
+    vector<uint8_t> attestationApplicationId = {};
+
+    result = writableCredential->getAttestationCertificate(
+            attestationApplicationId, attestationChallenge, &attestationCertificate);
+
+    EXPECT_TRUE(result.isOk()) << result.exceptionCode() << "; " << result.exceptionMessage()
+                               << endl;
+    EXPECT_TRUE(ValidateAttestationCertificate(attestationCertificate, attestationChallenge,
+                                               attestationApplicationId));
+}
+
+TEST_P(VtsAttestationUnitTests, verifyAttestationWithNonemptyChallengeNonemptyId) {
+    Status result;
+    sp<IWritableIdentityCredential> writableCredential;
+    ASSERT_TRUE(SetupWritableCredential(writableCredential, credentialStore_));
+
+    string challenge = "NotSoRandomChallenge1NotSoRandomChallenge1NotSoRandomChallenge1";
+    vector<uint8_t> attestationChallenge(challenge.begin(), challenge.end());
+    vector<Certificate> attestationCertificate;
+    string applicationId = "Attestation Verification";
+    vector<uint8_t> attestationApplicationId = {applicationId.begin(), applicationId.end()};
+
+    result = writableCredential->getAttestationCertificate(
+            attestationApplicationId, attestationChallenge, &attestationCertificate);
+
+    EXPECT_TRUE(result.isOk()) << result.exceptionCode() << "; " << result.exceptionMessage()
+                               << endl;
+    EXPECT_TRUE(ValidateAttestationCertificate(attestationCertificate, attestationChallenge,
+                                               attestationApplicationId));
+}
+
+TEST_P(VtsAttestationUnitTests, verifyAttestationWithVeryShortChallengeAndId) {
+    Status result;
+    sp<IWritableIdentityCredential> writableCredential;
+    ASSERT_TRUE(SetupWritableCredential(writableCredential, credentialStore_));
+
+    string challenge = "c";
+    vector<uint8_t> attestationChallenge(challenge.begin(), challenge.end());
+    vector<Certificate> attestationCertificate;
+    string applicationId = "i";
+    vector<uint8_t> attestationApplicationId = {applicationId.begin(), applicationId.end()};
+
+    result = writableCredential->getAttestationCertificate(
+            attestationApplicationId, attestationChallenge, &attestationCertificate);
+
+    EXPECT_TRUE(result.isOk()) << result.exceptionCode() << "; " << result.exceptionMessage()
+                               << endl;
+    EXPECT_TRUE(ValidateAttestationCertificate(attestationCertificate, attestationChallenge,
+                                               attestationApplicationId));
+}
+
+INSTANTIATE_TEST_SUITE_P(
+        Identity, VtsAttestationUnitTests,
+        testing::ValuesIn(android::getAidlHalInstanceNames(IIdentityCredentialStore::descriptor)),
+        android::PrintInstanceNameToString);
+
+}  // namespace android::hardware::identity
