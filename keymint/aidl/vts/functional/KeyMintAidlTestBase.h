41d8b84652bd277cbf9ce6df79e46ea66e4b95f6
Selene Huang <seleneh@google.com>
2020-08-26 23:38:09 -0700

    Implemente Key attestation and vts tests.   - Add key attestation certificate generation to generateKey and importKey.   - Add self signed certificate implementation when no challenge is   provided to asymetric signing key.   - Add fake certificate generation when no challenge is provided to   asymetric encryption key.   - Add ways for subject and serial of attestation to be set by caller.
    Test: atest VtsHalKeyMintV1_0TargetTest
    Change-Id: If290187d25fafa24cffae09c6f4e115392a82805

--- keymint/aidl/vts/functional/KeyMintAidlTestBase.h
+++ keymint/aidl/vts/functional/KeyMintAidlTestBase.h
@@ -60,14 +60,28 @@ class KeyMintAidlTestBase : public ::testing::TestWithParam<string> {
     uint32_t os_patch_level() { return os_patch_level_; }
 
     ErrorCode GetReturnErrorCode(Status result);
+
+    ErrorCode GenerateKey(const AuthorizationSet& key_desc, const AuthorizationSet& attest_params,
+                          vector<uint8_t>* key_blob, KeyCharacteristics* key_characteristics);
+
     ErrorCode GenerateKey(const AuthorizationSet& key_desc, vector<uint8_t>* key_blob,
                           KeyCharacteristics* key_characteristics);
 
+    ErrorCode GenerateKey(const AuthorizationSet& key_desc, const AuthorizationSet& attest_params);
+
     ErrorCode GenerateKey(const AuthorizationSet& key_desc);
+
+    ErrorCode ImportKey(const AuthorizationSet& key_desc, const AuthorizationSet& attest_params,
+                        KeyFormat format, const string& key_material, vector<uint8_t>* key_blob,
+                        KeyCharacteristics* key_characteristics);
 
     ErrorCode ImportKey(const AuthorizationSet& key_desc, KeyFormat format,
                         const string& key_material, vector<uint8_t>* key_blob,
                         KeyCharacteristics* key_characteristics);
+
+    ErrorCode ImportKey(const AuthorizationSet& key_desc, const AuthorizationSet& attest_params,
+                        KeyFormat format, const string& key_material);
+
     ErrorCode ImportKey(const AuthorizationSet& key_desc, KeyFormat format,
                         const string& key_material);
 
