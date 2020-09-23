57677ce789720a4860aa9d13444b4786b72c02b1
Roshan Pius <rpius@google.com>
2020-09-23 15:19:30 -0700

    supplicant(vts): Stop adding empty instance for vendor HAL
    We don't need to test supplicant in the presence of vendor HAL & without
    it. For a given device, this behavior is fixed.
    
    Bug: 161951052
    Test: atest VtsHalWifiSupplicantV1_1TargetTest
    Change-Id: I71097c3afef1765ca514abf2c350c423e3999020
    (cherry picked from commit 984b6e0607a8e4e2dbfbc11e6a8666ce16491c67)

--- wifi/supplicant/1.1/vts/functional/supplicant_hidl_test.cpp
+++ wifi/supplicant/1.1/vts/functional/supplicant_hidl_test.cpp
@@ -33,6 +33,7 @@ using ::android::hardware::wifi::supplicant::V1_0::SupplicantStatus;
 using ::android::hardware::wifi::supplicant::V1_0::SupplicantStatusCode;
 using ::android::hardware::wifi::supplicant::V1_0::IfaceType;
 using ::android::hardware::wifi::supplicant::V1_1::ISupplicant;
+using ::android::hardware::wifi::V1_0::IWifi;
 using ::android::sp;
 
 class SupplicantHidlTest : public SupplicantHidlTestBase {
@@ -139,6 +140,7 @@ TEST_P(SupplicantHidlTest, RemoveP2pInterface) {
  */
 TEST_P(SupplicantHidlTest, Terminate) { supplicant_->terminate(); }
 
+<<<<<<< HEAD   (77c3b5 Merge "vts: Use global hostapd_ since teardown will call ter)
 static std::vector<std::string> get_wifi_instances() {
     std::vector<std::string> instances =
         android::hardware::getAllHalInstanceNames(
@@ -150,10 +152,13 @@ static std::vector<std::string> get_wifi_instances() {
 }
 
 GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(SupplicantHidlTest);
+=======
+>>>>>>> CHANGE (984b6e supplicant(vts): Stop adding empty instance for vendor HAL)
 INSTANTIATE_TEST_CASE_P(
     PerInstance, SupplicantHidlTest,
     testing::Combine(
-        testing::ValuesIn(get_wifi_instances()),
+        testing::ValuesIn(
+            android::hardware::getAllHalInstanceNames(IWifi::descriptor)),
         testing::ValuesIn(android::hardware::getAllHalInstanceNames(
             android::hardware::wifi::supplicant::V1_1::ISupplicant::
                 descriptor))),
