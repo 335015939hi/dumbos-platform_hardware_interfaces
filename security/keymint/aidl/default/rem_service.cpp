0cfe812a50edb31c209cf41cbeffe07981e779f6
Shawn Willden <swillden@google.com>
2020-09-30 22:39:22 -0600

    Add RemotelyProvisionedComponent HAL.
    Test: VtsHalRemotelyProvisionedComponentTargetTest
    Change-Id: I51fb01f4c52949c81f3ad2d694a4afdf0fa67788

---
+++ security/keymint/aidl/default/rem_service.cpp
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "android.hardware.keymint-provisioner-service"
+
+#include <android-base/logging.h>
+#include <android/binder_manager.h>
+#include <android/binder_process.h>
+
+#include "RemotelyProvisionedComponent.h"
+
+using ::android::base::InitLogging;
+using ::android::base::StderrLogger;
+
+using aidl::android::hardware::security::keymint::RemotelyProvisionedComponent;
+using aidl::android::hardware::security::keymint::SecurityLevel;
+
+int main(int /* argc */, char* argv[]) {
+    InitLogging(argv, StderrLogger);
+
+    ABinderProcess_setThreadPoolMaxThreadCount(0 /* This thread will be added to get count 1 */);
+    /*
+    std::shared_ptr<RemotelyProvisionedComponent> component =
+        ndk::SharedRefBase::make<RemotelyProvisionedComponent>(SecurityLevel::SOFTWARE);
+
+    const std::string instance =
+            std::string() + RemotelyProvisionedComponent::descriptor + "/default";
+    LOG(INFO) << "instance: " << instance;
+
+    binder_status_t status =
+            AServiceManager_addService(component->asBinder().get(), instance.c_str());
+    CHECK(status == STATUS_OK);
+
+    ABinderProcess_joinThreadPool();*/
+    return EXIT_FAILURE;  // should not reach
+}
