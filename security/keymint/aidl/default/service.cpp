f99cd846095ab4e5d8b5ba33d8945616f59d247f
Shawn Willden <swillden@google.com>
2020-09-30 22:39:22 -0600

    Add RemotelyProvisionedComponent HAL.
    Test: VtsHalRemotelyProvisionedComponentTargetTest
    Change-Id: I51fb01f4c52949c81f3ad2d694a4afdf0fa67788

--- security/keymint/aidl/default/service.cpp
+++ security/keymint/aidl/default/service.cpp
@@ -21,15 +21,34 @@
 #include <android/binder_process.h>
 
 #include <AndroidKeyMintDevice.h>
+#include <keymaster/android_keymaster.h>
+#include <keymaster/contexts/pure_soft_keymaster_context.h>
+#include <keymaster/keymaster_configuration.h>
 #include <keymaster/soft_keymaster_logger.h>
 
+#include "RemotelyProvisionedComponent.h"
+
 using aidl::android::hardware::security::keymint::AndroidKeyMintDevice;
+using aidl::android::hardware::security::keymint::RemotelyProvisionedComponent;
 using aidl::android::hardware::security::keymint::SecurityLevel;
+using namespace keymaster;
 
 int main() {
     // Zero threads seems like a useless pool, but below we'll join this thread to it, increasing
     // the pool size to 1.
     ABinderProcess_setThreadPoolMaxThreadCount(0);
+    ::keymaster::AndroidKeymaster* sharedKm = new ::keymaster::AndroidKeymaster(
+            [&]() -> auto {
+                auto context = new PureSoftKeymasterContext(
+                        KmVersion::KEYMINT_1,
+                        static_cast<keymaster_security_level_t>(SecurityLevel::SOFTWARE));
+                context->SetSystemVersion(::keymaster::GetOsVersion(),
+                                          ::keymaster::GetOsPatchlevel());
+                return context;
+            }(),
+            16 /* operationTableSize */);
+
+    /** Add KeyMint to the ServiceManager **/
     std::shared_ptr<AndroidKeyMintDevice> keyMint =
             ndk::SharedRefBase::make<AndroidKeyMintDevice>(SecurityLevel::SOFTWARE);
 
@@ -38,6 +57,18 @@ int main() {
     LOG(INFO) << "instance: " << instanceName;
     binder_status_t status =
             AServiceManager_addService(keyMint->asBinder().get(), instanceName.c_str());
+    CHECK(status == STATUS_OK);
+
+    /** Add RemotelyProvisionedComponent to the ServiceManager **/
+    std::shared_ptr<RemotelyProvisionedComponent> component =
+            ndk::SharedRefBase::make<RemotelyProvisionedComponent>(
+                    std::shared_ptr<::keymaster::AndroidKeymaster>(sharedKm));
+
+    const std::string instance =
+            std::string() + RemotelyProvisionedComponent::descriptor + "/default";
+    LOG(INFO) << "instance: " << instance;
+
+    status = AServiceManager_addService(component->asBinder().get(), instance.c_str());
     CHECK(status == STATUS_OK);
 
     ABinderProcess_joinThreadPool();
