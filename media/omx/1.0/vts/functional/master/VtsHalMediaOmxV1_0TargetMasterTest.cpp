7239b37d204227df649d8bd1d0ca846ecc6d577a
Lajos Molnar <lajos@google.com>
2020-11-04 11:50:41 -0800

    omx: validate only standard OMX roles on GSI
    Test uses ::android::GetComponentRole to get OMX role for media type
    that only supports standard types on GSI.
    
    Bug: 164550113
    Change-Id: Ia9e64c8d887518f6c7d8c650a942bad7ce3457f0

--- media/omx/1.0/vts/functional/master/VtsHalMediaOmxV1_0TargetMasterTest.cpp
+++ media/omx/1.0/vts/functional/master/VtsHalMediaOmxV1_0TargetMasterTest.cpp
@@ -21,6 +21,7 @@
 
 #include <android-base/logging.h>
 #include <android-base/strings.h>
+#include <android-base/properties.h>
 
 #include <android/hardware/media/omx/1.0/IOmx.h>
 #include <android/hardware/media/omx/1.0/IOmxNode.h>
@@ -257,6 +258,12 @@ TEST_P(MasterHidlTest, ListRoles) {
     std::set<const std::string> roleKeys;
     std::map<const std::string, std::set<const std::string>> nodeToRoles;
     std::map<const std::string, std::set<const std::string>> ownerToNodes;
+    std::string product =
+        ::android::base::GetProperty(
+                 "ro.product.system_ext.name",
+                 ::android::base::GetProperty("ro.product.system.name", ""));
+    bool isGsi = product.rfind("aosp_", 0) == 0 || product.rfind("gsi_", 0) == 0;
+
     for (const IOmxStore::RoleInfo& role : roleList) {
         // Make sure there are no duplicates
         const auto [roleIter, inserted] = roleKeys.insert(role.role);
@@ -264,11 +271,15 @@ TEST_P(MasterHidlTest, ListRoles) {
 
         // Make sure role name follows expected format based on type and
         // isEncoder
-        const std::string role_name(
-                ::android::GetComponentRole(role.isEncoder, role.type.c_str()));
-        EXPECT_EQ(role_name, role.role) << "Role \"" << role.role << "\" does not match "
+        const char *role_name =
+            ::android::GetComponentRole(role.isEncoder, role.type.c_str());
+        if (role_name != nullptr) {
+            EXPECT_EQ(std::string(role_name), role.role) << "Role \"" << role.role << "\" does not match "
                                         << (role.isEncoder ? "an encoder " : "a decoder ")
-                                        << "for mime type \"" << role.type << ".";
+                                        << "for media type \"" << role.type << ".";
+        } else {
+            EXPECT_TRUE(isGsi) << "Media type \"" << role.type << "\" does not resolve to an OMX role.";
+        }
 
         // Check the nodes for this role
         std::set<const std::string> nodeKeys;
