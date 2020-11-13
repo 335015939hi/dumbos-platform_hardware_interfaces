62ebf746325ee3310d7949bb50d12afc710550e5
Hui Wang <huiwang@google.com>
2020-11-13 04:30:18 +0000

    Add VoNR network feature support information
    Define data structures for VoNR network feature support information,
    and update RegStateResult to allow the information to be reported
    through the response of getVoiceRegistrationStateResponse and
    getDataRegistrationStateResponse.
    
    Bug: 167991878
    Test: make, VTS
    Change-Id: I098bd1b6c86cd38be6fbc086b8ebd7b1bb2408fa

--- radio/1.6/vts/functional/radio_response.cpp
+++ radio/1.6/vts/functional/radio_response.cpp
@@ -1156,3 +1156,15 @@ Return<void> RadioResponse_v1_6::setAllowedNetworkTypeBitmapResponse(
     parent_v1_6.notify(info.serial);
     return Void();
 }
+
+Return<void> RadioResponse_v1_6::getVoiceRegistrationStateResponse_1_6(
+        const ::android::hardware::radio::V1_0::RadioResponseInfo& /*info*/,
+        const ::android::hardware::radio::V1_6::RegStateResult& /*regResponse*/) {
+    return Void();
+}
+
+Return<void> RadioResponse_v1_6::getDataRegistrationStateResponse_1_6(
+        const ::android::hardware::radio::V1_0::RadioResponseInfo& /*info*/,
+        const ::android::hardware::radio::V1_6::RegStateResult& /*regResponse*/) {
+    return Void();
+}
