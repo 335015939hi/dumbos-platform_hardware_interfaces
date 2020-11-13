e913de3e0e9669ab47634f4c9adbcba95ef08f94
Grzegorz Kołodziejczyk <grzegorz.kolodziejczyk@codecoup.pl>
2020-11-13 12:34:19 +0000

    Make Bluetooth audio HAL 2.1 and 2.0 audio session controls accessible
    Both audio HAL 2.1 and HAL 2.0 session controls should be available to
    access by stack in runtime. Active audio session control is chosen by manifest.
    
    Tag: #stability
    Test: vts-tradefed run vts -m VtsHalBluetoothAudioV2_1TargetTest
    Sponsor: jpawlowski@
    Change-Id: Ife660c336b1be94fe9e9751ee9c919a193307066

--- bluetooth/audio/2.1/default/session/BluetoothAudioSession.cpp
+++ bluetooth/audio/2.1/default/session/BluetoothAudioSession.cpp
@@ -24,6 +24,7 @@
 namespace android {
 namespace bluetooth {
 namespace audio {
+namespace V2_1 {
 
 using ::android::hardware::audio::common::V5_0::AudioContentType;
 using ::android::hardware::audio::common::V5_0::AudioUsage;
@@ -462,6 +463,7 @@ BluetoothAudioSessionInstance::GetSessionInstance(
   return session_ptr;
 }
 
+}  // namespace V2_1
 }  // namespace audio
 }  // namespace bluetooth
 }  // namespace android
