6621e537b26eafc09457322c62a38e47f81e01f9
Shawn Willden <swillden@google.com>
2020-11-19 15:40:37 -0700

    Update to support keymaster's configurable version.
    Test: VtsHalIdentityTargetTest
    Bug: b/173577355
    Change-Id: Ie7dda2c09b9d0b8e5b5b69ba4056ac40af67c870

--- identity/support/src/IdentityCredentialSupport.cpp
+++ identity/support/src/IdentityCredentialSupport.cpp
@@ -1008,10 +1008,12 @@ optional<vector<vector<uint8_t>>> createAttestation(
     // relying party is ever going to trust our batch key and those keys above
     // it.
     //
-    ::keymaster::PureSoftKeymasterContext context(KM_SECURITY_LEVEL_TRUSTED_ENVIRONMENT);
+    ::keymaster::PureSoftKeymasterContext context(::keymaster::kCurrentKeymasterVersion,
+                                                  KM_SECURITY_LEVEL_TRUSTED_ENVIRONMENT);
 
     error = generate_attestation_from_EVP_with_subject_name(
-            key, swEnforced, hwEnforced, auth_set, context, ::keymaster::kCurrentKeymasterVersion,
+            key, swEnforced, hwEnforced, auth_set, context,
+            ::keymaster::version_to_attestation_version(::keymaster::kCurrentKeymasterVersion),
             *attestation_chain, *attestation_signing_key, "Android Identity Credential Key",
             &cert_chain_out);
 
@@ -2387,7 +2389,6 @@ vector<vector<uint8_t>> chunkVector(const vector<uint8_t>& content, size_t maxCh
 
     return ret;
 }
-
 
 vector<uint8_t> testHardwareBoundKey = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
