18943931233b05c33747b84d85370f13fd3eb4f1
Parth A Baxi <parth.baxi@gm.com>
2021-03-03 15:00:05 -0500

    Fix expected error code value
    Update expected error code to VERIFICATION_FAILED
    = -30 instead of returning UNKNOWN_ERROR
    
    Test:
    VtsHalKeymasterV3_0Target
    1.RsaPkcs1Success
    2.RsaOaepSuccess
    3.RsaOaepDecryptWithWrongDigest
    
    Bug: 166143002
    
    Change-Id: If2a080883b42c1ec931f9b0221f4f228dc298719

--- keymaster/3.0/vts/functional/keymaster_hidl_hal_test.cpp
+++ keymaster/3.0/vts/functional/keymaster_hidl_hal_test.cpp
@@ -2718,7 +2718,7 @@ TEST_F(EncryptionOperationsTest, RsaOaepSuccess) {
 
         EXPECT_EQ(ErrorCode::OK, Begin(KeyPurpose::DECRYPT, params));
         string result;
-        EXPECT_EQ(ErrorCode::UNKNOWN_ERROR, Finish(ciphertext1, &result));
+        EXPECT_EQ(ErrorCode::VERIFICATION_FAILED, Finish(ciphertext1, &result));
         EXPECT_EQ(0U, result.size());
     }
 }
@@ -2763,7 +2763,7 @@ TEST_F(EncryptionOperationsTest, RsaOaepDecryptWithWrongDigest) {
         Begin(KeyPurpose::DECRYPT,
               AuthorizationSetBuilder().Digest(Digest::SHA_2_256).Padding(PaddingMode::RSA_OAEP)));
     string result;
-    EXPECT_EQ(ErrorCode::UNKNOWN_ERROR, Finish(ciphertext, &result));
+    EXPECT_EQ(ErrorCode::VERIFICATION_FAILED, Finish(ciphertext, &result));
     EXPECT_EQ(0U, result.size());
 }
 
@@ -2826,7 +2826,7 @@ TEST_F(EncryptionOperationsTest, RsaPkcs1Success) {
 
     EXPECT_EQ(ErrorCode::OK, Begin(KeyPurpose::DECRYPT, params));
     string result;
-    EXPECT_EQ(ErrorCode::UNKNOWN_ERROR, Finish(ciphertext1, &result));
+    EXPECT_EQ(ErrorCode::VERIFICATION_FAILED, Finish(ciphertext1, &result));
     EXPECT_EQ(0U, result.size());
 }
 
