ae13457be70347e5ea5ee1379dfee6a69ee242b9
Shawn Willden <swillden@google.com>
2021-06-21 08:57:13 -0600

    Delete KM1 & KM2
    Test: Build, to verify that nothing uses this code
    Change-Id: I6c50f2427f05bf83fa29294398ba3cf88e654a65

--- keymaster/3.0/default/KeymasterDevice.cpp
+++ keymaster/3.0/default/KeymasterDevice.cpp
@@ -22,8 +22,6 @@
 #include <log/log.h>
 
 #include <AndroidKeymaster3Device.h>
-#include <hardware/keymaster1.h>
-#include <hardware/keymaster2.h>
 #include <hardware/keymaster_defs.h>
 
 namespace android {
@@ -32,52 +30,8 @@ namespace keymaster {
 namespace V3_0 {
 namespace implementation {
 
-static int get_keymaster1_dev(keymaster1_device_t** dev, const hw_module_t* mod) {
-    int rc = keymaster1_open(mod, dev);
-    if (rc) {
-        ALOGE("Error %d opening keystore keymaster1 device", rc);
-        if (*dev) {
-            (*dev)->common.close(&(*dev)->common);
-            *dev = nullptr;
-        }
-    }
-    return rc;
-}
-
-static int get_keymaster2_dev(keymaster2_device_t** dev, const hw_module_t* mod) {
-    int rc = keymaster2_open(mod, dev);
-    if (rc) {
-        ALOGE("Error %d opening keystore keymaster2 device", rc);
-        *dev = nullptr;
-    }
-    return rc;
-}
-
 static IKeymasterDevice* createKeymaster3Device() {
-    const hw_module_t* mod = nullptr;
-
-    int rc = hw_get_module_by_class(KEYSTORE_HARDWARE_MODULE_ID, NULL, &mod);
-    if (rc) {
-        ALOGI("Could not find any keystore module, using software-only implementation.");
-        // SoftKeymasterDevice will be deleted by keymaster_device_release()
-        return ::keymaster::ng::CreateKeymasterDevice();
-    }
-
-    if (mod->module_api_version < KEYMASTER_MODULE_API_VERSION_1_0) {
-        return nullptr;
-    } else if (mod->module_api_version == KEYMASTER_MODULE_API_VERSION_1_0) {
-        keymaster1_device_t* dev = nullptr;
-        if (get_keymaster1_dev(&dev, mod)) {
-            return nullptr;
-        }
-        return ::keymaster::ng::CreateKeymasterDevice(dev);
-    } else {
-        keymaster2_device_t* dev = nullptr;
-        if (get_keymaster2_dev(&dev, mod)) {
-            return nullptr;
-        }
-        return ::keymaster::ng::CreateKeymasterDevice(dev);
-    }
+    return ::keymaster::ng::CreateKeymasterDevice();
 }
 
 IKeymasterDevice* HIDL_FETCH_IKeymasterDevice(const char* name) {
