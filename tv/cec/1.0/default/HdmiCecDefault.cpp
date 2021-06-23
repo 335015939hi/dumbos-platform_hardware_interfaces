70b301df6255e2df61379245eff8238a73a4ec0e
Shraddha Basantwani <shraddha.basantwani@ittiam.com>
2021-06-23 20:42:08 +0530

    CEC: Add implementation of SYSTEM_CEC_CONTROL option to default HdmiCec
    SYSTEM_CEC_CONTROL is updated when system goes in and out of standby mode.
    When set to true, Android system is handling CEC commands.
    When set to false, microprocessor is handling CEC commands.
    
    Bug: 185434120
    Test: manual
    Change-Id: I0fbb13a65639508634c6c163b5eaed1885f9e127

--- tv/cec/1.0/default/HdmiCecDefault.cpp
+++ tv/cec/1.0/default/HdmiCecDefault.cpp
@@ -40,6 +40,10 @@ namespace implementation {
 using std::find;
 using std::vector;
 
+static const int CEC_KEYCODE_POWER = 0x40;
+static const int CEC_KEYCODE_ROOT_MENU = 0x09;
+static const int CEC_KEYCODE_POWER_ON_FUNCTION = 0x6D;
+
 // When set to false, all the CEC commands are discarded. True by default after initialization.
 bool mCecEnabled;
 /*
@@ -47,6 +51,13 @@ bool mCecEnabled;
  * <Text View On>. True by default after initialization.
  */
 bool mWakeupEnabled;
+/*
+ * Updated when system goes in and out of standby mode.
+ * When set to true, Android system is handling CEC commands.
+ * When set to false, microprocessor is handling CEC commands.
+ * True by default after initialization.
+ */
+bool mCecControlEnabled;
 
 int mCecFd;
 int mExitFd;
@@ -59,6 +70,7 @@ HdmiCecDefault::HdmiCecDefault() {
     mExitFd = -1;
     mCecEnabled = false;
     mWakeupEnabled = false;
+    mCecControlEnabled = false;
     mCallback = nullptr;
 }
 
@@ -259,7 +271,9 @@ Return<void> HdmiCecDefault::setOption(OptionKey key, bool value) {
             LOG(DEBUG) << "setOption: WAKEUP: " << value;
             mWakeupEnabled = value;
             break;
-        default:
+        case OptionKey::SYSTEM_CEC_CONTROL:
+            LOG(DEBUG) << "setOption: SYSTEM_CEC_CONTROL: " << value;
+            mCecControlEnabled = value;
             break;
     }
     return Void();
@@ -333,6 +347,7 @@ Return<Result> HdmiCecDefault::init() {
 
     mCecEnabled = true;
     mWakeupEnabled = true;
+    mCecControlEnabled = true;
     initDeviceType();
     return Result::SUCCESS;
 }
@@ -426,6 +441,11 @@ void* HdmiCecDefault::event_thread(void*) {
                 continue;
             }
 
+            if (!mCecControlEnabled && !isTransferableInSleep(msg)) {
+                LOG(DEBUG) << "Filter message in standby mode";
+                continue;
+            }
+
             if (mCallback != nullptr) {
                 size_t length = std::min(msg.len - 1, (uint32_t)MaxLength::MESSAGE_BODY);
                 CecMessage cecMessage{
@@ -489,6 +509,52 @@ Return<Result> HdmiCecDefault::strToInt(char* string, int* num) {
     }
     *num = (int)lnum;
     return Result::SUCCESS;
+}
+
+bool HdmiCecDefault::isTransferableInSleep(struct cec_msg message) {
+    int opcode = getOpcode(message);
+    switch (opcode) {
+        case CEC_MESSAGE_ABORT:
+        case CEC_MESSAGE_DEVICE_VENDOR_ID:
+        case CEC_MESSAGE_GET_CEC_VERSION:
+        case CEC_MESSAGE_GET_MENU_LANGUAGE:
+        case CEC_MESSAGE_GIVE_DEVICE_POWER_STATUS:
+        case CEC_MESSAGE_GIVE_DEVICE_VENDOR_ID:
+        case CEC_MESSAGE_GIVE_OSD_NAME:
+        case CEC_MESSAGE_GIVE_PHYSICAL_ADDRESS:
+        case CEC_MESSAGE_REPORT_PHYSICAL_ADDRESS:
+        case CEC_MESSAGE_REPORT_POWER_STATUS:
+        case CEC_MESSAGE_SET_OSD_NAME:
+            return true;
+        case CEC_MESSAGE_USER_CONTROL_PRESSED:
+            return isPowerUICommand(message);
+        case CEC_MESSAGE_DECK_CONTROL:
+        case CEC_MESSAGE_PLAY:
+            return hasDeviceType(CEC_DEVICE_PLAYBACK);
+        case CEC_MESSAGE_IMAGE_VIEW_ON:
+        case CEC_MESSAGE_TEXT_VIEW_ON:
+            return hasDeviceType(CEC_DEVICE_TV);
+        case CEC_MESSAGE_SYSTEM_AUDIO_MODE_REQUEST:
+            return hasDeviceType(CEC_DEVICE_AUDIO_SYSTEM);
+        default:
+            return false;
+    }
+}
+
+int HdmiCecDefault::getFirstParam(struct cec_msg message) {
+    return (static_cast<uint8_t>(message.msg[2]) & 0xff);
+}
+
+bool HdmiCecDefault::isPowerUICommand(struct cec_msg message) {
+    int uiCommand = getFirstParam(message);
+    switch (uiCommand) {
+        case CEC_KEYCODE_POWER:
+        case CEC_KEYCODE_ROOT_MENU:
+        case CEC_KEYCODE_POWER_ON_FUNCTION:
+            return true;
+        default:
+            return false;
+    }
 }
 }  // namespace implementation
 }  // namespace V1_0
--- tv/cec/1.0/default/HdmiCecDefault.h
+++ tv/cec/1.0/default/HdmiCecDefault.h
@@ -52,6 +52,9 @@ struct HdmiCecDefault : public IHdmiCec, public hidl_death_recipient {
     static void* event_thread(void*);
     static int getOpcode(struct cec_msg message);
     static bool isWakeupMessage(struct cec_msg message);
+    static bool isTransferableInSleep(struct cec_msg message);
+    static int getFirstParam(struct cec_msg message);
+    static bool isPowerUICommand(struct cec_msg message);
     void initDeviceType();
     static bool hasDeviceType(int deviceType);
     Return<Result> strToInt(char* string, int* num);
