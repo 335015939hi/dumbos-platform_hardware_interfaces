f2eda302b12c444c95823466c4db0020354e4134
David Drysdale <drysdale@google.com>
2021-10-11 12:49:50 +0100

    KeyMint VTS: check INCLUDE_UNIQUE_ID works
    Test: VtsAidlKeyMintTargetTest
    Change-Id: I8bc674b47549aa1133f816c510289774db752e04

--- security/keymint/aidl/vts/functional/KeyMintTest.cpp
+++ security/keymint/aidl/vts/functional/KeyMintTest.cpp
@@ -1552,6 +1552,107 @@ TEST_P(NewKeyGenerationTest, EcdsaAttestationTags) {
 }
 
 /*
+ * NewKeyGenerationTest.EcdsaAttestationUniqueId
+ *
+ * Verifies that creation of an attested ECDSA key with a UNIQUE_ID included.
+ */
+TEST_P(NewKeyGenerationTest, EcdsaAttestationUniqueId) {
+    auto challenge = "hello";
+    auto app_id = "foo";
+    auto subject = "cert subj 2";
+    vector<uint8_t> subject_der(make_name_from_str(subject));
+    uint64_t serial_int = 0x1010;
+    vector<uint8_t> serial_blob(build_serial_blob(serial_int));
+    const AuthorizationSetBuilder base_builder =
+            AuthorizationSetBuilder()
+                    .Authorization(TAG_NO_AUTH_REQUIRED)
+                    .Authorization(TAG_INCLUDE_UNIQUE_ID)
+                    .EcdsaSigningKey(EcCurve::P_256)
+                    .Digest(Digest::NONE)
+                    .AttestationChallenge(challenge)
+                    .Authorization(TAG_CERTIFICATE_SERIAL, serial_blob)
+                    .Authorization(TAG_CERTIFICATE_SUBJECT, subject_der)
+                    .SetDefaultValidity();
+    AuthorizationSetBuilder builder = base_builder;
+    builder.AttestationApplicationId(app_id).Authorization(TAG_CREATION_DATETIME, 1619621648000);
+
+    ASSERT_EQ(ErrorCode::OK, GenerateKey(builder));
+    ASSERT_GT(key_blob_.size(), 0U);
+
+    EXPECT_TRUE(ChainSignaturesAreValid(cert_chain_));
+    ASSERT_GT(cert_chain_.size(), 0);
+    verify_subject_and_serial(cert_chain_[0], serial_int, subject, /* self_signed = */ false);
+
+    AuthorizationSet hw_enforced = HwEnforcedAuthorizations(key_characteristics_);
+    AuthorizationSet sw_enforced = SwEnforcedAuthorizations(key_characteristics_);
+
+    // Check that the unique ID field in the extension is non-empty.
+    vector<uint8_t> unique_id;
+    EXPECT_TRUE(verify_attestation_record(challenge, app_id, sw_enforced, hw_enforced, SecLevel(),
+                                          cert_chain_[0].encodedCertificate, &unique_id));
+    EXPECT_GT(unique_id.size(), 0);
+
+    CheckedDeleteKey();
+    hw_enforced.Clear();
+    sw_enforced.Clear();
+
+    // Generating a new key with the same parameters should give the same unique ID.
+    ASSERT_EQ(ErrorCode::OK, GenerateKey(builder));
+    ASSERT_GT(key_blob_.size(), 0U);
+    ASSERT_GT(cert_chain_.size(), 0);
+    verify_subject_and_serial(cert_chain_[0], serial_int, subject, /* self_signed = */ false);
+
+    hw_enforced = HwEnforcedAuthorizations(key_characteristics_);
+    sw_enforced = SwEnforcedAuthorizations(key_characteristics_);
+    vector<uint8_t> unique_id2;
+    EXPECT_TRUE(verify_attestation_record(challenge, app_id, sw_enforced, hw_enforced, SecLevel(),
+                                          cert_chain_[0].encodedCertificate, &unique_id2));
+    EXPECT_EQ(unique_id, unique_id2);
+    CheckedDeleteKey();
+    hw_enforced.Clear();
+    sw_enforced.Clear();
+
+    // A different attestation application ID should yield a different unique ID.
+    AuthorizationSetBuilder builder2 = base_builder;
+    auto app_id2 = "different_foo";
+    builder2.AttestationApplicationId(app_id2).Authorization(TAG_CREATION_DATETIME, 1619621648000);
+
+    ASSERT_EQ(ErrorCode::OK, GenerateKey(builder2));
+    ASSERT_GT(key_blob_.size(), 0U);
+    ASSERT_GT(cert_chain_.size(), 0);
+    verify_subject_and_serial(cert_chain_[0], serial_int, subject, /* self_signed = */ false);
+
+    hw_enforced = HwEnforcedAuthorizations(key_characteristics_);
+    sw_enforced = SwEnforcedAuthorizations(key_characteristics_);
+    vector<uint8_t> unique_id3;
+    EXPECT_TRUE(verify_attestation_record(challenge, app_id2, sw_enforced, hw_enforced, SecLevel(),
+                                          cert_chain_[0].encodedCertificate, &unique_id3));
+    EXPECT_NE(unique_id, unique_id3);
+    CheckedDeleteKey();
+    hw_enforced.Clear();
+    sw_enforced.Clear();
+
+    // A radically different date should yield a different unique ID.
+    AuthorizationSetBuilder builder3 = base_builder;
+    builder3.AttestationApplicationId(app_id).Authorization(TAG_CREATION_DATETIME, 1611621648000);
+
+    ASSERT_EQ(ErrorCode::OK, GenerateKey(builder3));
+    ASSERT_GT(key_blob_.size(), 0U);
+    ASSERT_GT(cert_chain_.size(), 0);
+    verify_subject_and_serial(cert_chain_[0], serial_int, subject, /* self_signed = */ false);
+
+    hw_enforced = HwEnforcedAuthorizations(key_characteristics_);
+    sw_enforced = SwEnforcedAuthorizations(key_characteristics_);
+    vector<uint8_t> unique_id4;
+    EXPECT_TRUE(verify_attestation_record(challenge, app_id, sw_enforced, hw_enforced, SecLevel(),
+                                          cert_chain_[0].encodedCertificate, &unique_id4));
+    EXPECT_NE(unique_id, unique_id4);
+    CheckedDeleteKey();
+    hw_enforced.Clear();
+    sw_enforced.Clear();
+}
+
+/*
  * NewKeyGenerationTest.EcdsaAttestationTagNoApplicationId
  *
  * Verifies that creation of an attested ECDSA key does not include APPLICATION_ID.
