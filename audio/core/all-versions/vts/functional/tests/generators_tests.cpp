33e630fade04658393373fca746784f18142626d
Mikhail Naganov <mnaganov@google.com>
2021-11-03 20:15:36 +0000

    audio: Fix handling of dynamic profiles in VtsHalAudioV7_0TargetTest
    The profiles generator wasn't loading correctly from the Audio
    Policy Manager config file profiles with unpopulated attributes,
    which is typical for dynamic profiles.
    
    Bug: 204314749
    Test: atest HalAudioV7_0GeneratorTest
    Change-Id: I514a4e03da165cacb5dbaaa16470130895681484

--- audio/core/all-versions/vts/functional/tests/generators_tests.cpp
+++ audio/core/all-versions/vts/functional/tests/generators_tests.cpp
@@ -129,4 +129,9 @@ TEST_P(GeneratorsTest, ValidateConfigs) {
 
 // Target file names are the same for all versions, see 'HalAudioVx_0GeneratorTest.xml' test configs
 INSTANTIATE_TEST_SUITE_P(Generators, GeneratorsTest,
-                         ::testing::Values("apm_config_no_vx.xml", "apm_config_with_vx.xml"));
+                         ::testing::Values("apm_config_no_vx.xml", "apm_config_with_vx.xml"
+#if MAJOR_VERSION == 7
+                                           ,
+                                           "apm_config_b_204314749_7_0.xml"
+#endif
+                                           ));
