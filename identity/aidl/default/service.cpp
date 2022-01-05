96d6480a5d0548358bbcfa13f4d76af52e7298e1
Seth Moore <sethmo@google.com>
2022-01-05 09:34:42 -0800

    Support RKP in the default identity cred service
    Allow the credstore to pass in remotely provisioned keys for attestation
    of the credential key.
    
    Update the VTS tests to check the new RKP functionality.
    
    Test: VtsHalIdentityTargetTest
    Bug: 194696876
    Change-Id: I96dcf3027e0f21790c35900ddf8cc0953bd3b1ca

--- identity/aidl/default/service.cpp
+++ identity/aidl/default/service.cpp
@@ -16,6 +16,7 @@
 
 #define LOG_TAG "android.hardware.identity-service"
 
+#include <aidl/android/hardware/security/keymint/IRemotelyProvisionedComponent.h>
 #include <android-base/logging.h>
 #include <android/binder_manager.h>
 #include <android/binder_process.h>
@@ -29,6 +30,7 @@ using ::android::base::InitLogging;
 using ::android::base::StderrLogger;
 
 using ::aidl::android::hardware::identity::IdentityCredentialStore;
+using ::aidl::android::hardware::security::keymint::IRemotelyProvisionedComponent;
 using ::android::hardware::identity::FakeSecureHardwareProxyFactory;
 using ::android::hardware::identity::SecureHardwareProxyFactory;
 
@@ -36,10 +38,13 @@ int main(int /*argc*/, char* argv[]) {
     InitLogging(argv, StderrLogger);
 
     sp<SecureHardwareProxyFactory> hwProxyFactory = new FakeSecureHardwareProxyFactory();
+    const std::string remotelyProvisionedComponent =
+            std::string(IRemotelyProvisionedComponent::descriptor) + "/default";
 
     ABinderProcess_setThreadPoolMaxThreadCount(0);
     std::shared_ptr<IdentityCredentialStore> store =
-            ndk::SharedRefBase::make<IdentityCredentialStore>(hwProxyFactory);
+            ndk::SharedRefBase::make<IdentityCredentialStore>(hwProxyFactory,
+                                                              remotelyProvisionedComponent);
 
     const std::string instance = std::string() + IdentityCredentialStore::descriptor + "/default";
     binder_status_t status = AServiceManager_addService(store->asBinder().get(), instance.c_str());
