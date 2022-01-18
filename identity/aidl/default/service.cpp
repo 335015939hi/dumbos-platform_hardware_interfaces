fbc82d9ccb25143a7099124ee30c128197a8a201
Seth Moore <sethmo@google.com>
2022-01-18 15:52:20 -0800

    Log to logd in the default identity service
    This allows us to see logs in logcat for the default service.
    
    Test: VtsHalIdentityTargetTest
    Bug: 194696876
    Change-Id: Ib368a2a0021f72b457fc5e4717e34bb696dfb0fb

--- identity/aidl/default/service.cpp
+++ identity/aidl/default/service.cpp
@@ -26,14 +26,13 @@
 
 using ::android::sp;
 using ::android::base::InitLogging;
-using ::android::base::StderrLogger;
 
 using ::aidl::android::hardware::identity::IdentityCredentialStore;
 using ::android::hardware::identity::FakeSecureHardwareProxyFactory;
 using ::android::hardware::identity::SecureHardwareProxyFactory;
 
 int main(int /*argc*/, char* argv[]) {
-    InitLogging(argv, StderrLogger);
+    InitLogging(argv);
 
     sp<SecureHardwareProxyFactory> hwProxyFactory = new FakeSecureHardwareProxyFactory();
 
