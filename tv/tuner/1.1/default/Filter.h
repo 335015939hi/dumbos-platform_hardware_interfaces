871c5354fb85a6c107b3e431b22459bb5e8c416d
anburaj <anburaj@tataelxsi.co.in>
2022-06-10 11:29:22 +0530

    Implement cuttlefish TunerHAL emulator
    Change-Id: I0709b0e3db3b6140fef93945b5ffbe1e163ac856

--- tv/tuner/1.1/default/Filter.h
+++ tv/tuner/1.1/default/Filter.h
@@ -48,6 +48,9 @@ const uint32_t BUFFER_SIZE_16M = 0x1000000;
 
 class Demux;
 class Dvr;
+class MpegPacket;
+class Table;
+class SDT;
 
 class Filter : public V1_1::IFilter {
   public:
@@ -131,6 +134,8 @@ class Filter : public V1_1::IFilter {
     DemuxFilterSettings mFilterSettings;
 
     uint16_t mTpid;
+    vector<uint8_t> mTableid; // Anbu - for testing
+    uint8_t mTable;
     sp<V1_0::IFilter> mDataSource;
     bool mIsDataSourceDemux = true;
     vector<uint8_t> mFilterOutput;
@@ -144,6 +149,7 @@ class Filter : public V1_1::IFilter {
 
     // Thread handlers
     pthread_t mFilterThread;
+    pthread_t mTSExtractorThread; // Anbu - for testing
 
     // FMQ status local records
     DemuxFilterStatus mFilterStatus;
@@ -151,6 +157,7 @@ class Filter : public V1_1::IFilter {
      * If a specific filter's writing loop is still running
      */
     bool mFilterThreadRunning;
+    bool mFilterTsExtractThrdRunning;
     bool mKeepFetchingDataFromFrontend;
 
     /**
@@ -159,7 +166,8 @@ class Filter : public V1_1::IFilter {
      */
     const uint16_t SECTION_WRITE_COUNT = 10;
 
-    bool DEBUG_FILTER = false;
+    bool DEBUG_FILTER = true;//false; // Anbu - testing
+    std::ifstream input; // Anbu -for testing
 
     /**
      * Filter handlers to handle the data filtering.
@@ -173,6 +181,8 @@ class Filter : public V1_1::IFilter {
     Result startPcrFilterHandler();
     Result startTemiFilterHandler();
     Result startFilterLoop();
+	Result startTsextractorLoop();
+	
 
     void deleteEventFlag();
     bool writeDataToFilterMQ(const std::vector<uint8_t>& data);
@@ -188,7 +198,10 @@ class Filter : public V1_1::IFilter {
     void startTsFilter(vector<uint8_t> data);
     bool startFilterDispatcher();
     static void* __threadLoopFilter(void* user);
+	static void* __threadLoopTsExtractor(void* user);
     void filterThreadLoop();
+	void tsextractorThreadLoop();
+	bool readpacket(vector<uint8_t>& rawpacket, ifstream& s);
 
     int createAvIonFd(int size);
     uint8_t* getIonBuffer(int fd, int size);
@@ -210,6 +223,7 @@ class Filter : public V1_1::IFilter {
     DemuxFilterEvent createTemiEvent();
     V1_1::DemuxFilterEventExt createMonitorEvent();
     V1_1::DemuxFilterEventExt createRestartEvent();
+	void mergePackets(vector<MpegPacket*>& packets);
     /**
      * Lock to protect writes to the FMQs
      */
@@ -226,6 +240,7 @@ class Filter : public V1_1::IFilter {
     std::mutex mFilterThreadLock;
     std::mutex mFilterOutputLock;
     std::mutex mRecordFilterOutputLock;
+    std::mutex mFilterextThreadLock;
 
     // temp handle single PES filter
     // TODO handle mulptiple Pes filters
--- tv/tuner/1.1/default/Frontend.cpp
+++ tv/tuner/1.1/default/Frontend.cpp
@@ -65,12 +65,6 @@ Return<Result> Frontend::tune(const FrontendSettings& /* settings */) {
         return Result::INVALID_STATE;
     }
 
-	ALOGD("%s> Def-tunerhal tune begin api %d",  __FUNCTION__, mId);   // Anbu - for testing
-
-	// Anbu - for testing
-	if (mIsLocked) {
-		return Result::SUCCESS;
-	}
     mTunerService->frontendStartTune(mId);
     mCallback->onEvent(FrontendEventType::LOCKED);
     mIsLocked = true;
