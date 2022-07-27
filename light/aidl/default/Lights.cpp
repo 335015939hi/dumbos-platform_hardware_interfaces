759e92c792a0f4e9fa62f29ede0dc528f0df4872
Sahil Somani <sahilsomani@google.com>
2022-07-27 16:43:30 -0700

    Added dummy Lights to the Light HAL default implementation
    Most of the VTS tests for the Light HAL involve looping over
    the list of Light objects returned by Lights::getLights(), but
    the current default implementation of this function returns no
    Light objects. Lights::getLights() was changed to create a few
    dummy Light objects when called. Lights::setLightStatus() was
    also changed to determine whether a given Light id was created
    by Lights::getLights() and thus valid or not.
    
    Test: VtsHalLightTargetTest
    Bug: 240485903
    Change-Id: I5ad0bdfdccacc2bde80c2dd89eaa84702f22a7fb

--- light/aidl/default/Lights.cpp
+++ light/aidl/default/Lights.cpp
@@ -23,12 +23,23 @@ namespace android {
 namespace hardware {
 namespace light {
 
+int numDefaultLights = 3;
+
 ndk::ScopedAStatus Lights::setLightState(int id, const HwLightState& state) {
     LOG(INFO) << "Lights setting state for id=" << id << " to color " << std::hex << state.color;
-    return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    if (id < 0 || id > numDefaultLights) {
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    } else {
+        return ndk::ScopedAStatus::ok();
+    }
 }
 
-ndk::ScopedAStatus Lights::getLights(std::vector<HwLight>* /*lights*/) {
+ndk::ScopedAStatus Lights::getLights(std::vector<HwLight>* lights) {
+    for (unsigned int i = 1; i <= numDefaultLights; i++) {
+        HwLight light;
+        light.id = light.ordinal = i;
+        lights->push_back(light);
+    }
     LOG(INFO) << "Lights reporting supported lights";
     return ndk::ScopedAStatus::ok();
 }
