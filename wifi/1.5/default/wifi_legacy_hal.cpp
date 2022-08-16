6e68e262fef38fca262cc13329417029ae1298ff
donghongweng <donghongweng@google.com>
2022-08-16 18:29:28 +0800

    Fix unqualified-std-cast-call compiler warning
    Test: m
    Change-Id: I403ce701846a3d3582e1dfa5557fe981e86f2140

--- wifi/1.5/default/wifi_legacy_hal.cpp
+++ wifi/1.5/default/wifi_legacy_hal.cpp
@@ -576,7 +576,7 @@ WifiLegacyHal::readApfPacketFilterData(const std::string& iface_name) {
     status = global_func_table_.wifi_read_packet_filter(
         getIfaceHandle(iface_name), /*src_offset=*/0, buffer.data(),
         buffer.size());
-    return {status, move(buffer)};
+    return {status, std::move(buffer)};
 }
 
 std::pair<wifi_error, wifi_gscan_capabilities>
