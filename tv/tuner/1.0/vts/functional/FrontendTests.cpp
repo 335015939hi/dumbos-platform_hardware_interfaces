2dd5bf53fb0ebaae6935b3f40cfe4a5fecd0bf3e
cmlin0118 <cmlin0118@realtek.com>
2022-09-28 17:41:03 +0800

    Fix tunerhal vts feId uninitialized on getFrontendIdByType
    Problem: FeId uninitialized on getFrontendIdByType
    Solution: Initializ feId
    Test: run vts -m VtsHalTvTunerV1_0TargetTest
    Bug: 248472007
    Change-Id: I373c49cf0a105869743be7f2c0737b4f5b30ae26

--- tv/tuner/1.0/vts/functional/FrontendTests.cpp
+++ tv/tuner/1.0/vts/functional/FrontendTests.cpp
@@ -418,6 +418,8 @@ AssertionResult FrontendTests::closeFrontend() {
 
 void FrontendTests::getFrontendIdByType(FrontendType feType, uint32_t& feId) {
     ASSERT_TRUE(getFrontendIds());
+    feId = INVALID_ID;
+    
     if (mFeIds.size() > 0) {
         for (size_t i = 0; i < mFeIds.size(); i++) {
             ASSERT_TRUE(getFrontendInfo(mFeIds[i]));
