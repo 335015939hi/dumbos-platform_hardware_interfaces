8073c31cc22e2c09214d70a1314744da67cd31e7
Sandeep Dhavale <dhavale@google.com>
2022-09-29 21:12:44 +0000

    Fastboot: Convert Fastboot HIDL HAL to AIDL
    Also adds mock implementation based on AIDL.
    
    Bug: 205760652
    Test: m
    Signed-off-by: Sandeep Dhavale <dhavale@google.com>
    Change-Id: Iee32e3e859d90ba494ab41fab739f6317993a3c3

---
+++ fastboot/aidl/default/Fastboot.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "aidl/android/hardware/fastboot/BnFastboot.h"
+
+namespace aidl {
+namespace android {
+namespace hardware {
+namespace fastboot {
+class Fastboot : public BnFastboot {
+    ::ndk::ScopedAStatus doOemCommand(
+            const std::string& in_oemCmd,
+            ::aidl::android::hardware::fastboot::Result* _aidl_return) override;
+    ::ndk::ScopedAStatus doOemSpecificErase(
+            ::aidl::android::hardware::fastboot::Result* _aidl_return) override;
+    ::ndk::ScopedAStatus getBatteryVoltageFlashingThreshold(
+            ::aidl::android::hardware::fastboot::Result* out_result,
+            int32_t* _aidl_return) override;
+    ::ndk::ScopedAStatus getOffModeChargeState(
+            ::aidl::android::hardware::fastboot::Result* out_result, bool* _aidl_return) override;
+    ::ndk::ScopedAStatus getPartitionType(
+            const std::string& in_partitionName,
+            ::aidl::android::hardware::fastboot::Result* out_result,
+            ::aidl::android::hardware::fastboot::FileSystemType* _aidl_return) override;
+    ::ndk::ScopedAStatus getVariant(::aidl::android::hardware::fastboot::Result* out_result,
+                                    std::string* _aidl_return) override;
+};
+
+}  // namespace fastboot
+}  // namespace hardware
+}  // namespace android
+}  // namespace aidl
