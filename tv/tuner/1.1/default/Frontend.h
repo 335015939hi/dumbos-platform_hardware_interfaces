3f9476c8cfe89ef1d3048fbbf710478c21a305ac
marko.nesovic <marko.nesovic@rt-rk.com>
2022-11-10 13:57:55 +0100

    Add MPEG Transport Stream Toolkit
    -Add TS Duck source code necessary
    https://tsduck.io/
    
    Change-Id: Id4c540c0b22895f534605fec72b3d828592ec64e

--- tv/tuner/1.1/default/Frontend.h
+++ tv/tuner/1.1/default/Frontend.h
@@ -18,9 +18,8 @@
 #define ANDROID_HARDWARE_TV_TUNER_V1_1_FRONTEND_H_
 
 #include <android/hardware/tv/tuner/1.1/IFrontend.h>
-#include <fstream>
-#include <iostream>
 #include "Tuner.h"
+#include "FileTuner/TsPlayPump/tsUString.h"
 
 using namespace std;
 
@@ -78,6 +77,12 @@ class Frontend : public V1_1::IFrontend {
 
     bool isLocked();
 
+    bool has_suffix(const string& s, const string& suffix);
+
+    vector<int> getFrequencyVector();
+
+    ts::UString formatFrequencyPath(int frequency);
+
   private:
     virtual ~Frontend();
     bool supportsSatellite();
@@ -87,8 +92,6 @@ class Frontend : public V1_1::IFrontend {
     FrontendId mId = 0;
     bool mIsLocked = false;
     uint32_t mCiCamId;
-
-    std::ifstream mFrontendData;
 };
 
 }  // namespace implementation
