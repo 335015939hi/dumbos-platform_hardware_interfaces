5c3878690eb84c928acb0a32896a8efd189a9d39
Max Bires <jbires@google.com>
2022-11-21 23:02:09 -0800

    Add VSR enforcement testing for IRPC versioning.
    Check the VSR API level for the device under test and ensure that the
    appropriate HAL version for the IRemotelyProvisionedComponent interface
    is present. E.g. a VSR 14 chipset should have IRPC v3 or newer.
    
    Bug: 251185719
    Test: atest VtsHalRemotelyProvisionedComponentTargetTest
    Change-Id: I136cac27953d1d5acaa45510ad9efec2a75d8038

--- security/keymint/aidl/vts/functional/VtsRemotelyProvisionedComponentTests.cpp
+++ security/keymint/aidl/vts/functional/VtsRemotelyProvisionedComponentTests.cpp
@@ -797,4 +797,23 @@ TEST_P(CertificateRequestV2Test, CertificateRequestV1Removed) {
 
 INSTANTIATE_REM_PROV_AIDL_TEST(CertificateRequestV2Test);
 
+using VsrRequirementTest = VtsRemotelyProvisionedComponentTests;
+
+TEST_P(VsrRequirementTest, Vsr13Test) {
+    RpcHardwareInfo hwInfo;
+    ASSERT_TRUE(provisionable_->getHardwareInfo(&hwInfo).isOk());
+    int vsr_api_level = get_vsr_api_level();
+    if (vsr_api_level < 34) {
+        GTEST_SKIP() << "Applies only to VSR API level 34 or newer, this device is: "
+                     << vsr_api_level;
+    }
+    switch (vsr_api_level) {
+        default:
+            EXPECT_GE(hwInfo.versionNumber, 3) << "VSR 14+ requires IRPC v3 or newer.";
+            break;
+    }
+}
+
+INSTANTIATE_KEYMINT_AIDL_TEST(VsrRequirementTest);
+
 }  // namespace aidl::android::hardware::security::keymint::test
