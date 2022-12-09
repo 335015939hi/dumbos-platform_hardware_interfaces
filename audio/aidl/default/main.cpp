61f665106d47864b1d488c3a22f15ffd29116c8f
Mikhail Naganov <mnaganov@google.com>
2022-12-09 00:33:47 +0000

    audio: Bring default HAL configuration closer to cuttlefish
    This is to simplify switching of cuttlefish to
    the AIDL implementation. Split out 'r_submix' module.
    
    Add all IModule instance names to the compatibility
    matrix.
    
    Bug: 205884982
    Test: atest VtsHalAudioCoreTargetTest
    Change-Id: I424db14257281671eea87aa743deb778ca75f388

--- audio/aidl/default/main.cpp
+++ audio/aidl/default/main.cpp
@@ -42,10 +42,15 @@ int main() {
             AServiceManager_addService(config->asBinder().get(), configName.c_str());
     CHECK_EQ(STATUS_OK, status);
 
-    // Make the default module
-    auto moduleDefault = ndk::SharedRefBase::make<Module>();
+    // Make modules
+    auto moduleDefault = ndk::SharedRefBase::make<Module>(Module::Type::DEFAULT);
     const std::string moduleDefaultName = std::string() + Module::descriptor + "/default";
     status = AServiceManager_addService(moduleDefault->asBinder().get(), moduleDefaultName.c_str());
+    CHECK_EQ(STATUS_OK, status);
+
+    auto moduleRSubmix = ndk::SharedRefBase::make<Module>(Module::Type::R_SUBMIX);
+    const std::string moduleRSubmixName = std::string() + Module::descriptor + "/r_submix";
+    status = AServiceManager_addService(moduleRSubmix->asBinder().get(), moduleRSubmixName.c_str());
     CHECK_EQ(STATUS_OK, status);
 
     ABinderProcess_joinThreadPool();
