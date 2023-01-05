8dd5ddbb59767ee64612fa37806b5544f1cb50aa
Mikhail Naganov <mnaganov@google.com>
2023-01-05 20:16:07 +0000

    audio: Improve test coverage
    Add a vendor-specific parameter "aosp.forceTransientBurst"
    which helps to cover the case of non-full async writes.
    This parameter is specific to the "AOSP as vendor" implementation,
    other vendors are not required to have it—that's why it's
    not in AIDL.
    
    Fix minor issues in VTS found after revisiting the code, and by
    looking at logs during testing.
    
    Bug: 262402957
    Test: atest VtsHalAudioCoreTargetTest
    Change-Id: Ide961d91a8d9da24392561654f04eb8207b7b781

--- audio/aidl/default/include/core-impl/Module.h
+++ audio/aidl/default/include/core-impl/Module.h
@@ -36,6 +36,11 @@ class Module : public BnModule {
     explicit Module(Type type) : mType(type) {}
 
   private:
+    struct VendorDebug {
+        static const std::string kForceTransientBurstName;
+        bool forceTransientBurst = false;
+    };
+
     ndk::ScopedAStatus setModuleDebug(
             const ::aidl::android::hardware::audio::core::ModuleDebug& in_debug) override;
     ndk::ScopedAStatus getTelephony(std::shared_ptr<ITelephony>* _aidl_return) override;
@@ -127,6 +132,7 @@ class Module : public BnModule {
     const Type mType;
     std::unique_ptr<internal::Configuration> mConfig;
     ModuleDebug mDebug;
+    VendorDebug mVendorDebug;
     // Since it is required to return the same instance of the ITelephony, even
     // if the client has released it on its side, we need to hold it via a strong pointer.
     std::shared_ptr<ITelephony> mTelephony;
