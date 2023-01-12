b1beb5482f6f2ab0964dafac412993f632e24b93
Daniel Angell <danielangell@google.com>
2023-01-12 22:33:08 +0000

    Add ro.serialno sysprop to JSON output
    To help with error reporting on the RKP servers in the future it
    will be helpful to be able to address CSRs by serial number when
    possible.
    
    Bug: 264302050
    Test: libkeymint_remote_prov_support_test and sent JSON to server
    Change-Id: I2808441c200d0679e618580abc464cd3c71c220e

--- security/keymint/support/remote_prov_utils.cpp
+++ security/keymint/support/remote_prov_utils.cpp
@@ -417,9 +417,14 @@ ErrMsgOr<std::vector<BccEntryData>> validateBcc(const cppbor::Array* bcc) {
 
 JsonOutput jsonEncodeCsrWithBuild(const std::string instance_name, const cppbor::Array& csr) {
     const std::string kFingerprintProp = "ro.build.fingerprint";
+    const std::string kSerialNoProp = "ro.serialno";
 
     if (!::android::base::WaitForPropertyCreation(kFingerprintProp)) {
         return JsonOutput::Error("Unable to read build fingerprint");
+    }
+
+    if (!::android::base::WaitForPropertyCreation(kSerialNoProp)) {
+        return JsonOutput::Error("Unable to read device serial number");
     }
 
     bytevec csrCbor = csr.encode();
@@ -441,6 +446,7 @@ JsonOutput jsonEncodeCsrWithBuild(const std::string instance_name, const cppbor:
     Json::Value json(Json::objectValue);
     json["name"] = instance_name;
     json["build_fingerprint"] = ::android::base::GetProperty(kFingerprintProp, /*default=*/"");
+    json["serialno"] = ::android::base::GetProperty(kSerialNoProp, /*default=*/"");
     json["csr"] = base64.data();  // Boring writes a NUL-terminated c-string
 
     Json::StreamWriterBuilder factory;
