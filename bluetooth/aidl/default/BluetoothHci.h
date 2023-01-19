1650ce7c800116b20ef18d5e83efb15c4c8a117a
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2023-01-19 20:22:56 +0000

    Revert "bluetooth: Add support for linux bluetooth raw hci sockets"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/b68dd3fa-1238-4b18-b603-65cf134ca943). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I2c010d27168a8893719b34f4aa3baf95fa89e8fc

--- bluetooth/aidl/default/BluetoothHci.h
+++ bluetooth/aidl/default/BluetoothHci.h
@@ -24,7 +24,6 @@
 
 #include "async_fd_watcher.h"
 #include "h4_protocol.h"
-#include "net_bluetooth_mgmt.h"
 
 namespace aidl::android::hardware::bluetooth::impl {
 
@@ -65,10 +64,8 @@ class BluetoothHci : public BnBluetoothHci {
 
   ::android::hardware::bluetooth::async::AsyncFdWatcher mFdWatcher;
 
-  int getFdFromDevPath();
   void send(::android::hardware::bluetooth::hci::PacketType type,
             const std::vector<uint8_t>& packet);
-  std::unique_ptr<NetBluetoothMgmt> management_{};
 };
 
 }  // namespace aidl::android::hardware::bluetooth::impl
