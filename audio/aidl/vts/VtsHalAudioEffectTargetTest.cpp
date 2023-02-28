4aa9dbef40d0b5d2ed19ff6641df26aa01a953f8
Shunkai Yao <yaoshunkai@google.com>
2023-02-28 01:50:40 +0000

    AIDL effect: Effect factory add EffectProxy information in proxy
    Effect factory have the knowledge of all effect configurations, in the
    default implementation, this configuration defined in
    audio_effects_config.xml.
    
    Bug: 261129656
    Test: Enable AIDL, atest
    CtsMediaAudioTestCases: android.media.audio.cts.BassBoostTest
    Test: atest VtsHalAudioEffectTargetTest
    Change-Id: Ie9bd4070acdca53c4236bae533957b8fde6e9183

--- audio/aidl/vts/VtsHalAudioEffectTargetTest.cpp
+++ audio/aidl/vts/VtsHalAudioEffectTargetTest.cpp
@@ -140,7 +140,10 @@ TEST_P(AudioEffectTest, GetDescritorBeforeOpen) {
     Descriptor desc;
     ASSERT_NO_FATAL_FAILURE(create(mFactory, mEffect, mDescriptor));
     ASSERT_NO_FATAL_FAILURE(getDescriptor(mEffect, desc));
-    EXPECT_EQ(mDescriptor.common, desc.common);
+    EXPECT_EQ(mDescriptor.common.id.type, desc.common.id.type);
+    EXPECT_EQ(mDescriptor.common.id.uuid, desc.common.id.uuid);
+    EXPECT_EQ(mDescriptor.common.name, desc.common.name);
+    EXPECT_EQ(mDescriptor.common.implementor, desc.common.implementor);
     // Effect implementation Must fill in implementor and name
     EXPECT_NE("", desc.common.name);
     EXPECT_NE("", desc.common.implementor);
@@ -177,7 +180,13 @@ TEST_P(AudioEffectTest, DescriptorExistAndUnique) {
 
     ASSERT_NO_FATAL_FAILURE(create(mFactory, mEffect, mDescriptor));
     ASSERT_NO_FATAL_FAILURE(getDescriptor(mEffect, desc));
-    EXPECT_EQ(1ul, idSet.count(desc.common.id));
+    int uuidCount = 0;
+    for (const auto& id : idSet) {
+        if (id.uuid == desc.common.id.uuid && id.type == desc.common.id.type) {
+            uuidCount++;
+        }
+    }
+    EXPECT_EQ(1, uuidCount);
     ASSERT_NO_FATAL_FAILURE(destroy(mFactory, mEffect));
 }
 
