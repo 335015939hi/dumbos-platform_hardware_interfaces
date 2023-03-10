4ed7c5efb3b4158c0a25543bb2b4300126bb36ee
Mikhail Naganov <mnaganov@google.com>
2023-03-09 18:19:01 -0800

    audio: Put audio/aidl/common/include/Utils.h into 'aidl' NS
    These utilities are for types generated from AIDL. Since
    the types end up in the 'aidl' namespace (NDK backend), it makes
    sense to keep the utilities in the same namespace.
    
    While changing the namespace, I've found numerous useless
    includes of 'Utils.h' and removing them, also reorganizing
    lists of includes along the way to match the style guide.
    
    Bug: 205884982
    Test: m
    Test: atest --test-mapping hardware/interfaces/audio/aidl/TEST_MAPPING
    Change-Id: Ib23b47092364046f632f9503b92b9baff5b902f5

--- audio/aidl/default/dynamicProcessing/DynamicsProcessingSw.cpp
+++ audio/aidl/default/dynamicProcessing/DynamicsProcessingSw.cpp
@@ -14,13 +14,12 @@
  * limitations under the License.
  */
 
-#include <cstddef>
-#define LOG_TAG "AHAL_DynamicsProcessingSw"
-#include <Utils.h>
 #include <algorithm>
+#include <cstddef>
 #include <set>
 #include <unordered_set>
 
+#define LOG_TAG "AHAL_DynamicsProcessingSw"
 #include <android-base/logging.h>
 #include <fmq/AidlMessageQueue.h>
 
