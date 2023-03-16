fe6928cdbb328874e4712cf177dd341c90e46f7c
Josh Wu <joshwu@google.com>
2023-03-16 21:49:06 +0800

    Add HciProxy
    This is a proxy app helping developers to test or utilize controllers
    with external stacks.
    
    Bug: 274065571
    Test: m
    Test: avatar
    Change-Id: Idd309ee3b42d153c7c61e037b540371bb2602e4c

---
+++ bluetooth/hci/hci_proxy/AidlWrapper.kt
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.hardware.bluetooth.hci.hci_proxy
+
+import android.hardware.bluetooth.IBluetoothHci
+import android.hardware.bluetooth.IBluetoothHciCallbacks
+import android.util.Log
+
+import java.io.DataOutputStream
+import java.io.IOException
+
+class AidlWrapper(service: IBluetoothHci) : HalWrapper() {
+  companion object {
+    private val TAG = "HciProxyAidl"
+  }
+
+  private val service: IBluetoothHci
+  private var callbacks: BluetoothHciCallbacks? = null
+
+  init {
+    this.service = service
+  }
+
+  class BluetoothHciCallbacks(out:DataOutputStream): IBluetoothHciCallbacks.Stub() {
+    private val out: DataOutputStream
+
+    init {
+      this.out = out
+    }
+
+    override fun initializationComplete(status:Int) {
+        Log.i(TAG, "HAL client init complete, status=$status")
+    }
+
+    override fun getInterfaceHash() = IBluetoothHciCallbacks.HASH
+    override fun getInterfaceVersion() = IBluetoothHciCallbacks.VERSION
+
+    override fun hciEventReceived(data: ByteArray) {
+        out.writeByte(HciConstants.HCI_PACKET_TYPE_EVENT.toInt())
+        out.write(data)
+        out.flush()
+    }
+
+    override fun aclDataReceived(data: ByteArray) {
+        out.writeByte(HciConstants.HCI_PACKET_TYPE_ACL_DATA.toInt())
+        out.write(data)
+        out.flush()
+    }
+
+    override fun scoDataReceived(data: ByteArray) {
+        out.writeByte(HciConstants.HCI_PACKET_TYPE_SCO_DATA.toInt())
+        out.write(data)
+        out.flush()
+    }
+
+    override fun isoDataReceived(data: ByteArray) {
+        out.writeByte(HciConstants.HCI_PACKET_TYPE_ISO_DATA.toInt())
+        out.write(data)
+        out.flush()
+    }
+  }
+
+  override fun initialize(outputStream: DataOutputStream) {
+    this.callbacks = BluetoothHciCallbacks(outputStream)
+    service.initialize(this.callbacks)
+  }
+
+  override fun close() {
+    service.close()
+    this.callbacks = null
+  }
+
+  override fun sendHciCommand(data: ByteArray) {
+    service.sendHciCommand(data)
+  }
+
+  override fun sendAclData(data: ByteArray) {
+    service.sendAclData(data)
+  }
+
+  override fun sendScoData(data: ByteArray) {
+    service.sendScoData(data)
+  }
+
+  override fun sendIsoData(data: ByteArray) {
+    service.sendIsoData(data)
+  }
+}
