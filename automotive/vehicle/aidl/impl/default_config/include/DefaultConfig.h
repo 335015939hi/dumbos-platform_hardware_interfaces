7896309d07ab4bc85e4a5ab73b4c13ae53fd1120
Kuang Chen <kuangc@nvidia.com>
2023-03-27 10:22:31 +0800

    Add PERF_VEHICLE_SPEED_DISPLAY default config
    Test AtsCarPropertyDeviceApp requires the PERF_VEHICLE_SPEED_DISPLAY
    feature must be implemented, but it isn't configured by default.
    Test: perfVehicleSpeedDisplay_mustBeImplemented
    
    Change-Id: Ife8103e598fb856de33543f9460a59f64065c45d

--- automotive/vehicle/aidl/impl/default_config/include/DefaultConfig.h
+++ automotive/vehicle/aidl/impl/default_config/include/DefaultConfig.h
@@ -170,6 +170,16 @@ const std::vector<ConfigDeclaration> kVehicleProperties = {
 
         {.config =
                  {
+                         .prop = toInt(VehicleProperty::PERF_VEHICLE_SPEED_DISPLAY),
+                         .access = VehiclePropertyAccess::READ,
+                         .changeMode = VehiclePropertyChangeMode::CONTINUOUS,
+                         .minSampleRate = 1.0f,
+                         .maxSampleRate = 10.0f,
+                 },
+         .initialValue = {.floatValues = {0.0f}}},
+
+        {.config =
+                 {
                          .prop = toInt(VehicleProperty::VEHICLE_SPEED_DISPLAY_UNITS),
                          .access = VehiclePropertyAccess::READ_WRITE,
                          .changeMode = VehiclePropertyChangeMode::ON_CHANGE,
