112208974d97caf2b01efe1c1fc4ba158f5ab43a
Henri Chataing <henrichataing@google.com>
2023-04-20 10:21:56 -0700

    Bluetooth: Make the H4 parser module standalone
    The H4 parsing logic is currently split between the packetizer which
    extracts the IDCs and the parser which extracts the header and payload.
    That make it complicated to use the parser without the packetizer which
    comes with its own socket management logic.
    
    This change reimplements the parser as a separate module,
    linked in a separate library.
    
    Test: atest --host bluetooth-vendor-interface-hci-test
    Change-Id: Iaafc967c723e4460f46ea2d8ccc0fcb34acc1d6f

--- bluetooth/hci/h4_protocol.h
+++ bluetooth/hci/h4_protocol.h
@@ -19,8 +19,7 @@
 #include <memory>
 #include <vector>
 
-#include "hci_internals.h"
-#include "hci_packetizer.h"
+#include "h4_parser.h"
 
 namespace android::hardware::bluetooth::hci {
 
@@ -35,26 +34,17 @@ class H4Protocol {
 
   size_t Send(PacketType type, const uint8_t* data, size_t length);
   size_t Send(PacketType type, const std::vector<uint8_t>& data);
-
   void OnDataReady();
 
  protected:
-  size_t OnPacketReady(const std::vector<uint8_t>& packet);
-  void SendDataToPacketizer(uint8_t* buffer, size_t length);
+  void SendDataToPacketizer(uint8_t const* buffer, size_t length);
 
  private:
   int uart_fd_;
   bool disconnected_{false};
 
-  PacketReadCallback cmd_cb_;
-  PacketReadCallback acl_cb_;
-  PacketReadCallback sco_cb_;
-  PacketReadCallback event_cb_;
-  PacketReadCallback iso_cb_;
   DisconnectCallback disconnect_cb_;
-
-  PacketType hci_packet_type_{PacketType::UNKNOWN};
-  HciPacketizer hci_packetizer_;
+  H4Parser h4_parser_;
 
   /**
    * Question : Why read in single chunk rather than multiple reads?
