70983fa8b7c87d1dbf0708d21619ae779faf81f1
Rajesh Nyamagoud <nyamagoud@google.com>
2023-04-27 00:43:16 +0000

    Validating key characteristics of generated/imported keys.
    Updated VTS tests to verify mgf-digests in key characteristics of
    RSA-OAEP keys. Added new tests to import RSA-OAEP keys with
    mgf-digests and verified imported key characteristics.
    
    Bug: 279721313
    Test: atest VtsAidlKeyMintTargetTest
    Change-Id: I06474a85c9e77fded264031ff5636f2c35bee6b4

--- security/keymint/aidl/vts/functional/KeyMintAidlTestBase.h
+++ security/keymint/aidl/vts/functional/KeyMintAidlTestBase.h
@@ -404,6 +404,11 @@ string bin2hex(const vector<uint8_t>& data);
 X509_Ptr parse_cert_blob(const vector<uint8_t>& blob);
 ASN1_OCTET_STRING* get_attestation_record(X509* certificate);
 vector<uint8_t> make_name_from_str(const string& name);
+void check_mgf_digests(
+        const vector<KeyCharacteristics>& key_characteristics,
+        std::vector<android::hardware::security::keymint::Digest>& expected_mgf_digests);
+bool is_mgf_digest_exists(const vector<KeyCharacteristics>& key_characteristics,
+                          android::hardware::security::keymint::Digest expected_mgf_digest);
 void check_maced_pubkey(const MacedPublicKey& macedPubKey, bool testMode,
                         vector<uint8_t>* payload_value);
 void p256_pub_key(const vector<uint8_t>& coseKeyData, EVP_PKEY_Ptr* signingKey);
