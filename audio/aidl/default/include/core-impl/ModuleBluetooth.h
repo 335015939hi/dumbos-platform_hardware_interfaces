00e64d59b338efd703e4b49300c0b64f76913d5c
Ram Mohan <ram.mohan@ittiam.com>
2023-07-01 00:47:09 +0530

    aosp aidl bluetooth parameter support
    Bug: 228804498
    Test: atest VtsHalAudioCoreTargetTest
    Change-Id: If1820018d5a6750eed0d0b486e15fc7c717aa11c

--- audio/aidl/default/include/core-impl/ModuleBluetooth.h
+++ audio/aidl/default/include/core-impl/ModuleBluetooth.h
@@ -16,6 +16,7 @@
 
 #pragma once
 
+#include "core-impl/Bluetooth.h"
 #include "core-impl/Module.h"
 
 namespace aidl::android::hardware::audio::core {
@@ -25,6 +26,13 @@ class ModuleBluetooth final : public Module {
     ModuleBluetooth() : Module(Type::BLUETOOTH) {}
 
   private:
+    ChildInterface<IBluetoothA2dp> mBluetoothA2dp;
+    ChildInterface<IBluetoothLe> mBluetoothLe;
+
+    BtProfileHandles getBtProfileManagerHandles() override;
+
+    ndk::ScopedAStatus getBluetoothA2dp(std::shared_ptr<IBluetoothA2dp>* _aidl_return) override;
+    ndk::ScopedAStatus getBluetoothLe(std::shared_ptr<IBluetoothLe>* _aidl_return) override;
     ndk::ScopedAStatus getMicMute(bool* _aidl_return) override;
     ndk::ScopedAStatus setMicMute(bool in_mute) override;
 
