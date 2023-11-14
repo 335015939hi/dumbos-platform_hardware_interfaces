5e4b1c491a08d5778eb77cae8898aa9ce77a9c9c
Ted Wang <tedwang@google.com>
2023-11-14 05:34:21 +0000

    Bluetooth Finder: Add Vts and AIDL default implement
    Bug: 307897345
    Bug: 310790725
    Test: atest VtsHalBluetoothFinderTargetTest
    Change-Id: I20eba31695627cde2fdb212657c952f74615ea46

---
+++ bluetooth/finder/aidl/default/service.cpp
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "aidl.android.hardware.bluetooth.finder.default"
+
+#include <android/binder_manager.h>
+#include <android/binder_process.h>
+#include <utils/Log.h>
+
+#include "BluetoothFinder.h"
+
+using ::aidl::android::hardware::bluetooth::finder::impl::BluetoothFinder;
+
+extern "C" __attribute__((visibility("default"))) binder_status_t
+RegisterBluetoothFinderService() {
+  ALOGI("Bluetooth Finder HAL registering");
+  std::shared_ptr<BluetoothFinder> service =
+      ndk::SharedRefBase::make<BluetoothFinder>();
+  std::string instance =
+      std::string() + BluetoothFinder::descriptor + "/default";
+  auto result =
+      AServiceManager_addService(service->asBinder().get(), instance.c_str());
+  ALOGW_IF(result != STATUS_OK, "Could not register as a service!");
+  return result;
+}
