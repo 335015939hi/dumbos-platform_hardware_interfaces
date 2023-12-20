7fd3207778f22ff71ed862d80172b44c1a188876
David Drysdale <drysdale@google.com>
2023-12-20 17:12:03 +0000

    Add vbmeta digest consistency check
    Calculate the vbmeta digest, and compare it to the value in the
    corresponding Android property.
    
    Note that this test does *not* involve KeyMint; it's purely a
    consistency check of the device's properties.
    
    Bug: 314044749
    Test: VtsAidlKeyMintTargetTest
    Change-Id: I7d62f124b9765f8d491ea7b89b4f0632a7ba00a2

--- security/keymint/aidl/vts/functional/BootloaderStateTest.cpp
+++ security/keymint/aidl/vts/functional/BootloaderStateTest.cpp
@@ -36,6 +36,96 @@ using ::android::getAidlHalInstanceNames;
 using ::std::string;
 using ::std::vector;
 
+namespace {
+
+std::optional<uint8_t> HexCharToInt(char c) {
+    if (c >= '0' && c <= '9') {
+        return c - '0';
+    } else if (c >= 'a' && c <= 'f') {
+        return (c - 'a') + 10;
+    } else if (c >= 'A' && c <= 'F') {
+        return (c - 'A') + 10;
+    } else {
+        return std::nullopt;
+    }
+}
+
+std::optional<std::vector<uint8_t>> FromHex(std::string_view hex_string) {
+    if (hex_string.size() % 2 == 1) {
+        return std::nullopt;
+    }
+    std::vector<uint8_t> out;
+    out.reserve(hex_string.size() / 2);
+    for (auto next = hex_string.begin(); next != hex_string.end(); next += 2) {
+        auto high_nibble = HexCharToInt(*next);
+        auto low_nibble = HexCharToInt(*(next + 1));
+        if (!high_nibble || !low_nibble) {
+            return std::nullopt;
+        }
+        out.push_back((*high_nibble << 4) | *low_nibble);
+    }
+    return out;
+}
+
+std::optional<std::vector<uint8_t>> GetVbmetaDigestProperty() {
+    auto vbmeta_string = ::android::base::GetProperty(
+            "ro.boot.vbmeta.digest",
+            /*default_value=*/"0000000000000000000000000000000000000000000000000000000000000000");
+    return FromHex(vbmeta_string);
+}
+
+// Following error codes from avb_slot_data() mean that slot data was loaded
+// (even if verification failed).
+inline bool avb_slot_data_loaded(AvbSlotVerifyResult result) {
+    switch (result) {
+        case AVB_SLOT_VERIFY_RESULT_OK:
+        case AVB_SLOT_VERIFY_RESULT_ERROR_VERIFICATION:
+        case AVB_SLOT_VERIFY_RESULT_ERROR_ROLLBACK_INDEX:
+        case AVB_SLOT_VERIFY_RESULT_ERROR_PUBLIC_KEY_REJECTED:
+            return true;
+        default:
+            return false;
+    }
+}
+
+}  // namespace
+
+// Check that the Android vbmeta digest property matches the calculated value.
+// This test does not involve KeyMint!
+TEST(VbmetaDigest, CalculatedMatchesProperty) {
+    AvbSlotVerifyData* avbSlotData;
+    auto suffix = fs_mgr_get_slot_suffix();
+    const char* partitions[] = {nullptr};
+    auto avbOps = avb_ops_user_new();
+
+    // For VTS, devices run with vendor_boot-debug.img, which is not release key
+    // signed. Use AVB_SLOT_VERIFY_FLAGS_ALLOW_VERIFICATION_ERROR to bypass avb
+    // verification errors. This is OK since we only care about the digest for
+    // this test case.
+    auto result = avb_slot_verify(avbOps, partitions, suffix.c_str(),
+                                  AVB_SLOT_VERIFY_FLAGS_ALLOW_VERIFICATION_ERROR,
+                                  AVB_HASHTREE_ERROR_MODE_EIO, &avbSlotData);
+    ASSERT_TRUE(avb_slot_data_loaded(result)) << "Failed to load avb slot data";
+
+    // Unfortunately, bootloader is not required to report the algorithm used
+    // to calculate the digest. There are only two supported options though,
+    // SHA256 and SHA512. Attested VBMeta digest must match one of these.
+    vector<uint8_t> digest256(AVB_SHA256_DIGEST_SIZE);
+    vector<uint8_t> digest512(AVB_SHA512_DIGEST_SIZE);
+
+    avb_slot_verify_data_calculate_vbmeta_digest(avbSlotData, AVB_DIGEST_TYPE_SHA256,
+                                                 digest256.data());
+    avb_slot_verify_data_calculate_vbmeta_digest(avbSlotData, AVB_DIGEST_TYPE_SHA512,
+                                                 digest512.data());
+
+    // Get the vbmeta digest from the Android property.
+    auto vbmeta_digest = GetVbmetaDigestProperty();
+    ASSERT_TRUE((vbmeta_digest == digest256) || (vbmeta_digest == digest512))
+            << "vbmeta digest from property (" << bin2hex(vbmeta_digest.value())
+            << ") does not match computed digest (sha256: " << bin2hex(digest256)
+            << ", sha512: " << bin2hex(digest512) << ").  Note: **no KeyMint involvement**";
+}
+
 // Since this test needs to talk to KeyMint HAL, it can only run as root. Thus,
 // bootloader can not be locked.
 class BootloaderStateTest : public KeyMintAidlTestBase {
@@ -105,20 +195,6 @@ TEST_P(BootloaderStateTest, VbStateIsUnverified) {
     auto vbStateProp = ::android::base::GetProperty("ro.boot.verifiedbootstate", "");
     ASSERT_EQ(vbStateProp, "orange")
             << "Verified boot state must be \"UNVERIFIED\" aka \"orange\".";
-}
-
-// Following error codes from avb_slot_data() mean that slot data was loaded
-// (even if verification failed).
-static inline bool avb_slot_data_loaded(AvbSlotVerifyResult result) {
-    switch (result) {
-        case AVB_SLOT_VERIFY_RESULT_OK:
-        case AVB_SLOT_VERIFY_RESULT_ERROR_VERIFICATION:
-        case AVB_SLOT_VERIFY_RESULT_ERROR_ROLLBACK_INDEX:
-        case AVB_SLOT_VERIFY_RESULT_ERROR_PUBLIC_KEY_REJECTED:
-            return true;
-        default:
-            return false;
-    }
 }
 
 // Check that attested vbmeta digest is correct.
