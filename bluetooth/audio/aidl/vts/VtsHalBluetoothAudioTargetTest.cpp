3b9a8b5c924ea4856d61ff400927b18135cbdc8a
Bao Do <quocbaodo@google.com>
2023-12-20 15:31:02 +0800

    Fix test run for GSI
    GSI used mixed testing procedures, making HFP session unavailable
    when testing with the latest VTS. This fix allow optional checking
    of HFP session provider.
    
    Bug: 315338603
    Test: atest VtsHalBluetoothAudioTargetTest
    Change-Id: Idb0a780a67857c76c93b13f7b3a64436f6fc647f

--- bluetooth/audio/aidl/vts/VtsHalBluetoothAudioTargetTest.cpp
+++ bluetooth/audio/aidl/vts/VtsHalBluetoothAudioTargetTest.cpp
@@ -623,8 +623,6 @@ class BluetoothAudioProviderFactoryAidl
       SessionType::LE_AUDIO_BROADCAST_HARDWARE_OFFLOAD_ENCODING_DATAPATH,
       SessionType::A2DP_SOFTWARE_DECODING_DATAPATH,
       SessionType::A2DP_HARDWARE_OFFLOAD_DECODING_DATAPATH,
-      SessionType::HFP_SOFTWARE_ENCODING_DATAPATH,
-      SessionType::HFP_SOFTWARE_DECODING_DATAPATH,
   };
 };
 
@@ -1505,7 +1503,6 @@ class BluetoothAudioProviderHfpSoftwareEncodingAidl
     BluetoothAudioProviderFactoryAidl::SetUp();
     GetProviderCapabilitiesHelper(SessionType::HFP_SOFTWARE_ENCODING_DATAPATH);
     OpenProviderHelper(SessionType::HFP_SOFTWARE_ENCODING_DATAPATH);
-    ASSERT_NE(audio_provider_, nullptr);
   }
 
   virtual void TearDown() override {
@@ -1549,6 +1546,10 @@ TEST_P(BluetoothAudioProviderHfpSoftwareEncodingAidl,
  */
 TEST_P(BluetoothAudioProviderHfpSoftwareEncodingAidl,
        StartAndEndHfpEncodingSoftwareSessionWithPossiblePcmConfig) {
+  // No test run when can't open provider
+  // This ensure the test will run correctly when testing
+  // Newer VTS with older HAL version
+  if (audio_provider_ == nullptr) return;
   for (auto sample_rate : hfp_sample_rates_) {
     for (auto bits_per_sample : hfp_bits_per_samples_) {
       for (auto channel_mode : hfp_channel_modes_) {
@@ -1572,7 +1573,6 @@ class BluetoothAudioProviderHfpSoftwareDecodingAidl
     BluetoothAudioProviderFactoryAidl::SetUp();
     GetProviderCapabilitiesHelper(SessionType::HFP_SOFTWARE_DECODING_DATAPATH);
     OpenProviderHelper(SessionType::HFP_SOFTWARE_DECODING_DATAPATH);
-    ASSERT_NE(audio_provider_, nullptr);
   }
 
   virtual void TearDown() override {
@@ -1613,6 +1613,10 @@ TEST_P(BluetoothAudioProviderHfpSoftwareDecodingAidl,
  */
 TEST_P(BluetoothAudioProviderHfpSoftwareDecodingAidl,
        StartAndEndHfpDecodingSoftwareSessionWithPossiblePcmConfig) {
+  // No test run when can't open provider
+  // This ensure the test will run correctly when testing
+  // Newer VTS with older HAL version
+  if (audio_provider_ == nullptr) return;
   for (auto sample_rate : hfp_sample_rates_) {
     for (auto bits_per_sample : hfp_bits_per_samples_) {
       for (auto channel_mode : hfp_channel_modes_) {
@@ -1929,6 +1933,10 @@ TEST_P(BluetoothAudioProviderHfpHardwareAidl, OpenHfpHardwareProvider) {}
  */
 TEST_P(BluetoothAudioProviderHfpHardwareAidl,
        StartAndEndHfpHardwareSessionWithPossiblePcmConfig) {
+  // No test run when can't open provider
+  // This ensure the test will run correctly when testing
+  // Newer VTS with older HAL version
+  if (audio_provider_ == nullptr) return;
   // Try to open with a sample configuration
   EXPECT_TRUE(OpenSession(CodecId::Core::CVSD, 6, false, true));
   EXPECT_TRUE(audio_provider_->endSession().isOk());
