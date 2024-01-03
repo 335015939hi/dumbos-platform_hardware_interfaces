cf8a938091a42ecddf931dd86c8cb98ab19920fb
Shunkai Yao <yaoshunkai@google.com>
2024-01-03 18:27:45 +0000

    Audio Effect VTS: run Dowmmix data path test only after HAL version 2
    Bug: 315074570
    Test: VtsHalDownmixTargetTest
    Change-Id: Iaa17519521f136901ec217f7d5b5fd0d03889533

--- audio/aidl/vts/VtsHalDownmixTargetTest.cpp
+++ audio/aidl/vts/VtsHalDownmixTargetTest.cpp
@@ -32,6 +32,9 @@ using aidl::android::hardware::audio::effect::Parameter;
 using android::audio_utils::channels::ChannelMix;
 using android::hardware::audio::common::testing::detail::TestExecutionTracer;
 
+// minimal HAL interface version to run downmix data path test
+constexpr int32_t kMinDataTestHalVersion = 2;
+
 // Testing for enum values
 static const std::vector<Downmix::Type> kTypeValues = {ndk::enum_range<Downmix::Type>().begin(),
                                                        ndk::enum_range<Downmix::Type>().end()};
@@ -228,6 +231,10 @@ class DownmixFoldDataTest : public ::testing::TestWithParam<DownmixDataTestParam
 
     void SetUp() override {
         SetUpDownmix(mInputChannelLayout);
+        if (int32_t version;
+            mEffect->getInterfaceVersion(&version).isOk() && version < kMinDataTestHalVersion) {
+            GTEST_SKIP() << "Skipping the data test for version: " << version << "\n";
+        }
         if (!isLayoutValid(mInputChannelLayout)) {
             GTEST_SKIP() << "Layout not supported \n";
         }
@@ -375,6 +382,10 @@ class DownmixStripDataTest : public ::testing::TestWithParam<DownmixStripDataTes
 
     void SetUp() override {
         SetUpDownmix(mInputChannelLayout);
+        if (int32_t version;
+            mEffect->getInterfaceVersion(&version).isOk() && version < kMinDataTestHalVersion) {
+            GTEST_SKIP() << "Skipping the data test for version: " << version << "\n";
+        }
         if (!isLayoutValid(mInputChannelLayout)) {
             GTEST_SKIP() << "Layout not supported \n";
         }
@@ -451,7 +462,7 @@ INSTANTIATE_TEST_SUITE_P(
             auto descriptor = std::get<STRIP_INSTANCE_NAME>(info.param).second;
             std::string layout =
                     std::to_string(static_cast<int>(std::get<STRIP_INPUT_LAYOUT>(info.param)));
-            std::string name = getPrefix(descriptor) + "_strip" + "_layout" + layout;
+            std::string name = getPrefix(descriptor) + "_strip" + "_layout_" + layout;
             std::replace_if(
                     name.begin(), name.end(), [](const char c) { return !std::isalnum(c); }, '_');
             return name;
