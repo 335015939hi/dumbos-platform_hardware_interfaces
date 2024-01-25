4298b3348cc157fc9cc0f08024d1f8655f2ff761
David Drysdale <drysdale@google.com>
2024-01-25 18:54:56 +0000

    Secretkeeper: diagnostic DICE display
    Also add a dump-bcc command.
    
    Test: secretkeeper_cli --dice get
    Test: secretkeeper_cli dump-bcc bcc_good
    Change-Id: I97b23f89e00a7c288ffae9a80fb9c89369ef72d3

--- security/secretkeeper/aidl/vts/lib.rs
+++ security/secretkeeper/aidl/vts/lib.rs
@@ -16,26 +16,56 @@
 
 //! Test helper functions.
 
+pub mod diags;
 pub mod dice_sample;
 
-// Constants for DICE map keys.
+// Constants for DICE map keys from the Open Profile for DICE; all bstr unless indicated.
 
-/// Map key for authority hash.
-pub const AUTHORITY_HASH: i64 = -4670549;
+/// Map key for code hash (bstr).
+pub const CODE_HASH: i64 = -4670545;
+/// Map key for code descriptor.
+pub const CODE_DESC: i64 = -4670546;
+/// Map key for config hash (bstr).
+pub const CONFIG_HASH: i64 = -4670547;
 /// Map key for config descriptor.
 pub const CONFIG_DESC: i64 = -4670548;
-/// Map key for component name.
-pub const COMPONENT_NAME: i64 = -70002;
-/// Map key for component version.
-pub const COMPONENT_VERSION: i64 = -70003;
-/// Map key for Resettable.
-pub const COMPONENT_RESETTABLE: i64 = -70004;
-/// Map key for security version.
-pub const SECURITY_VERSION: i64 = -70005;
-/// Map key for mode.
+/// Map key for authority hash (bstr).
+pub const AUTHORITY_HASH: i64 = -4670549;
+/// Map key for authority descriptor (bstr).
+pub const AUTHORITY_DESCRIPTOR: i64 = -4670550;
+/// Map key for mode (bstr).
 pub const MODE: i64 = -4670551;
-/// Map key for SubcomponentDescriptor.
+/// Map key for subject public key (bstr .cbor COSE_Key).
+pub const SUBJECT_PUBLIC_KEY: i64 = -4670552;
+/// Map key for key usage (bstr).
+pub const KEY_USAGE: i64 = -4670553;
+/// Map key for profile name (tstr).
+pub const PROFILE_NAME: i64 = -4670554;
+
+// Constants for DICE map keys in RKP.
+
+/// Map key for component name (tstr).
+pub const COMPONENT_NAME: i64 = -70002;
+/// Map key for component version (int/tstr).
+pub const COMPONENT_VERSION: i64 = -70003;
+/// Map key for resettable (null).
+pub const COMPONENT_RESETTABLE: i64 = -70004;
+/// Map key for security version (uint).
+pub const SECURITY_VERSION: i64 = -70005;
+/// Map key for RKP VM marker (null).
+pub const RKP_VM: i64 = -70006;
+
+// Additional constants for DICE map keys for RKP VMs.
+
+/// Map key for payload config filename (tstr).
+pub const PAYLOAD_CONFIG_FILENAME: i64 = -71000;
+/// Map key for payload config (PayloadConfig).
+pub const PAYLOAD_CONFIG: i64 = -71001;
+/// Map key for subcomponent descriptor (array of SubcomponentDescriptor).
 pub const SUBCOMPONENT_DESCRIPTORS: i64 = -71002;
+
+// Map keys for SubcomponentDescriptor.
+
 /// Map key for name of subcomponent.
 pub const SUBCOMPONENT_NAME: i64 = 1;
 /// Map key for Security Version of subcomponent.
