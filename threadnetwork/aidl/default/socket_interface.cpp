5a2cf5822c248d7ed7d07c7c91ba425d8dadf28e
shihchienc <shihchienc@google.com>
2024-01-29 08:35:15 +0000

    [Thread] Implement init/deinit socket interface
    Bug: 313425570
    Test: build pass & manual test
    Change-Id: I34388f76972f9b88f390a04b100c2fbf61019a1d

--- threadnetwork/aidl/default/socket_interface.cpp
+++ threadnetwork/aidl/default/socket_interface.cpp
@@ -22,6 +22,17 @@
 
 #include "socket_interface.hpp"
 
+#include <errno.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/un.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "common/code_utils.hpp"
+#include "lib/platform/exit_code.h"
+#include "openthread/openthread-system.h"
+
 namespace aidl {
 namespace android {
 namespace hardware {
@@ -35,6 +46,91 @@ SocketInterface::SocketInterface(const ot::Url::Url& aRadioUrl)
       mRadioUrl(aRadioUrl) {
     memset(&mInterfaceMetrics, 0, sizeof(mInterfaceMetrics));
     mInterfaceMetrics.mRcpInterfaceType = kSpinelInterfaceTypeVendor;
+}
+
+otError SocketInterface::Init(ReceiveFrameCallback aCallback, void* aCallbackContext,
+                              RxFrameBuffer& aFrameBuffer) {
+    otError error = OT_ERROR_NONE;
+    struct stat st;
+
+    VerifyOrExit(mSockFd == -1, error = OT_ERROR_ALREADY);
+
+    VerifyOrDie(stat(mRadioUrl.GetPath(), &st) == 0, OT_EXIT_ERROR_ERRNO);
+
+    if (S_ISSOCK(st.st_mode)) {
+        mSockFd = OpenFile(mRadioUrl);
+        VerifyOrExit(mSockFd != -1, error = OT_ERROR_FAILED);
+    } else {
+        otLogCritPlat("Radio file '%s' not supported", mRadioUrl.GetPath());
+        ExitNow(error = OT_ERROR_FAILED);
+    }
+
+    mReceiveFrameCallback = aCallback;
+    mReceiveFrameContext = aCallbackContext;
+    mReceiveFrameBuffer = &aFrameBuffer;
+
+exit:
+    return error;
+}
+
+SocketInterface::~SocketInterface(void) {
+    Deinit();
+}
+
+void SocketInterface::Deinit(void) {
+    CloseFile();
+
+    mReceiveFrameCallback = nullptr;
+    mReceiveFrameContext = nullptr;
+    mReceiveFrameBuffer = nullptr;
+}
+
+void SocketInterface::UpdateFdSet(void* aMainloopContext) {
+    otSysMainloopContext* context = reinterpret_cast<otSysMainloopContext*>(aMainloopContext);
+
+    assert(context != nullptr);
+
+    FD_SET(mSockFd, &context->mReadFdSet);
+
+    if (context->mMaxFd < mSockFd) {
+        context->mMaxFd = mSockFd;
+    }
+}
+
+int SocketInterface::OpenFile(const ot::Url::Url& aRadioUrl) {
+    int fd = -1;
+    sockaddr_un server_address;
+
+    VerifyOrExit(sizeof(server_address.sun_path) > strlen(aRadioUrl.GetPath()),
+                 otLogCritPlat("Invalid file path legnth"));
+    strncpy(server_address.sun_path, aRadioUrl.GetPath(), sizeof(server_address.sun_path));
+    server_address.sun_family = AF_UNIX;
+
+    fd = socket(AF_UNIX, SOCK_SEQPACKET, 0);
+    VerifyOrExit(fd != -1, otLogCritPlat("open(): errno=%s", strerror(errno)));
+
+    if (connect(fd, reinterpret_cast<struct sockaddr*>(&server_address), sizeof(server_address)) ==
+        -1) {
+        otLogCritPlat("connect(): errno=%s", strerror(errno));
+        close(fd);
+        fd = -1;
+    }
+
+exit:
+    return fd;
+}
+
+void SocketInterface::CloseFile(void) {
+    VerifyOrExit(mSockFd != -1);
+
+    VerifyOrExit(0 == close(mSockFd), otLogCritPlat("close(): errno=%s", strerror(errno)));
+    VerifyOrExit(wait(nullptr) != -1 || errno == ECHILD,
+                 otLogCritPlat("wait(): errno=%s", strerror(errno)));
+
+    mSockFd = -1;
+
+exit:
+    return;
 }
 
 }  // namespace threadnetwork
