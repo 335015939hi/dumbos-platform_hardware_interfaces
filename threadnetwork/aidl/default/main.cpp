d971c7ae086b7d5deb09ee77464d6684543d3323
Zhanglong Xia <zhanglongxia@google.com>
2024-02-01 16:16:08 +0800

    Add log level control mechanism to Thread HAL service
    This CL adds the log level control mechanism to allow developers
    to specify the log level of the HAL service.
    
    Bug: b/323300157
    Test: Set the `debug-level` parameter, then check the output logs.
    Change-Id: Ifb1ec446f7844dcbd70b89d1adbee43d9e6144a6

--- threadnetwork/aidl/default/main.cpp
+++ threadnetwork/aidl/default/main.cpp
@@ -18,35 +18,80 @@
 #include <android-base/logging.h>
 #include <android/binder_manager.h>
 #include <android/binder_process.h>
+#include <getopt.h>
 #include <utils/Log.h>
 
+#include "log.hpp"
 #include "service.hpp"
 #include "thread_chip.hpp"
 
 using aidl::android::hardware::threadnetwork::IThreadChip;
 using aidl::android::hardware::threadnetwork::ThreadChip;
 
+static void dumpUsage(const char* aProgramName) {
+    logCrit("Syntax:\n"
+            "    %s [Options] RadioURL [RadioURL]\n"
+            "Options:\n"
+            "    -d  --debug-level   Debug level of logging\n"
+            "                        (3: Debug, 4:Info, 5:Warn, 6: Error, 7: Crit)\n",
+            aProgramName);
+}
+
+static std::shared_ptr<ThreadChip> createThreadChipService(int identifier, char* url) {
+    const std::string serviceName(std::string() + IThreadChip::descriptor + "/chip" +
+                                  std::to_string(identifier));
+
+    logInfo("ServiceName: %s", serviceName.c_str());
+    logInfo("Url: %s", url);
+
+    auto threadChip = ndk::SharedRefBase::make<ThreadChip>(url);
+    CHECK_NE(threadChip, nullptr);
+
+    auto status = AServiceManager_addService(threadChip->asBinder().get(), serviceName.c_str());
+    CHECK_EQ(status, STATUS_OK);
+
+    return threadChip;
+}
+
 int main(int argc, char* argv[]) {
-    CHECK_GT(argc, 1);
+    constexpr struct option kOptions[] = {{"debug-level", required_argument, NULL, 'd'},
+                                          {0, 0, 0, 0}};
     std::vector<std::shared_ptr<ThreadChip>> threadChips;
     aidl::android::hardware::threadnetwork::Service service;
+    int identifier = 0;
 
-    for (int id = 0; id < argc - 1; id++) {
-        binder_status_t status;
-        const std::string serviceName(std::string() + IThreadChip::descriptor + "/chip" +
-                                      std::to_string(id));
-        auto threadChip = ndk::SharedRefBase::make<ThreadChip>(argv[id + 1]);
+    optind = 1;
 
-        CHECK_NE(threadChip, nullptr);
+    while (true) {
+        int index = 0;
+        int option = getopt_long(argc, argv, "d:", kOptions, &index);
 
-        status = AServiceManager_addService(threadChip->asBinder().get(), serviceName.c_str());
-        CHECK_EQ(status, STATUS_OK);
+        if (option == -1) {
+            break;
+        }
 
-        ALOGI("ServiceName: %s, Url: %s", serviceName.c_str(), argv[id + 1]);
+        switch (option) {
+            case 'd':
+                setLogLevel(static_cast<android_LogPriority>(atoi(optarg)));
+                break;
+            default:
+                dumpUsage(argv[0]);
+                return EXIT_FAILURE;
+        }
+    }
+
+    if (optind >= argc) {
+        logCrit("No radio URL is specified!");
+        dumpUsage(argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    for (; optind < argc; optind++) {
+        auto threadChip = createThreadChipService(identifier++, argv[optind]);
         threadChips.push_back(std::move(threadChip));
     }
 
-    ALOGI("Thread Network HAL is running");
+    logInfo("Thread Network HAL is running");
 
     service.startLoop();
     return EXIT_FAILURE;  // should not reach
