f4f67c8ba8511e9affc2622b2a80364d760d86a8
Yuyang Huang <yuyanghuang@google.com>
2024-02-12 19:56:37 +0900

    Update VTS to exempt low-power TVs from APF
    This change updates the VTS to exempt TV devices that consume <= 2W of
    standby power from APF requirements. This update aligns with latest GTVS
    policy.
    
    doc: https://docs.partner.android.com/tv/requirements/gtvs/android-14
    
    Bug: 306587099
    Test: TH
    Change-Id: Ifec617520db20d1ef61f1eca63b7160d9191f446

--- wifi/aidl/vts/functional/wifi_sta_iface_aidl_test.cpp
+++ wifi/aidl/vts/functional/wifi_sta_iface_aidl_test.cpp
@@ -74,16 +74,25 @@ class WifiStaIfaceAidlTest : public testing::TestWithParam<std::string> {
         return testing::deviceSupportsFeature("com.google.android.tv.mdns_offload");
     }
 
-    // Detected panel TV device by using ro.oem.key1 property.
+    bool doesDeviceSupportFullNetworkingUnder2w() {
+        return testing::deviceSupportsFeature("com.google.android.tv.full_networking_under_2w");
+    }
+
+    // Detected TV devices by using ro.oem.key1 property.
     // https://docs.partner.android.com/tv/build/platform/props-vars/ro-oem-key1
-    bool isPanelTvDevice() {
-        const std::string oem_key1 = getPropertyString("ro.oem.key1");
+    bool isTvDevice(const std::string oem_key1) {
         if (oem_key1.size() < 9) {
             return false;
         }
         if (oem_key1.substr(0, 3) != "ATV") {
             return false;
         }
+        return true;
+    }
+
+    // Detected panel TV device by using ro.oem.key1 property.
+    // https://docs.partner.android.com/tv/build/platform/props-vars/ro-oem-key1
+    bool isPanelTvDevice(const std::string oem_key1) {
         const std::string psz_string = oem_key1.substr(6, 3);
         // If PSZ string contains non digit, then it is not a panel TV device.
         for (char ch : psz_string) {
@@ -135,10 +144,23 @@ TEST_P(WifiStaIfaceAidlTest, GetFeatureSet) {
  */
 // @VsrTest = 5.3.12
 TEST_P(WifiStaIfaceAidlTest, CheckApfIsSupported) {
-    // Flat panel TV devices that support MDNS offload do not have to implement APF if the WiFi
-    // chipset does not have sufficient RAM to do so.
-    if (isPanelTvDevice() && isMdnsOffloadPresentInNIC()) {
-        GTEST_SKIP() << "Panel TV supports mDNS offload. It is not required to support APF";
+    const std::string oem_key1 = getPropertyString("ro.oem.key1");
+    if (isTvDevice(oem_key1)) {
+        // Flat panel TV devices that support MDNS offload do not have to implement APF if the WiFi
+        // chipset does not have sufficient RAM to do so.
+        if (isPanelTvDevice(oem_key1) && isMdnsOffloadPresentInNIC()) {
+            GTEST_SKIP() << "Panel TV supports mDNS offload. It is not required to support APF";
+        }
+        // For TV devices declaring the
+        // com.google.android.tv.full_networking_under_2w feature, this indicates
+        // the device can meet the <= 2W standby power requirement while
+        // continuously processing network packets on the CPU, even in standby mode.
+        // In these cases, APF support is strongly recommended rather than being
+        // mandatory.
+        if (doesDeviceSupportFullNetworkingUnder2w()) {
+            GTEST_SKIP() << "TV Device meets the <= 2W standby power demand requirement. It is not "
+                            "required to support APF.";
+        }
     }
     int vendor_api_level = property_get_int32("ro.vendor.api_level", 0);
     // Before VSR 14, APF support is optional.
