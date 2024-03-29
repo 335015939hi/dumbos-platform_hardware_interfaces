e16aa4dc8d2110a780819171e7fbd400dea8d467
François Gaffie <francois.gaffie@renault.com>
2024-03-29 16:07:46 +0100

    Add missing Cap Settings aidl support and helper
    Bug: 307310023
    Test: manual
    
    Change-Id: I08c71ce9a67f065df36f71c0602e2c9e4e97ae6b
    Signed-off-by: François Gaffie <francois.gaffie@renault.com>

--- audio/aidl/default/include/core-impl/XsdcConversion.h
+++ audio/aidl/default/include/core-impl/XsdcConversion.h
@@ -15,15 +15,25 @@
 
 namespace aidl::android::hardware::audio::core::internal {
 
-ConversionResult<::aidl::android::media::audio::common::AudioHalCapCriterion>
-convertCapCriterionToAidl(
-        const ::android::audio::policy::engine::configuration::CriterionType& xsdcCriterion);
-ConversionResult<::aidl::android::media::audio::common::AudioHalCapCriterionType>
-convertCapCriterionTypeToAidl(
-        const ::android::audio::policy::engine::configuration::CriterionTypeType&
-                xsdcCriterionType);
-ConversionResult<::aidl::android::media::audio::common::AudioHalVolumeCurve::CurvePoint>
+namespace engineconfiguration = ::android::audio::policy::engine::configuration;
+namespace aidlaudiocommon = ::aidl::android::media::audio::common;
+
+ConversionResult<aidlaudiocommon::AudioHalCapCriterion>
+convertCapCriterionToAidl(const engineconfiguration::CriterionType& xsdcCriterion);
+ConversionResult<aidlaudiocommon::AudioHalCapCriterionType>
+convertCapCriterionTypeToAidl(const engineconfiguration::CriterionTypeType& xsdcCriterionType);
+ConversionResult<aidlaudiocommon::AudioHalVolumeCurve::CurvePoint>
 convertCurvePointToAidl(const std::string& xsdcCurvePoint);
 ConversionResult<std::unique_ptr<Module::Configuration>> convertModuleConfigToAidl(
         const ::android::audio::policy::configuration::Modules::Module& moduleConfig);
+ConversionResult<aidlaudiocommon::AudioUsage>
+        convertAudioUsageToAidl(const engineconfiguration::UsageEnumType& xsdcUsage);
+ConversionResult<aidlaudiocommon::AudioContentType>
+        convertAudioContentTypeToAidl(const engineconfiguration::ContentType& xsdcContentType);
+ConversionResult<aidlaudiocommon::AudioSource>
+        convertAudioSourceToAidl(const engineconfiguration::SourceEnumType& xsdcSourceType);
+ConversionResult<aidlaudiocommon::AudioStreamType>
+        convertAudioStreamTypeToAidl(const engineconfiguration::Stream& xsdStreamType);
+ConversionResult<int32_t> convertAudioFlagsToAidl(
+        const std::vector<engineconfiguration::FlagType>& xsdcFlagTypeVec);
 }  // namespace aidl::android::hardware::audio::core::internal
