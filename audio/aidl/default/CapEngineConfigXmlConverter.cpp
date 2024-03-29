6a7970b4bd2cb3cad0975cad33684264e470d776
François Gaffie <francois.gaffie@renault.com>
2024-03-29 16:07:46 +0100

    Add missing Cap Settings aidl support and helper
    Bug: 307310023
    Test: manual
    
    Change-Id: I08c71ce9a67f065df36f71c0602e2c9e4e97ae6b
    Signed-off-by: François Gaffie <francois.gaffie@renault.com>

---
+++ audio/aidl/default/CapEngineConfigXmlConverter.cpp
@@ -0,0 +1,222 @@
+/*
+* Copyright (C) 2024 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#define LOG_TAG "AHAL_Config"
+
+#include <aidl/android/media/audio/common/AudioProductStrategyType.h>
+#include <android-base/logging.h>
+
+#include "core-impl/CapEngineConfigXmlConverter.h"
+#include "core-impl/XsdcConversion.h"
+
+using aidl::android::media::audio::common::AudioHalCapConfiguration;
+using aidl::android::media::audio::common::AudioHalCapDomain;
+using aidl::android::media::audio::common::AudioHalCapSetting;
+using aidl::android::media::audio::common::AudioHalCapParameter;
+
+using ::android::BAD_VALUE;
+using ::android::base::unexpected;
+
+namespace eng_xsd = android::audio::policy::capengine::configuration;
+
+namespace aidl::android::hardware::audio::core::internal {
+
+std::optional<std::vector<std::optional<AudioHalCapDomain>>>&
+        CapEngineConfigXmlConverter::getAidlCapEngineConfig() {
+    return mAidlCapDomains;
+}
+
+ConversionResult<std::string> convertRule(const eng_xsd::CompoundRuleType& xsdcCompoundRule)
+{
+    bool isPreviousCompoundRule = true;
+    std::string rule {};
+    rule += eng_xsd::toString(xsdcCompoundRule.getType()) + "{";
+
+    for (const auto& childXsdcCoumpoundRule : xsdcCompoundRule.getCompoundRule_optional()) {
+        if (childXsdcCoumpoundRule.hasCompoundRule_optional()) {
+            rule += (isPreviousCompoundRule ? "" : " , ");
+            rule += VALUE_OR_FATAL(convertRule(childXsdcCoumpoundRule));
+        } else if (childXsdcCoumpoundRule.hasSelectionCriterionRule_optional()) {
+            rule += VALUE_OR_FATAL(convertRule(childXsdcCoumpoundRule));
+        }
+        isPreviousCompoundRule = false;
+    }
+    if (xsdcCompoundRule.hasSelectionCriterionRule_optional()) {
+        for (const auto &criterionRule: xsdcCompoundRule.getSelectionCriterionRule_optional()) {
+            if (!isPreviousCompoundRule) {
+                rule += " , ";
+            }
+            isPreviousCompoundRule = false;
+            std::string selectionCriterion = criterionRule.getSelectionCriterion();
+            std::string matchesWhen = eng_xsd::toString(criterionRule.getMatchesWhen());
+            std::string value = criterionRule.getValue();
+            rule += " " + selectionCriterion + " " + matchesWhen + " " + value + " ";
+        }
+    }
+    rule += "}";
+    return rule;
+}
+
+ConversionResult<AudioHalCapConfiguration> CapEngineConfigXmlConverter::convertConfigurationToAidl(
+        const eng_xsd::ConfigurationsType::Configuration& xsdcConfiguration) {
+    AudioHalCapConfiguration aidlCapConfiguration;
+    aidlCapConfiguration.name = xsdcConfiguration.getName();
+    std::string rule {};
+    if (xsdcConfiguration.hasCompoundRule()) {
+        for (const auto& compoundRule : xsdcConfiguration.getCompoundRule()) {
+            rule += VALUE_OR_FATAL(convertRule(compoundRule));
+        }
+    }
+    aidlCapConfiguration.rule = rule;
+    return aidlCapConfiguration;
+}
+
+ConversionResult<std::string> CapEngineConfigXmlConverter::convertConfigurableElementToAidl(
+        const eng_xsd::ConfigurableElementsType::ConfigurableElement& xsdcConfigurableElement) {
+    std::string aidlConfigurableElement;
+    aidlConfigurableElement = xsdcConfigurableElement.getPath();
+    return aidlConfigurableElement;
+}
+
+template <typename ParamType>
+ConversionResult<AudioHalCapSetting::ParameterSetting>
+CapEngineConfigXmlConverter::convertParamToAidl(const ParamType& xsdcTypedParam,
+        const eng_xsd::ConfigurableElementSettingsType& element) {
+    const auto& path = element.getPath();
+    const auto& name = xsdcTypedParam.getName();
+    const auto& value = xsdcTypedParam.getValue();
+    if (path.find(eng_xsd::toString(name)) == std::string::npos) {
+        return unexpected(BAD_VALUE);
+    }
+    AudioHalCapParameter aidlParamName = static_cast<AudioHalCapParameter>(name);
+    std::string xsdcNameLiteral = eng_xsd::toString(name);
+    std::string aidlParamNameLiteral;
+    std::transform(xsdcNameLiteral.begin(), xsdcNameLiteral.end(), xsdcNameLiteral.begin(),
+                   [](char c) { return std::toupper(c); });
+    if (xsdcNameLiteral != toString(aidlParamName)) {
+        return unexpected(BAD_VALUE);
+    }
+    return AudioHalCapSetting::ParameterSetting{path, aidlParamName, value};
+}
+
+ConversionResult<AudioHalCapSetting> CapEngineConfigXmlConverter::convertSettingToAidl(
+        const eng_xsd::SettingsType::Configuration& xsdcSetting) {
+    AudioHalCapSetting aidlCapSetting;
+    aidlCapSetting.configurationName = xsdcSetting.getName();
+    for (const auto& element : xsdcSetting.getConfigurableElement()) {
+        if (element.hasBooleanParameter_optional()) {
+            const auto* xsdcParam =  element.getFirstBooleanParameter_optional();
+            const auto param = VALUE_OR_FATAL(convertParamToAidl(*xsdcParam, element));
+            aidlCapSetting.parameterSettings.push_back(param);
+        } else if (element.hasIntegerParameter_optional()) {
+            const auto* xsdcParam = element.getFirstIntegerParameter_optional();
+            const auto param = VALUE_OR_FATAL(convertParamToAidl(*xsdcParam, element));
+            aidlCapSetting.parameterSettings.push_back(param);
+        } else if (element.hasEnumParameter_optional()) {
+            const auto* xsdcParam = element.getFirstEnumParameter_optional();
+            const auto param = VALUE_OR_FATAL(convertParamToAidl(*xsdcParam, element));
+            aidlCapSetting.parameterSettings.push_back(param);
+        } else if (element.hasFixedPointParameter_optional()) {
+            const auto* xsdcParam = element.getFirstFloatingPointParameter_optional();
+            const auto param = VALUE_OR_FATAL(convertParamToAidl(*xsdcParam, element));
+            aidlCapSetting.parameterSettings.push_back(param);
+        } else if (element.hasFloatingPointParameter_optional()) {
+            const auto* xsdcParam = element.getFirstIntegerParameter_optional();
+            const auto param = VALUE_OR_FATAL(convertParamToAidl(*xsdcParam, element));
+            aidlCapSetting.parameterSettings.push_back(param);
+        }  else if (element.hasBitParameter_optional()) {
+            const auto* xsdcParam = element.getFirstBitParameter_optional();
+            const auto param = VALUE_OR_FATAL(convertParamToAidl(*xsdcParam, element));
+            aidlCapSetting.parameterSettings.push_back(param);
+        } else if (element.hasBitParameterBlock_optional()) {
+            const auto* xsdcParam = element.getFirstBitParameterBlock_optional();
+            if (xsdcParam->hasBitParameter()) {
+                const auto* xsdcBitParam = xsdcParam->getFirstBitParameter();
+                const auto param = VALUE_OR_FATAL(convertParamToAidl(*xsdcBitParam, element));
+                aidlCapSetting.parameterSettings.push_back(param);
+            } else {
+                return unexpected(BAD_VALUE);
+            }
+        } else if (element.hasStringParameter_optional()) {
+            const auto* xsdcParam = element.getFirstStringParameter_optional();
+            const auto param = VALUE_OR_FATAL(convertParamToAidl(*xsdcParam, element));
+            aidlCapSetting.parameterSettings.push_back(param);
+        } else if (element.hasParameterBlock_optional()) {
+            return unexpected(BAD_VALUE);
+        }
+    }
+    return aidlCapSetting;
+}
+
+ConversionResult<AudioHalCapDomain> CapEngineConfigXmlConverter::convertConfigurableDomainToAidl(
+        const eng_xsd::ConfigurableDomainType& xsdcConfigurableDomain) {
+    AudioHalCapDomain aidlConfigurableDomain;
+
+    aidlConfigurableDomain.name = xsdcConfigurableDomain.getName();
+    if (xsdcConfigurableDomain.hasSequenceAware() && xsdcConfigurableDomain.getSequenceAware()) {
+        ALOGE("%s sequence aware not supported", __func__);
+        return unexpected(BAD_VALUE);
+    }
+
+    if (xsdcConfigurableDomain.hasConfigurations()) {
+        aidlConfigurableDomain.configurations = VALUE_OR_FATAL(
+                (convertWrappedCollectionToAidl<eng_xsd::ConfigurationsType,
+                        eng_xsd::ConfigurationsType::Configuration, AudioHalCapConfiguration>(
+                        xsdcConfigurableDomain.getConfigurations(),
+                        &eng_xsd::ConfigurationsType::getConfiguration,
+                        std::bind(&CapEngineConfigXmlConverter::convertConfigurationToAidl, this,
+                                  std::placeholders::_1))));
+    }
+    if (xsdcConfigurableDomain.hasConfigurableElements()) {
+        aidlConfigurableDomain.parameters = VALUE_OR_FATAL(
+                (convertWrappedCollectionToAidl<eng_xsd::ConfigurableElementsType,
+                        eng_xsd::ConfigurableElementsType::ConfigurableElement,
+                        std::string>(
+                        xsdcConfigurableDomain.getConfigurableElements(),
+                        &eng_xsd::ConfigurableElementsType::getConfigurableElement,
+                        std::bind(&CapEngineConfigXmlConverter::convertConfigurableElementToAidl,
+                                  this, std::placeholders::_1))));
+    }
+    if (xsdcConfigurableDomain.hasSettings()) {
+        aidlConfigurableDomain.capSettings = VALUE_OR_FATAL(
+                (convertWrappedCollectionToAidl<eng_xsd::SettingsType,
+                        eng_xsd::SettingsType::Configuration, AudioHalCapSetting>(
+                        xsdcConfigurableDomain.getSettings(),
+                        &eng_xsd::SettingsType::getConfiguration,
+                        std::bind(&CapEngineConfigXmlConverter::convertSettingToAidl, this,
+                                  std::placeholders::_1))));
+    }
+    return aidlConfigurableDomain;
+}
+
+void CapEngineConfigXmlConverter::init() {
+    if (getXsdcConfig()->hasConfigurableDomain()) {
+        std::vector<AudioHalCapDomain> domains = VALUE_OR_FATAL(
+                (convertCollectionToAidl<eng_xsd::ConfigurableDomainType, AudioHalCapDomain>(
+                        getXsdcConfig()->getConfigurableDomain(),
+                        std::bind(&CapEngineConfigXmlConverter::convertConfigurableDomainToAidl,
+                                  this, std::placeholders::_1))));
+        std::vector<std::optional<AudioHalCapDomain>> optDomains {};
+        for (auto &domain: domains) {
+            optDomains.push_back(std::optional<AudioHalCapDomain>{std::move(domain)});
+        }
+        mAidlCapDomains = std::make_optional(std::move(optDomains));
+    } else {
+        mAidlCapDomains = std::nullopt;
+    }
+}
+
+} // namespace android
---
+++ audio/aidl/default/include/core-impl/CapEngineConfigXmlConverter.h
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <aidl/android/media/audio/common/AudioHalEngineConfig.h>
+#include <aidl/android/hardware/audio/core/BnConfig.h>
+#include <system/audio_config.h>
+
+#include <android_audio_policy_capengine_configuration.h>
+#include <android_audio_policy_capengine_configuration_enums.h>
+
+#include "EngineConfigXmlConverter.h"
+
+namespace aidl::android::hardware::audio::core::internal {
+
+class CapEngineConfigXmlConverter {
+
+public:
+    explicit CapEngineConfigXmlConverter(const std::string& configFilePath)
+            : mConverter(configFilePath,
+                    &::android::audio::policy::capengine::configuration::readConfigurableDomains) {
+        if (mConverter.getXsdcConfig()) {
+            init();
+        }
+    }
+    std::string getError() const { return mConverter.getError(); }
+    ::android::status_t getStatus() const { return mConverter.getStatus(); }
+
+    std::optional<std::vector<std::optional<
+            ::aidl::android::media::audio::common::AudioHalCapDomain>>>& getAidlCapEngineConfig();
+
+private:
+    template <typename ParamType>
+    ConversionResult<::aidl::android::media::audio::common::AudioHalCapSetting::ParameterSetting>
+            convertParamToAidl(const ParamType& xsdcTypedParam,
+            const ::android::audio::policy::capengine::configuration::ConfigurableElementSettingsType&
+            element);
+
+    ConversionResult<::aidl::android::media::audio::common::AudioHalCapConfiguration>
+            convertConfigurationToAidl(
+            const ::android::audio::policy::capengine::configuration::ConfigurationsType::Configuration&
+            xsdcConfiguration);
+    ConversionResult<std::string> convertConfigurableElementToAidl(
+            const ::android::audio::policy::capengine::configuration::ConfigurableElementsType::ConfigurableElement&
+            xsdcConfigurableElement);
+    ConversionResult<::aidl::android::media::audio::common::AudioHalCapSetting>
+            convertSettingToAidl(
+            const ::android::audio::policy::capengine::configuration::SettingsType::Configuration&
+            xsdcSetting);
+    ConversionResult<::aidl::android::media::audio::common::AudioHalCapDomain>
+            convertConfigurableDomainToAidl(
+            const ::android::audio::policy::capengine::configuration::ConfigurableDomainType&
+            xsdcConfigurableDomain);
+
+
+    const std::optional<::android::audio::policy::capengine::configuration::ConfigurableDomains>&
+    getXsdcConfig() {
+        return mConverter.getXsdcConfig();
+    }
+    void init();
+
+    std::optional<std::vector<std::optional<
+            ::aidl::android::media::audio::common::AudioHalCapDomain>>> mAidlCapDomains;
+    XmlConverter<::android::audio::policy::capengine::configuration::ConfigurableDomains>
+            mConverter;
+};
+}
