19b17efd70a5716f7c9454c30be23c50c69402e7
A. Cody Schuffelen <schuffelen@google.com>
2024-04-02 16:53:34 -0700

    C++ AIDL on-device GateKeeper / SharedSecret impl
    Because Keystore2 always requires valid auth tokens for user keys, this
    copies and extends the existing in-guest GateKeeper implementation to
    negotiate a shared secret key with Keymint in order to generate a
    session key for signing auth tokens.
    
    This implementation also uses AIDL rather than HIDL to interact with the
    framework. The name `ISharedSecret/strongbox` is used as a shortcut
    since the framework is already prepared to receive an implementation by
    this name from the vendor side. It's also packaged in an APEX.
    
    The files are cobbled together from a few sources:
    
    - SoftGateKeeper.h is based on hardware/interfaces/gatekeeper/1.0/software/SoftGateKeeper.h
    - GateKeeper.{cpp,h} are based on device/google/cuttlefish/guest/hals/gatekeeper/remote/remote_gatekeeper.{cpp,h}
    - SharedSecret.{cpp,h} are based on device/google/cuttlefish/guest/hals/keymint/remote/remote_shared_secret.{cpp,h}
    - Apex files are based on device/google/cuttlefish/guest/hals/keymint/rust/
    
    Bug: 332376454
    Change-Id: I81845d5e6370bdddb1a24d67437964e03a5fb243
    Test: Run with rust-nonsecure keymint on Cuttlefish

---
+++ gatekeeper/aidl/software/SharedSecret.cpp
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2024, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "SharedSecret.h"
+
+#include <algorithm>
+#include <cstring>
+#include <vector>
+
+#include <openssl/rand.h>
+
+#include <KeyMintUtils.h>
+#include <aidl/android/hardware/security/sharedsecret/BnSharedSecret.h>
+#include <aidl/android/hardware/security/sharedsecret/SharedSecretParameters.h>
+#include <android-base/logging.h>
+#include <keymaster/android_keymaster_messages.h>
+#include <keymaster/android_keymaster_utils.h>
+#include <keymaster/km_openssl/ckdf.h>
+#include <keymaster/km_openssl/hmac.h>
+
+namespace aidl::android::hardware::security::sharedsecret {
+
+::ndk::ScopedAStatus SoftSharedSecret::getSharedSecretParameters(
+        SharedSecretParameters* out_params) {
+    out_params->seed = std::vector<std::uint8_t>(32, 0);
+    if (nonce_.empty()) {
+        nonce_.resize(32, 0);
+        RAND_bytes(nonce_.data(), 32);
+    }
+    out_params->nonce = nonce_;
+    LOG(INFO) << "Presented shared secret parameters with seed size " << out_params->seed.size()
+              << " and nonce size " << out_params->nonce.size();
+    return ::ndk::ScopedAStatus::ok();
+}
+
+::ndk::ScopedAStatus SoftSharedSecret::computeSharedSecret(
+        const std::vector<SharedSecretParameters>& params, std::vector<uint8_t>* sharing_check) {
+    LOG(INFO) << "Computing shared secret";
+    // Reimplemented based on SoftKeymasterEnforcement, which does not expose
+    // enough functionality to satisfy the GateKeeper interface
+    if (nonce_.empty() || hmac_key_.size() > 0) {
+        LOG(ERROR) << "wrong phase in shared secret calculation process";
+        return ::ndk::ScopedAStatus::fromExceptionCode(EX_ILLEGAL_STATE);
+    }
+    keymaster::KeymasterKeyBlob key_agreement_key;
+    if (key_agreement_key.Reset(32) == nullptr) {
+        LOG(ERROR) << "key agreement key memory allocation failed";
+        return ::ndk::ScopedAStatus::fromExceptionCode(EX_ILLEGAL_STATE);
+    }
+    std::memset(key_agreement_key.writable_data(), 0, 32);
+    constexpr const char kSharedHmacLabel[] = "KeymasterSharedMac";
+    keymaster::KeymasterBlob label;
+    if (label.Reset(sizeof(kSharedHmacLabel) - 1) == nullptr) {
+        LOG(ERROR) << "label memory allocation failed";
+        return ::ndk::ScopedAStatus::fromExceptionCode(EX_ILLEGAL_STATE);
+    }
+    std::memcpy(label.writable_data(), kSharedHmacLabel, sizeof(kSharedHmacLabel) - 1);
+
+    static_assert(sizeof(keymaster_blob_t) == sizeof(keymaster::KeymasterBlob));
+
+    bool found_mine = false;
+    std::vector<keymaster::KeymasterBlob> context_blobs;
+    for (const auto& param : params) {
+        auto& seed_blob = context_blobs.emplace_back();
+        if (seed_blob.Reset(param.seed.size()) == nullptr) {
+            LOG(ERROR) << "seed memory allocation failed";
+            return ::ndk::ScopedAStatus::fromExceptionCode(EX_ILLEGAL_STATE);
+        }
+        std::copy(param.seed.begin(), param.seed.end(), seed_blob.writable_data());
+        auto& nonce_blob = context_blobs.emplace_back();
+        if (nonce_blob.Reset(param.nonce.size()) == nullptr) {
+            LOG(ERROR) << "Nonce memory allocation failed";
+            return ::ndk::ScopedAStatus::fromExceptionCode(EX_ILLEGAL_STATE);
+        }
+        std::copy(param.nonce.begin(), param.nonce.end(), nonce_blob.writable_data());
+        if (param.nonce == nonce_) {
+            found_mine = true;
+        }
+    }
+    if (!found_mine) {
+        LOG(ERROR) << "Did not receive my own shared secret parameter back";
+        return ::ndk::ScopedAStatus::fromExceptionCode(EX_ILLEGAL_ARGUMENT);
+    }
+    auto context_blobs_ptr = reinterpret_cast<keymaster_blob_t*>(context_blobs.data());
+    if (hmac_key_.Reset(32) == nullptr) {
+        LOG(ERROR) << "hmac key allocation failed";
+        return ::ndk::ScopedAStatus::fromExceptionCode(EX_ILLEGAL_STATE);
+    }
+    std::stringstream ckdf_input;
+    ckdf_input << "key_agreement_key (" << key_agreement_key.key_material_size << " ) {";
+    for (auto i = 0; i < key_agreement_key.key_material_size; i++) {
+        ckdf_input << (int)key_agreement_key.key_material[i] << ", ";
+    }
+    ckdf_input << "} label (" << label.data_length << ") { ";
+    for (auto i = 0; i < label.data_length; i++) {
+        ckdf_input << (int)label.data[i] << ", ";
+    }
+    ckdf_input << "}";
+    for (const auto& blob : context_blobs) {
+        ckdf_input << " context_blob (" << blob.data_length << ") { ";
+        for (auto i = 0; i < blob.data_length; i++) {
+            ckdf_input << (int)blob.data[i] << ", ";
+        }
+        ckdf_input << "} ";
+    }
+    LOG(INFO) << "Running CKDF with input " << ckdf_input.str();
+    auto error = keymaster::ckdf(key_agreement_key, label, context_blobs_ptr, context_blobs.size(),
+                                 &hmac_key_);
+    if (error != KM_ERROR_OK) {
+        LOG(ERROR) << "CKDF failed";
+        return keymint::km_utils::kmError2ScopedAStatus(error);
+    }
+
+    constexpr const char kMacVerificationString[] = "Keymaster HMAC Verification";
+    keymaster::KeymasterBlob verification_input;
+    if (verification_input.Reset(sizeof(kMacVerificationString) - 1) == nullptr) {
+        LOG(ERROR) << "verification input allocation failed";
+        return ::ndk::ScopedAStatus::fromExceptionCode(EX_ILLEGAL_STATE);
+    }
+    std::memcpy(verification_input.writable_data(), kMacVerificationString,
+                sizeof(kMacVerificationString) - 1);
+
+    keymaster::HmacSha256 hmac_impl;
+    if (!hmac_impl.Init(hmac_key_.key_material, hmac_key_.key_material_size)) {
+        LOG(ERROR) << "hmac initialization failed";
+        return ::ndk::ScopedAStatus::fromExceptionCode(EX_ILLEGAL_STATE);
+    }
+    sharing_check->clear();
+    sharing_check->resize(32, 0);
+    if (!hmac_impl.Sign((const uint8_t*)kMacVerificationString, sizeof(kMacVerificationString) - 1,
+                        sharing_check->data(), sharing_check->size())) {
+        return ::ndk::ScopedAStatus::fromExceptionCode(EX_ILLEGAL_STATE);
+    }
+    LOG(INFO) << "Returning shared secret calculation checksum";
+    return ::ndk::ScopedAStatus::ok();
+}
+
+}  // namespace aidl::android::hardware::security::sharedsecret
