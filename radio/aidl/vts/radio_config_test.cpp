a0d72bf09acd76442068b38894ea8bad4d87af85
sandeepjs <sandeepjs@google.com>
2024-05-21 18:08:44 +0000

    update vts setSimSlotMapping test cases to handle  different MEP types Test: atest PerInstance/RadioConfigTest#setSimSlotsMapping/0_android_hardware_radio_config_IRadioConfig_default
    Change-Id: I34229669713afabbb7802dfa7c79ff9689a7bdf5

--- radio/aidl/vts/radio_config_test.cpp
+++ radio/aidl/vts/radio_config_test.cpp
@@ -234,9 +234,17 @@ TEST_P(RadioConfigTest, setSimSlotsMapping) {
                     // logicalSlotId should be less than the maximum number of
                     // supported SIM slots.
                     EXPECT_LT(logicalSlotId, slotPortMappingList.size());
+
                     if (logicalSlotId >= 0 && logicalSlotId < slotPortMappingList.size()) {
                         slotPortMappingList[logicalSlotId].physicalSlotId = i;
-                        slotPortMappingList[logicalSlotId].portId = j;
+                        if (radioRsp_config->simSlotStatus[i].supportedMepMode ==
+                                    MultipleEnabledProfilesMode::MEP_A1 ||
+                            radioRsp_config->simSlotStatus[i].supportedMepMode ==
+                                    MultipleEnabledProfilesMode::MEP_A2) {
+                            slotPortMappingList[logicalSlotId].portId = j + 1;
+                        } else {
+                            slotPortMappingList[logicalSlotId].portId = j;
+                        }
                     }
                 }
             }
