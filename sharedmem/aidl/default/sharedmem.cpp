8baf97f3b4348180287010ffc5bd2f6f96110d61
Carlos Galo <carlosgalo@google.com>
2024-08-29 16:18:22 +0000

    SharedMem: Add AIDL implementation
    Adding new SharedMem AIDL to route request between ashmem and memfd.
    
    Test: WIP
    Change-Id: Ib4505fc142dc3e048b32753b82331b2444f98929
    Signed-off-by: Carlos Galo <carlosgalo@google.com>

---
+++ sharedmem/aidl/default/sharedmem.cpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <cutils/ashmem.h>
+
+#include "sharedmem.h"
+
+namespace aidl {
+namespace android {
+namespace hardware {
+namespace sharedmem {
+
+ndk::ScopedAStatus createRegion(const std::string& in_name, int32_t in_size,
+                                ::ndk::ScopedFileDescriptor* _aidl_return) {
+    const int result = ashmem_create_region(in_name, in_size);
+    return ndk::ScopedAStatus::ok();
+}
+
+}  // namespace sharedmem
+}  // namespace hardware
+}  // namespace android
+}  // namespace aidl
