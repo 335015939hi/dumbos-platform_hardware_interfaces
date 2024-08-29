990d61befbe5a7eedf11950c8a2464e5b592c82e
David Drysdale <drysdale@google.com>
2024-08-29 12:51:22 +0100

    KeyMint HAL that runs multiple instances
    DO NOT SUBMIT: experimental proof-of-concept code
    
    Test: build
    Change-Id: I51b5999fdf6c0e44c7879660d2b89dc7564f856b

---
+++ security/keymint/aidl/default/multi.rs
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//! Multi-instance implementation of the KeyMint HAL and related HALs.
+
+use kmr_nonsecure_main::{local_keymint_setup, HalServiceError};
+use log::{error, info, warn};
+
+static DEFAULT_SERVICE_INSTANCE: &str = "default";
+static ALT1_SERVICE_INSTANCE: &str = "alt1";
+static ALT2_SERVICE_INSTANCE: &str = "alt2";
+
+fn main() {
+    if let Err(HalServiceError(e)) = inner_main() {
+        panic!("HAL service failed: {:?}", e);
+    }
+}
+
+fn inner_main() -> Result<(), HalServiceError> {
+    // Initialize Android logging.
+    android_logger::init_once(
+        android_logger::Config::default()
+            .with_tag("keymint-multi-hal-nonsecure")
+            .with_max_level(log::LevelFilter::Info)
+            .with_log_buffer(android_logger::LogId::System),
+    );
+    // Redirect panic messages to logcat.
+    std::panic::set_hook(Box::new(|panic_info| {
+        error!("{}", panic_info);
+    }));
+
+    warn!("Insecure multi-instance KeyMint HAL service is starting.");
+
+    info!("Starting thread pool now.");
+    binder::ProcessState::start_thread_pool();
+
+    // Register for multiple instances of the various HALs.
+    for instance in [
+        DEFAULT_SERVICE_INSTANCE,
+        ALT1_SERVICE_INSTANCE,
+        ALT2_SERVICE_INSTANCE,
+    ] {
+        // Set up the local TA and register HAL services.
+        local_keymint_setup(instance)?;
+    }
+
+    binder::ProcessState::join_thread_pool();
+    info!("KeyMint HAL service is terminating."); // should not reach here
+    Ok(())
+}
