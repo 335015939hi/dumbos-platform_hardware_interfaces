116c272a37179bb8446a649478366c8e1a1e048b
Daniel Zheng <zhengdaniel@google.com>
2024-10-08 12:39:17 -0700

    boot_control death recipient
    When boot control service dies, we should log that it is no longer
    available. To do this, we should register a death recipient in the
    client.
    
    Bug: 369289491
    Test: kill bootctl service while running OTA
    Change-Id: If766451cafe939725e7a88b1ca23b2a4af0d1a80

--- boot/aidl/client/BootControlClient.cpp
+++ boot/aidl/client/BootControlClient.cpp
@@ -18,6 +18,7 @@
 
 #include <aidl/android/hardware/boot/IBootControl.h>
 #include <android-base/logging.h>
+#include <android/binder_ibinder.h>
 #include <android/binder_manager.h>
 #include <android/hardware/boot/1.0/IBootControl.h>
 #include <android/hardware/boot/1.1/IBootControl.h>
@@ -61,15 +62,28 @@ std::ostream& operator<<(std::ostream& os, MergeStatus status) {
 }
 
 namespace android::hal {
+
 class BootControlClientAidl final : public BootControlClient {
     using IBootControl = ::aidl::android::hardware::boot::IBootControl;
 
   public:
-    BootControlClientAidl(std::shared_ptr<IBootControl> module) : module_(module) {}
+    BootControlClientAidl(std::shared_ptr<IBootControl> module) : module_(module) {
+        boot_control_death_recipient = AIBinder_DeathRecipient_new(onBootCootControlServiceDied);
+        binder_status_t status = AIBinder_linkToDeath(module->asBinder().get(),
+                                                      boot_control_death_recipient, nullptr);
+        if (status != STATUS_OK) {
+            LOG(ERROR) << "Could not link to binder death";
+            return;
+        }
+    }
 
     BootControlVersion GetVersion() const override { return BootControlVersion::BOOTCTL_AIDL; }
 
-    ~BootControlClientAidl() = default;
+    ~BootControlClientAidl() {
+        if (boot_control_death_recipient) {
+            AIBinder_unlinkToDeath(module_->asBinder().get(), boot_control_death_recipient, this);
+        }
+    }
     virtual int32_t GetNumSlots() const {
         int32_t ret = -1;
         LOG_NDK_STATUS(module_->getNumberSlots(&ret));
@@ -162,6 +176,8 @@ class BootControlClientAidl final : public BootControlClient {
 
   private:
     const std::shared_ptr<IBootControl> module_;
+    AIBinder_DeathRecipient* boot_control_death_recipient;
+    static void onBootCootControlServiceDied(void*) { LOG(ERROR) << "Boot Control Service Died"; }
 };
 
 using namespace android::hardware::boot;
@@ -326,7 +342,6 @@ class BootControlClientHIDL final : public BootControlClient {
 std::unique_ptr<BootControlClient> BootControlClient::WaitForService() {
     const auto instance_name =
             std::string(::aidl::android::hardware::boot::IBootControl::descriptor) + "/default";
-
     if (AServiceManager_isDeclared(instance_name.c_str())) {
         auto module = ::aidl::android::hardware::boot::IBootControl::fromBinder(
                 ndk::SpAIBinder(AServiceManager_waitForService(instance_name.c_str())));
