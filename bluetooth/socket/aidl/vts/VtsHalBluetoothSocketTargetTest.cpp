68eafa145d003910c3336f36168789b4a3de70ca
Jayden Kim <jaydenk@google.com>
2024-10-14 19:20:10 +0000

    Add VTS for Bluetooth Socket Offload
    Bug: 342012881
    Test: atest VtsHalBluetoothSocketTargetTest
    Change-Id: Ib9eeef10c3414bf58665255120c119e3ea85c7de

---
+++ bluetooth/socket/aidl/vts/VtsHalBluetoothSocketTargetTest.cpp
@@ -0,0 +1,158 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <aidl/Gtest.h>
+#include <aidl/Vintf.h>
+#include <aidl/android/hardware/bluetooth/socket/BnBluetoothSocketCallback.h>
+#include <aidl/android/hardware/bluetooth/socket/IBluetoothSocket.h>
+#include <aidl/android/hardware/bluetooth/socket/IBluetoothSocketCallback.h>
+#include <android-base/logging.h>
+#include <android/binder_manager.h>
+#include <android/binder_process.h>
+#include <binder/IServiceManager.h>
+#include <utils/Log.h>
+
+using ::aidl::android::hardware::bluetooth::socket::BnBluetoothSocketCallback;
+using ::aidl::android::hardware::bluetooth::socket::FailureReason;
+using ::aidl::android::hardware::bluetooth::socket::IBluetoothSocket;
+using ::aidl::android::hardware::bluetooth::socket::RequestReason;
+using ::aidl::android::hardware::bluetooth::socket::SocketCapabilities;
+using ::aidl::android::hardware::bluetooth::socket::SocketContext;
+using ::ndk::ScopedAStatus;
+
+class BluetoothSocketCallback : public BnBluetoothSocketCallback {
+  ScopedAStatus onOpened(int64_t in_socketId) override;
+  ScopedAStatus onOpenFailed(int64_t in_socketId, FailureReason in_reason,
+                             int32_t in_vendorStatusCode) override;
+  ScopedAStatus onClosed(int64_t in_socketId) override;
+  ScopedAStatus onCloseFailed(int64_t in_socketId, FailureReason in_reason,
+                              int32_t in_vendorStatusCode) override;
+  ScopedAStatus onCloseRequest(int64_t in_socketId, RequestReason in_reason,
+                               int32_t in_vendorStatusCode) override;
+  ScopedAStatus onRestarted(int32_t in_vendorStatusCode) override;
+};
+
+ScopedAStatus BluetoothSocketCallback::onOpened(int64_t /*in_socketId*/) {
+  return ::ndk::ScopedAStatus::ok();
+}
+ScopedAStatus BluetoothSocketCallback::onOpenFailed(
+    int64_t /*in_socketId*/, FailureReason /*in_reason*/,
+    int32_t /*in_vendorStatusCode*/) {
+  return ::ndk::ScopedAStatus::ok();
+}
+ScopedAStatus BluetoothSocketCallback::onClosed(int64_t /*in_socketId*/) {
+  return ::ndk::ScopedAStatus::ok();
+}
+ScopedAStatus BluetoothSocketCallback::onCloseFailed(
+    int64_t /*in_socketId*/, FailureReason /*in_reason*/,
+    int32_t /*in_vendorStatusCode*/) {
+  return ::ndk::ScopedAStatus::ok();
+}
+ScopedAStatus BluetoothSocketCallback::onCloseRequest(
+    int64_t /*in_socketId*/, RequestReason /*in_reason*/,
+    int32_t /*in_vendorStatusCode*/) {
+  return ::ndk::ScopedAStatus::ok();
+}
+ScopedAStatus BluetoothSocketCallback::onRestarted(
+    int32_t /*in_vendorStatusCode*/) {
+  return ::ndk::ScopedAStatus::ok();
+}
+
+class BluetoothSocketTest : public ::testing::TestWithParam<std::string> {
+ public:
+  virtual void SetUp() override {
+    ALOGI("SetUp Socket Test");
+    bluetooth_socket_ = IBluetoothSocket::fromBinder(
+        ndk::SpAIBinder(AServiceManager_waitForService(GetParam().c_str())));
+    ASSERT_NE(bluetooth_socket_, nullptr);
+  }
+
+  virtual void TearDown() override {
+    ALOGI("TearDown Socket Test");
+    bluetooth_socket_ = nullptr;
+    ASSERT_EQ(bluetooth_socket_, nullptr);
+  }
+
+  ScopedAStatus initialize();
+  ScopedAStatus getSocketCapabilities(SocketCapabilities* _aidl_return);
+  ScopedAStatus open(SocketContext& in_context);
+  ScopedAStatus close(long in_socket_id);
+
+ private:
+  std::shared_ptr<IBluetoothSocket> bluetooth_socket_;
+};
+
+ScopedAStatus BluetoothSocketTest::initialize() {
+  std::shared_ptr<BluetoothSocketCallback> callback = nullptr;
+  callback = ndk::SharedRefBase::make<BluetoothSocketCallback>();
+  return bluetooth_socket_->initialize(callback);
+}
+
+ScopedAStatus BluetoothSocketTest::getSocketCapabilities(
+    SocketCapabilities* _aidl_return) {
+  return bluetooth_socket_->getSocketCapabilities(_aidl_return);
+}
+
+ScopedAStatus BluetoothSocketTest::open(SocketContext& in_context) {
+  return bluetooth_socket_->open(in_context);
+}
+
+ScopedAStatus BluetoothSocketTest::close(long in_socketId) {
+  return bluetooth_socket_->close(in_socketId);
+}
+
+TEST_P(BluetoothSocketTest, Initialize) {
+  ScopedAStatus status = initialize();
+  ASSERT_TRUE(status.isOk());
+}
+
+TEST_P(BluetoothSocketTest, GetSocketCapabilities) {
+  SocketCapabilities socket_capabilities;
+  ScopedAStatus status = initialize();
+  ASSERT_TRUE(status.isOk());
+  status = getSocketCapabilities(&socket_capabilities);
+  ASSERT_TRUE(status.isOk());
+}
+
+TEST_P(BluetoothSocketTest, OpenSocket) {
+  SocketContext socket_context;
+  ScopedAStatus status = initialize();
+  ASSERT_TRUE(status.isOk());
+  status = open(socket_context);
+  ASSERT_TRUE(status.isOk());
+}
+
+TEST_P(BluetoothSocketTest, CloseSocket) {
+  long socket_id = 1;
+  ScopedAStatus status = initialize();
+  ASSERT_TRUE(status.isOk());
+  status = close(socket_id);
+  ASSERT_TRUE(status.isOk());
+}
+
+GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(BluetoothSocketTest);
+INSTANTIATE_TEST_SUITE_P(PerInstance, BluetoothSocketTest,
+                         testing::ValuesIn(android::getAidlHalInstanceNames(
+                             IBluetoothSocket::descriptor)),
+                         android::PrintInstanceNameToString);
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  ABinderProcess_startThreadPool();
+  int status = RUN_ALL_TESTS();
+  ALOGI("Test result = %d", status);
+  return status;
+}
