cb568acbe2cf1d1b5c6ef37788b66281722bb067
Sean Thomas <seanmthomas@google.com>
2024-10-16 11:27:36 +0000

    returning appropriately
    Change-Id: I1b6c85b858d76b1ea418400342c89553cbd4bc1d

--- security/keymint/support/include/remote_prov/remote_prov_utils.h
+++ security/keymint/support/include/remote_prov/remote_prov_utils.h
@@ -178,7 +178,7 @@ ErrMsgOr<std::vector<BccEntryData>> verifyProductionProtectedData(
 ErrMsgOr<std::unique_ptr<cppbor::Array>> verifyFactoryCsr(
         const cppbor::Array& keysToSign, const std::vector<uint8_t>& csr,
         IRemotelyProvisionedComponent* provisionable, const std::vector<uint8_t>& challenge,
-        bool allowDegenerate = true);
+        bool allowDegenerate = true, bool requireUds = false);
 /**
  * Verify the CSR as if the device is a final production sample.
  */
