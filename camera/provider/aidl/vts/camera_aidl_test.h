259f865cbb01155b73eef0f9f07d7b7a7b7e3181
Yudhistira Erlandinata <yerlandinata@google.com>
2024-11-12 15:46:36 +0800

    Camera: VTS: Fix getMinSize wrong assumption
    CameraAidlTest::getMinSize(Size a, Size b) was actually used to get max
    supported output size, but not larger than a reference size, e.g.
    Size jpegMaxSize(1920, 1440); // Reference size
    Size maxAvailableJpegSize;
    getMaxOutputSizeForFormat(staticMeta, PixelFormat::BLOB, &maxAvailableJpegSize);
    Size jpegChosenSize = getMinSize(jpegMaxSize, maxAvailableJpegSize);
    
    Here comes the problem: If the camera provider does not actually support
    the reference size, but it got chosen, then the test will be wrong.
    
    Refactor the getMinSize to be getPreferredSize. The new function
    accepts two params: the prefered max size, and all available sizes.
    It will return the max size from the available sizes, ignoring those
    larger than the prefered max size. This way the function can still
    serve the original purpose while always returning supported size.
    
    Test: VtsAidlHalCameraProvider_TargetTest on vendor HAL
    Bug: 378423470
    Change-Id: Ia42f6e012edef7531ea37b8168073791906bebc3

--- camera/provider/aidl/vts/camera_aidl_test.h
+++ camera/provider/aidl/vts/camera_aidl_test.h
@@ -328,6 +328,9 @@ class CameraAidlTest : public ::testing::TestWithParam<std::string> {
     static Status getMaxOutputSizeForFormat(const camera_metadata_t* staticMeta, PixelFormat format,
                                             Size* size, bool maxResolution = false);
 
+    static Status getAllOutputSizesForFormat(const camera_metadata_t* staticMeta,
+                                             PixelFormat format, std::vector<Size>& out);
+
     static Status getMandatoryConcurrentStreams(const camera_metadata_t* staticMeta,
                                                 std::vector<AvailableStream>* outputStreams);
 
@@ -433,7 +436,11 @@ class CameraAidlTest : public ::testing::TestWithParam<std::string> {
     // Used by switchToOffline where a new result queue is created for offline reqs
     void updateInflightResultQueue(const std::shared_ptr<ResultMetadataQueue>& resultQueue);
 
-    static Size getMinSize(Size a, Size b);
+    // Returns maximum size from |sizes|, excluding sizes larger than |preferredMax|.
+    // |sizes| must not be empty.
+    // Motivation: Just because the ISP supports an output size larger than |preferredMax|,
+    //             it doesn't necessarily means the ISP also supports the |preferredMax|.
+    static Size getPreferredSize(Size preferredMax, const std::vector<Size>& sizes);
 
     void processColorSpaceRequest(RequestAvailableColorSpaceProfilesMap colorSpace,
                                   RequestAvailableDynamicRangeProfilesMap dynamicRangeProfile);
