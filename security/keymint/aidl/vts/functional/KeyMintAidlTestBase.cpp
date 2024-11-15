5f02579c3f78185cbed10d796d0014181021d90a
Catherine Vlasov <cvlasov@google.com>
2024-11-15 15:03:54 +0000

    Add strict equality check for VB key digest length on VSR-16+.
    Bug: 376832222
    Bug: 220834466
    Test: atest VtsAidlKeyMintTargetTest
    Change-Id: I6f3decf87a4ec3de57ed21e193954ef3d1a328df

--- security/keymint/aidl/vts/functional/KeyMintAidlTestBase.cpp
+++ security/keymint/aidl/vts/functional/KeyMintAidlTestBase.cpp
@@ -1812,16 +1812,25 @@ void verify_root_of_trust(const vector<uint8_t>& verified_boot_key, bool device_
         }
     }
 
+    if (get_vsr_api_level() > __ANDROID_API_V__) {
+        // The attestation should contain the SHA-256 hash of the Verified Boot
+        // key on locked devices and 32 bytes of zeroes on unlocked devices.
+        // This wasn't checked for earlier versions of the KeyMint HAL, so only
+        // only be strict for VSR-16+.
+        EXPECT_EQ(verified_boot_key.size(), 32);
+    } else if (get_vsr_api_level() == __ANDROID_API_V__) {
+        // The attestation should contain the SHA-256 hash of the Verified Boot
+        // key on locked devices. Behaviour on unlocked devices isn't specified
+        // in the HAL interface specification, so the check isn't for strict
+        // equality in case unlocked devices have zero-length values. This wasn't
+        // checked for earlier versions of the KeyMint HAL, so only check on VSR-15.
+        EXPECT_LE(verified_boot_key.size(), 32);
+    }
+
     // Verified boot key should be all 0's if the boot state is not verified or self signed
     std::string empty_boot_key(32, '\0');
     std::string verified_boot_key_str((const char*)verified_boot_key.data(),
                                       verified_boot_key.size());
-    if (get_vsr_api_level() >= __ANDROID_API_V__) {
-        // The attestation should contain the SHA-256 hash of the verified boot
-        // key.  However, this was not checked for earlier versions of the KeyMint
-        // HAL so only be strict for VSR-V and above.
-        EXPECT_LE(verified_boot_key.size(), 32);
-    }
     EXPECT_NE(property_get("ro.boot.verifiedbootstate", property_value, ""), 0);
     if (!strcmp(property_value, "green")) {
         EXPECT_EQ(verified_boot_state, VerifiedBoot::VERIFIED);
