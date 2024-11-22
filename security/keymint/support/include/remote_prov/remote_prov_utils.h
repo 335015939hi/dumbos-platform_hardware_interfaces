ca72fb6e3d8c54500fb33eb3a7e62b7ba3a4ce7f
Sean Thomas <seanmthomas@google.com>
2024-11-22 19:12:00 +0000

    Unlocked devices should have nonnormal DICE chains
    Adds a test that checks two system properties for an indication that the
    device is unlocked. If the DICE chain is not degenerate, then the test
    checks that the mode on the leaf certificate in the DICE chain is not
    normal.
    
    Test: atest VtsHalRemotelyProvisionedComponentTargetTest
          atest libkeymint_remote_prov_support_test
    Bug: 377829077
    Change-Id: I65a9845f32d673833ccd2993d59f9ed899d2da11

--- security/keymint/support/include/remote_prov/remote_prov_utils.h
+++ security/keymint/support/include/remote_prov/remote_prov_utils.h
@@ -214,6 +214,10 @@ ErrMsgOr<std::unique_ptr<cppbor::Array>> verifyProductionCsr(const cppbor::Array
 ErrMsgOr<bool> isCsrWithProperDiceChain(const std::vector<uint8_t>& csr,
                                         const std::string& instanceName);
 
+/** Checks whether the DICE chain in the CSR has a certificate with a non-normal mode. */
+ErrMsgOr<bool> hasNotNormalModeInDiceChain(const std::vector<uint8_t>& csr,
+                                           std::string_view instanceName);
+
 /** Verify the DICE chain. */
 ErrMsgOr<std::vector<BccEntryData>> validateBcc(const cppbor::Array* bcc,
                                                 hwtrust::DiceChain::Kind kind, bool allowAnyMode,
