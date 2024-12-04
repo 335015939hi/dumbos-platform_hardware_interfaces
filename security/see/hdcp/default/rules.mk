4cd22addd6d8e6b7990a34b7adcaad1fa631b598
Armelle Laine <armellel@google.com>
2024-12-04 14:46:00 +0000

    security: see: hdcp: mock reference implementation
    Bug: 380193246
    Test: N/A
    Change-Id: I0fb6ac67e48b42e23249a997149d8787bff21204

---
+++ security/see/hdcp/default/rules.mk
@@ -0,0 +1,40 @@
+# Copyright (C) 2024 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+MODULE := $(LOCAL_DIR)
+
+MANIFEST := $(LOCAL_DIR)/manifest.json
+
+MODULE_SRCS += \
+	$(LOCAL_DIR)/src/main.rs \
+
+MODULE_CRATE_NAME := hdcpauthcontrol_hal
+
+MODULE_LIBRARY_DEPS += \
+	hardware/interfaces/security/see/hdcp/aidl/trusty/hdcp/rust \
+	hardware/interfaces/security/see/hdcp/aidl/trusty/drm/rust \
+	frameworks/native/libs/binder/trusty/rust \
+	frameworks/native/libs/binder/trusty/rust/binder_rpc_server \
+	frameworks/native/libs/binder/trusty/rust/rpcbinder \
+	trusty/user/base/lib/trusty-sys \
+	trusty/user/base/lib/trusty-std \
+	$(call FIND_CRATE,log) \
+	$(call FIND_CRATE,mockall) \
+	trusty/user/base/lib/trusty-log \
+
+include make/trusted_app.mk
+
