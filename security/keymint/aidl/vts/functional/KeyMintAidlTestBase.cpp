bab73331830dbf98476319d3bf4b3ccb3e706f0d
Yinchu Chen <chenyc5@motorola.com>
2025-02-14 02:01:24 -0800

    Skipping test on GSI + first_api_level < U
    The test is marked as assumption failed for Key attestation fails on the
    combination of the build with the vendor froze on the Android T base
    build and Android U/V GSI build because the attestation properties
    ro.product.*_for_attestation have to be present in vendor properties
    files.
    
    Bug: 396582186
    Test:
    VtsAidlKeyMintTargetTest.PerInstance/NewKeyGenerationTest
    
    Change-Id: I661fb0041b6e8918a32f6ee72024ebad119260d0

--- security/keymint/aidl/vts/functional/KeyMintAidlTestBase.cpp
+++ security/keymint/aidl/vts/functional/KeyMintAidlTestBase.cpp
@@ -249,7 +249,13 @@ uint32_t KeyMintAidlTestBase::boot_patch_level() {
  * which is mandatory for KeyMint version 2 and first_api_level 33 or greater.
  */
 bool KeyMintAidlTestBase::isDeviceIdAttestationRequired() {
-    return AidlVersion() >= 2 && property_get_int32("ro.vendor.api_level", 0) >= __ANDROID_API_T__;
+    if (!is_gsi_image()) {
+        return AidlVersion() >= 2 &&
+               property_get_int32("ro.vendor.api_level", 0) >= __ANDROID_API_T__;
+    }
+    else {
+        return AidlVersion() >= 2 && get_vendor_api_level() >= __ANDROID_API_U__;
+    }
 }
 
 /**
